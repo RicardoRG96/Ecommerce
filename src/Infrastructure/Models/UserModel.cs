292abad8efb1cc3ca799d61f5957d0f23d25fd73
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-06 07:49:13 -0400

    add UserModel to Infrastructure layer

---
+++ src/Infrastructure/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public sealed class UserModel
+    {
+        public long UserId { get; set; }
+        public string? Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
