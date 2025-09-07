3e8d19d2f9ea943beb31a733876205614605047c
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-07 09:09:45 -0300

    add Schemas class to database configuration

--- src/Infrastructure/Database/ApplicationDbContext.cs
+++ src/Infrastructure/Database/ApplicationDbContext.cs
@@ -1,10 +1,5 @@
 ﻿using Infrastructure.Models.Users;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Database
 {
