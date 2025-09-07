0243e171d28853c0ff826b4ca9d29beddcde9908
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-07 10:39:57 -0300

    refactor to CountryModel

--- src/Infrastructure/Models/Users/CountryModel.cs
+++ src/Infrastructure/Models/Users/CountryModel.cs
@@ -1,15 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Infrastructure.Models.Users
+﻿namespace Infrastructure.Models.Users
 {
     public sealed class CountryModel
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long CountryId { get; set; }
-
-        [Required]
-        public string? Name { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<AddressModel>? Addresses { get; } = new List<AddressModel>();
     }
 }
