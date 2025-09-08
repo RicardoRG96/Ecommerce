b20f7c1c424251dbacd34668132bde6f2676d04f
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-08 06:45:20 -0300

    add MunicipalityModelConfiguration

---
+++ src/Infrastructure/Database/Configurations/MunicipalityModelConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Models.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.Configurations
+{
+    public class MunicipalityModelConfiguration : IEntityTypeConfiguration<MunicipalityModel>
+    {
+        public void Configure(EntityTypeBuilder<MunicipalityModel> builder)
+        {
+            builder.HasKey(m => m.MunicipalityId);
+
+            builder.Property(m => m.MunicipalityId)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(m => m.Name)
+                .IsRequired()
+                .HasMaxLength(120);
+
+            builder.Property(m => m.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(m => m.UpdatedAt)
+                .ValueGeneratedOnUpdate()
+                .HasDefaultValueSql("NULL");
+
+            // Relationships
+            builder.HasMany(m => m.Addresses)
+                .WithOne(a => a.Municipality)
+                .HasForeignKey(a => a.MunicipalityId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
