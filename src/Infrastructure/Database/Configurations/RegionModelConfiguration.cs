366e3e7f33cd996ed7c802cb08d7f874ac51c068
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-08 06:39:43 -0300

    add RegionModelConfiguration

---
+++ src/Infrastructure/Database/Configurations/RegionModelConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Models.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.Configurations
+{
+    public class RegionModelConfiguration : IEntityTypeConfiguration<RegionModel>
+    {
+        public void Configure(EntityTypeBuilder<RegionModel> builder)
+        {
+            builder.HasKey(r => r.RegionId);
+
+            builder.Property(r => r.RegionId)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(120);
+
+            builder.Property(r => r.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(r => r.UpdatedAt)
+                .ValueGeneratedOnUpdate()
+                .HasDefaultValueSql("NULL");
+
+            // Relationships
+            builder.HasMany(r => r.Municipalities)
+                .WithOne(m => m.Region)
+                .HasForeignKey(m => m.RegionId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
