efffec01abecf046fb099dbfdf2d9651281f3dde
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-08 08:00:00 -0300

    add DependencyInjection class

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,42 @@
+﻿using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration) => 
+            services
+                /* in a near future, we will have more services to add */
+                //.AddServices()
+                .AddDatabase(configuration)
+                .AddHealthChecks(configuration);
+
+        // Placeholder for adding more services in the future
+        //public static IServiceCollection AddServices(this IServiceCollection services)
+        //{
+        //    // add transient, scoped, or singleton services here
+        //    return services;
+        //}
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("EcommerceDb");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            // add here an interface for the DbContext if we will not use repository pattern
+            // services.AddScoped<IApplication>
+
+            return services;
+        }
+    }
+}
