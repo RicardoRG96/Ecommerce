6fef24bb6c87e5bdb81c126ab93db587187cb64a
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-11 05:52:20 -0300

    Add DependencyInjection to Web Api layer

---
+++ src/Web.Api/Web.Api/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddControllers();
+
+            services.AddExceptionHandler<GlobalExceptionHandler>();
+            services.AddProblemDetails();
+
+            return services;
+        }
+    }
+}
