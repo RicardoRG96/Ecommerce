c9a1d263036a7eceee9a066b3741891a852d2c30
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-13 06:24:40 -0300

    Add User entity

--- src/Domain/Users/Entities/User.cs
+++ src/Domain/Users/Entities/User.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Users.Entities
+﻿namespace Domain.Users.Entities
 {
     public class User
     {
         public long UserId { get; set; }
+        public string? Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
