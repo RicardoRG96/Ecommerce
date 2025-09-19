51b01346d36f3f01340c75d69fbf18c7e2bf6d9d
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-19 07:46:51 -0300

    Refactor to User Domain entity

--- src/Domain/Users/Entities/User.cs
+++ src/Domain/Users/Entities/User.cs
@@ -1,6 +1,8 @@
-﻿namespace Domain.Users.Entities
+﻿using SharedKernel;
+
+namespace Domain.Users.Entities
 {
-    public sealed class User
+    public sealed class User : BaseAuditableEntity
     {
         public long UserId { get; set; }
         public string? Avatar { get; set; }
@@ -11,6 +13,7 @@
         public string PasswordHash { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string? PhoneNumber { get; set; }
+        public IList<Address> Addresses { get; private set; } = new List<Address>();
         public bool HasLegalAge() => (DateTime.UtcNow - DateOfBirth).TotalDays / 365.25 >= 18; // Legal age in Chile is 18
     }
 }
