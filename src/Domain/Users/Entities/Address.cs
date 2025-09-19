48e230bc525c1ba110e41cc00a1662bae67e454a
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-19 09:13:05 -0300

    Refactor to Address domain entity

--- src/Domain/Users/Entities/Address.cs
+++ src/Domain/Users/Entities/Address.cs
@@ -1,18 +1,23 @@
-﻿namespace Domain.Users.Entities
+﻿using SharedKernel;
+
+namespace Domain.Users.Entities
 {
-    public sealed class Address
+    public sealed class Address : BaseAuditableEntity
     {
         public long AddressId { get; set; }
         public long UserId { get; set; }
-        public long CountryId { get; set; }
+        public long CountryId { get; set; } = 1; // Default to Chile
         public long MunicipalityId { get; set; }
-        public string Title { get; set; }
-        public string City { get; set; }
-        public string Street { get; set; }
-        public string Number { get; set; }
+        public string? Title { get; set; }
+        public string? City { get; set; }
+        public string? Street { get; set; }
+        public string? Number { get; set; }
         public string? Apartament { get; set; }
         public string? Reference { get; set; }
         public string? PostalCode { get; set; }
         public bool IsDefault { get; set; }
+        public IList<User> Users { get; private set; } = new List<User>();
+        public Country Country { get; set; } = null!;
+        public Municipality Municipality { get; set; } = null!;
     }
 }
