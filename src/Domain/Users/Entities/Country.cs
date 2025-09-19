a78cda355b7726c720a3b319e4c0cc1252573dd8
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-19 07:52:24 -0300

    Refactor to Model domain entity

--- src/Domain/Users/Entities/Country.cs
+++ src/Domain/Users/Entities/Country.cs
@@ -1,8 +1,11 @@
-﻿namespace Domain.Users.Entities
+﻿using SharedKernel;
+
+namespace Domain.Users.Entities
 {
-    public sealed class Country
+    public sealed class Country : BaseAuditableEntity
     {
         public long CountryId { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Address> Addresses { get; private set; } = new List<Address>();
     }
 }
