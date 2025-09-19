afe82130b59c013878783719e44e6fc84d82569d
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-19 07:41:43 -0300

    Add BaseAuditableEntity

---
+++ src/SharedKernel/BaseAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedKernel
+{
+    public abstract class BaseAuditableEntity : BaseEntity
+    {
+        public DateTimeOffset CreatedAt { get; set; }
+        public string? CreatedBy { get; set; } // utilizarlo en un interceptor (Infrastructure/Persistence/Interceptors)
+        public DateTimeOffset LastModified { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
