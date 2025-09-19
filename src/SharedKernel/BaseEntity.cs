2eee666e48b29a5fd4aad6bdf6de04f88e4e9d97
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-19 07:33:19 -0300

    Add BaseEntity

---
+++ src/SharedKernel/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharedKernel
+{
+    public abstract class BaseEntity
+    {
+        private readonly List<IDomainEvent> _domainEvents = [];
+
+        [NotMapped]
+        public List<IDomainEvent> DomainEvents => [.. _domainEvents];
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+
+        public void Raise(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Remove(domainEvent);
+        }
+    }
+}
