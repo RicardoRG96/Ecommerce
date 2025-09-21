ddc383a1284c1ea129b2ef3afd68796e7917efd1
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-21 08:52:55 -0300

    Add AddressRepository

---
+++ src/Infrastructure/Persistence/Repositories/Users/AddressRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Domain.Users.Entities;
+using Infrastructure.Persistence.Database;
+
+namespace Infrastructure.Persistence.Repositories.Users
+{
+    public class AddressRepository : Repository<Address>, IAddressRepository
+    {
+        public AddressRepository(ApplicationDbContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
