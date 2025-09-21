61bcafa5a94e75ffa7e2c44ee1c2af43ccf4d33e
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-21 08:54:12 -0300

    Add CountryRepository

---
+++ src/Infrastructure/Persistence/Repositories/Users/CountryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Domain.Users.Entities;
+using Infrastructure.Persistence.Database;
+
+namespace Infrastructure.Persistence.Repositories.Users
+{
+    public class CountryRepository : Repository<Country>, ICountryRepository
+    {
+        public CountryRepository(ApplicationDbContext context)
+            : base(context)
+        {
+            
+        }
+    }
+}
