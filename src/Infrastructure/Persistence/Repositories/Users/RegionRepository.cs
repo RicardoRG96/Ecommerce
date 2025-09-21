7daaed3a6b2cc44141371d635c3f14ef3daec8b4
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-21 08:58:13 -0300

    Add RegionRepository

---
+++ src/Infrastructure/Persistence/Repositories/Users/RegionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Domain.Users.Entities;
+using Infrastructure.Persistence.Database;
+
+namespace Infrastructure.Persistence.Repositories.Users
+{
+    public class RegionRepository : Repository<Region>, IRegionRepository
+    {
+        public RegionRepository(ApplicationDbContext context)
+            : base(context)
+        {
+            
+        }
+    }
+}
