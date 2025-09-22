36ac2e40ecce51c73a06fc8215cbdd3d4a6b5546
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-22 06:31:35 -0300

    Add UnitOfWork Implementation

---
+++ src/Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Application.Abstractions.Data.UnitOfWork;
+using Infrastructure.Persistence.Database;
+
+namespace Infrastructure.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
