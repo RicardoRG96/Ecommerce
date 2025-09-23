7ebc8236e2417934fd7e700a8f80428c866425e2
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-23 05:53:29 -0300

    Add AddressErrors

--- src/Domain/Errors/Users/UserErrors.cs
+++ src/Domain/Errors/Users/UserErrors.cs
@@ -16,6 +16,10 @@ namespace Domain.Errors.Users
             "Users.NotFoundByEmail",
             "The user with the specified email was not found");
 
+        public static readonly Error NotFoundByUsername = Error.NotFound(
+            "Users.NotFoundByUsername",
+            "The user with the specified username was not found");
+
         public static readonly Error EmailNotUnique = Error.Conflict(
             "Users.EmailNotUnique",
             "The provided email is not unique");
