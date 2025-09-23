337bae378f444d463c2113aa5922656f6aeb2ef9
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-23 06:45:47 -0300

    Add UserResponse

---
+++ src/Application/Users/User/GetById/UserResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Users.User.GetById
+{
+    public sealed class UserResponse
+    {
+        public int Id { get; set; }
+        public string Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
