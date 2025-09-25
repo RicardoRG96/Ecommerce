2d45e9f9cc36cf199b01526e51400d96a942e19e
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-25 06:14:58 -0300

    Add GetByUsernameQuery to GetByUsername UseCase

---
+++ src/Application/Users/Users/GetByUsername/GetByUsernameQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Users.GetByUsername
+{
+    public sealed record GetByUsernameQuery(string Username) : IQuery<UserResponse>;
+}
