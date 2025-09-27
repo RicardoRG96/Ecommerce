4b493da27e6cdc07133fd4e8d36eeb57781dfc13
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-27 10:10:54 -0300

    Add SwaggerExtensions

---
+++ src/Web.Api/Web.Api/OpenApi/SwaggerExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Web.Api.OpenApi
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Ecommerce API"
+                });
+
+                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+    }
+}
