ea4584f8a8dd4323541a0b33bace55cccac96b51
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-27 10:46:27 -0300

    Add VersioningExtensions

---
+++ src/Web.Api/Web.Api/Versioning/VersioningExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Asp.Versioning;
+
+namespace Web.Api.Versioning
+{
+    /// <summary>
+    /// Provides extension methods for configuring API versioning services.
+    /// </summary>
+    public static class VersioningExtensions
+    {
+        /// <summary>
+        /// Adds API versioning and related services to the specified <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The service collection to add versioning to.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            });
+
+            services.AddEndpointsApiExplorer();
+
+            return services;
+        }
+    }
+}
