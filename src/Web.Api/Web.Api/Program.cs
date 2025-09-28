b748373f71eb63e1b9573cd1445bf7617415463c
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-28 09:32:34 -0300

    Add extension method AddApiVersioning to Program.cs

--- src/Web.Api/Web.Api/Program.cs
+++ src/Web.Api/Web.Api/Program.cs
@@ -8,13 +8,10 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddPresentation()
     .AddInfrastructure(builder.Configuration)
-    .AddSwagger();
+    .AddSwagger()
+    .AddApiVersioning();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
