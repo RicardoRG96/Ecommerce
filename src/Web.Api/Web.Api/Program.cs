639f37b31b1d3c738f3da57380b087e6a516a620
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-28 09:38:41 -0300

    refactor to Program.cs

--- src/Web.Api/Web.Api/Program.cs
+++ src/Web.Api/Web.Api/Program.cs
@@ -1,6 +1,7 @@
 using Asp.Versioning.ApiExplorer;
 using Infrastructure;
 using Web.Api;
+using Web.Api.Extensions;
 using Web.Api.OpenApi;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -16,22 +17,7 @@ WebApplication app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        IReadOnlyList<ApiVersionDescription> descriptions = app.DescribeApiVersions();
-
-        foreach (ApiVersionDescription description in descriptions)
-        {
-            string url = $"/swagger/{description.GroupName}/swagger.json";
-            string name = description.GroupName.ToUpperInvariant();
-
-            options.SwaggerEndpoint(url, name);
-            options.DisplayRequestDuration();
-            options.EnableDeepLinking();
-            options.ShowExtensions();
-        }
-    });
+    app.UseSwaggerWithUi();
 }
 
 app.UseHttpsRedirection();
