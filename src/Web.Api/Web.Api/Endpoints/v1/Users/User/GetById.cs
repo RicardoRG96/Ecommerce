e1e442537c85a90e9317288a508a65fa8b2c7249
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-30 06:56:54 -0300

    Refactor GetById user endpoint

--- src/Web.Api/Web.Api/Endpoints/v1/Users/User/GetById.cs
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/User/GetById.cs
@@ -1,8 +1,11 @@
 ﻿
 using Application.Abstractions.Messaging;
 using Application.Users.Users.GetById;
+using Asp.Versioning;
+using Asp.Versioning.Builder;
 using SharedKernel;
 using Web.Api.Extensions;
+using Web.Api.Infrastructure;
 
 namespace Web.Api.Endpoints.v1.Users.User
 {
@@ -10,16 +13,23 @@ namespace Web.Api.Endpoints.v1.Users.User
     {
         public void MapEndpoint(IEndpointRouteBuilder app)
         {
-            app.MapGet("users/{userId}", async (
+            ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+                .HasApiVersion(new ApiVersion(1))
+                .ReportApiVersions()
+                .Build();
+
+            app.MapGet("api/v{apiVersion:apiVersion}/users/{userId}", async (
                 long userId,
                 IQueryHandler<GetUserByIdQuery, UserResponse> handler,
                 CancellationToken cancellationToken) =>
             {
                 GetUserByIdQuery query = new(userId);
+
                 Result<UserResponse> result = await handler.Handle(query);
 
                 return result.Match(Results.Ok, CustomResults.Problem);
             })
+            .WithApiVersionSet(apiVersionSet)
             .WithTags(Tags.Users);
         }
     }
