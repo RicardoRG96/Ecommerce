c0cfada9ccbc6ec4cd641154c70a7a7989ea7429
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-30 05:50:42 -0300

    Add Tags class

---
+++ src/Web.Api/Web.Api/Endpoints/Tags.cs
@@ -0,0 +1,35 @@
+﻿namespace Web.Api.Endpoints
+{
+    public static class Tags
+    {
+        // Users domain tags
+        public const string Users = "Users";
+        public const string Addresses = "Addresses";
+        public const string Municipalitites = "Municipalitites";
+        public const string Regions = "Regions";
+        public const string Countries = "Countries";
+        public const string Wishlist = "Wishlist";
+
+        // Products domain tags
+        public const string Products = "Products";
+        public const string ProductsSku = "ProductsSku";
+        public const string Categories = "Categories";
+        public const string SubCategories = "SubCategories";
+        public const string ProductAttributes = "ProductAttributes";
+        public const string Brands = "Brands";
+        public const string ProductGallery = "ProductGallery";
+        public const string Discount = "Discount";
+        public const string DiscountCode = "DiscountCode";
+
+        // Orders domain tags
+        public const string Orders = "Orders";
+        public const string OrderItems = "OrderItems";
+
+        // Cart domain tags
+        public const string Carts = "Carts";
+        public const string CartItems = "CartItems";
+
+        // Payment domain tags
+        public const string PaymentDetails = "PaymentDetails";
+    }
+}
