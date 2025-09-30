d6d5a44a7e96dddd7b985270a896eb93f51059a2
RicardoRG96 <ricardoretamal10@gmail.com>
2025-09-30 06:10:17 -0300

    Add GetById endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/User/GetById.cs
@@ -0,0 +1,26 @@
+﻿
+using Application.Abstractions.Messaging;
+using Application.Users.Users.GetById;
+using SharedKernel;
+using Web.Api.Extensions;
+
+namespace Web.Api.Endpoints.v1.Users.User
+{
+    public class GetById : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("users/{userId}", async (
+                long userId,
+                IQueryHandler<GetUserByIdQuery, UserResponse> handler,
+                CancellationToken cancellationToken) =>
+            {
+                GetUserByIdQuery query = new(userId);
+                Result<UserResponse> result = await handler.Handle(query);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .WithTags(Tags.Users);
+        }
+    }
+}
