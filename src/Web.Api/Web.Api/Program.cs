3ab068bae23d5d901fd22cc5deac324d153dd4a1
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-01 06:32:40 -0300

    Add endpoints configuration to Program.cs

--- src/Web.Api/Web.Api/Program.cs
+++ src/Web.Api/Web.Api/Program.cs
@@ -1,4 +1,7 @@
+using Asp.Versioning;
+using Asp.Versioning.Builder;
 using Infrastructure;
+using System.Reflection;
 using Web.Api;
 using Web.Api.Extensions;
 using Web.Api.OpenApi;
@@ -13,7 +16,20 @@ builder.Services
     .AddSwagger()
     .AddApiVersioning();
 
+builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
 WebApplication app = builder.Build();
+
+ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+                .HasApiVersion(new ApiVersion(1))
+                .ReportApiVersions()
+                .Build();
+
+RouteGroupBuilder versionedGroup = app
+    .MapGroup("api/v{apiVersion:apiVersion}")
+    .WithApiVersionSet(apiVersionSet);
+
+app.MapEndpoints(versionedGroup);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
