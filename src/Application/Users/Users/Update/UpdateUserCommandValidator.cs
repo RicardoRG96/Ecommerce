7ad90d91f59a39952bb229021a076b12576908d8
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-04 09:26:57 -0300

    Refactor to UpdateUserCommandValidator

--- src/Application/Users/Users/Update/UpdateUserCommandValidator.cs
+++ src/Application/Users/Users/Update/UpdateUserCommandValidator.cs
@@ -9,7 +9,7 @@ namespace Application.Users.Users.Update
             RuleFor(c => c.UserId).NotEmpty();
             RuleFor(c => c.FirstName).NotEmpty().MaximumLength(80);
             RuleFor(c => c.LastName).NotEmpty().MaximumLength(80);
-            RuleFor(c => c.PhoneNumber).NotEmpty().Length(9);
+            RuleFor(c => c.PhoneNumber).NotEmpty().Length(12);
         }
     }
 }
