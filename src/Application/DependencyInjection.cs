b0f28093221275038f036237f4f669587befd8d3
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-04 09:19:35 -0300

    Refactor to DependencyInjection

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -20,7 +20,9 @@ namespace Application
                     .AsImplementedInterfaces()
                     .WithScopedLifetime());
 
-            services.Decorate(typeof(ICommandHandler<,>), typeof(ValidationDecorator.CommandHandler<,>));
+            // descomentar cuando agregue una implementacion para este tipo de handler
+            // ya que por ahora no tengo ninguna
+            //services.Decorate(typeof(ICommandHandler<,>), typeof(ValidationDecorator.CommandHandler<,>));
             services.Decorate(typeof(ICommandHandler<>), typeof(ValidationDecorator.CommandBaseHandler<>));
 
             services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
