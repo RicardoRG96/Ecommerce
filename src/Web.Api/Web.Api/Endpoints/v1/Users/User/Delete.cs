5226a21c84b5a17fc60ef0990b54702c95e2ab64
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-04 09:47:09 -0300

    Add Delete user endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/User/Delete.cs
@@ -0,0 +1,28 @@
+﻿
+using Application.Abstractions.Messaging;
+using Application.Users.Users.Delete;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Users.User
+{
+    internal sealed class Delete : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("users/{userId}", async (
+                long userId,
+                ICommandHandler<DeleteUserCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                DeleteUserCommand command = new(userId);
+
+                Result result = await handler.Handle(command);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .WithTags(Tags.Users);
+        }
+    }
+}
