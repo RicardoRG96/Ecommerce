cb53d28d78c6a95eceb7ea20d6bf2e71bc2e2ce4
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-05 08:26:11 -0300

    Refactor to DeleteUserCommandHandler

--- src/Application/Users/Users/Delete/DeleteUserCommandHandler.cs
+++ src/Application/Users/Users/Delete/DeleteUserCommandHandler.cs
@@ -18,9 +18,9 @@ namespace Application.Users.Users.Delete
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<Result> Handle(DeleteUserCommand command)
+        public async Task<Result> Handle(DeleteUserCommand command, CancellationToken cancellationToken)
         {
-            User? user = await _userRepository.GetByIdAsync(command.UserId);
+            User? user = await _userRepository.GetByIdAsync(command.UserId, cancellationToken);
 
             if (user is null)
             {
@@ -28,7 +28,7 @@ namespace Application.Users.Users.Delete
             }
 
             _userRepository.Delete(user);
-            await _unitOfWork.SaveChangesAsync();
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
             return Result.Success();
         }
