9fe0b60e853826bb1ccb608be6aefe6b9f6774df
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-05 10:31:07 -0300

    Refactor to GetAllAsync method definition in IRepository

--- src/Application/Abstractions/Data/Repositories/IRepository.cs
+++ src/Application/Abstractions/Data/Repositories/IRepository.cs
@@ -1,9 +1,14 @@
-﻿namespace Application.Abstractions.Data.Repositories
+﻿using SharedKernel;
+
+namespace Application.Abstractions.Data.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
         Task<TEntity?> GetByIdAsync(long id, CancellationToken cancellationToken);
-        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+        Task<PaginatedList<TEntity>> GetAllAsync(
+            int pageNumber, 
+            int pageSize, 
+            CancellationToken cancellationToken);
         Task AddAsync(TEntity entity, CancellationToken cancellationToken);
         void Update(TEntity entity);
         void Delete(TEntity entity);
