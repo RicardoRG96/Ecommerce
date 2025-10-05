07bf32cc85fbe76aebd06b267fb67a08a7ae793d
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-05 08:09:34 -0300

    Refactor to IRepository

--- src/Application/Abstractions/Data/Repositories/IRepository.cs
+++ src/Application/Abstractions/Data/Repositories/IRepository.cs
@@ -2,10 +2,10 @@
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-        Task<TEntity?> GetByIdAsync(long id);
-        Task<IEnumerable<TEntity>> GetAllAsync();
-        Task AddAsync(TEntity entity);
-        void Update(TEntity entity);
-        void Delete(TEntity entity);
+        Task<TEntity?> GetByIdAsync(long id, CancellationToken cancellationToken);
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+        void Update(TEntity entity, CancellationToken cancellationToken);
+        void Delete(TEntity entity, CancellationToken cancellationToken);
     }
 }
