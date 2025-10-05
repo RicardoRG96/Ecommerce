27569346107cb3d9fd5c522ef256f094dfd01eba
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-05 10:09:20 -0300

    Add PaginatedList

---
+++ src/SharedKernel/PaginatedList.cs
@@ -0,0 +1,30 @@
+﻿namespace SharedKernel
+{
+    public class PaginatedList<T>
+    {
+        public IReadOnlyCollection<T> Items { get; }
+        public int PageNumber { get; }
+        public int TotalPages { get; }
+        public int TotalCount { get; }
+
+        private PaginatedList(IReadOnlyCollection<T> items, int count, int pageNumber, int pageSize)
+        {
+            Items = items;
+            PageNumber = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalCount = count;
+        }
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+
+        public static async Task<PaginatedList<T>> CreateAsync(
+            IReadOnlyCollection<T> items,
+            int count,
+            int pageNumber, 
+            int pageSize)
+        {
+            return new PaginatedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
