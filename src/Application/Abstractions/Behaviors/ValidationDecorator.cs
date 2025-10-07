2affb4ce7a240c59f54a28f13dd806ebeee6fbdf
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-07 05:58:38 -0300

    Add QueryHandler class to ValidationDecorator

--- src/Application/Abstractions/Behaviors/ValidationDecorator.cs
+++ src/Application/Abstractions/Behaviors/ValidationDecorator.cs
@@ -45,6 +45,25 @@ namespace Application.Abstractions.Behaviors
             }
         }
 
+        internal sealed class QueryHandler<TQuery, TResponse>(
+            IQueryHandler<TQuery, TResponse> innerHandler,
+            IEnumerable<IValidator<TQuery>> validators)
+            : IQueryHandler<TQuery, TResponse>
+            where TQuery : IQuery<TResponse>
+        {
+            public async Task<Result<TResponse>> Handle(TQuery query, CancellationToken cancellationToken)
+            {
+                ValidationFailure[] validationFailures = await ValidateAsync(query, validators);
+
+                if (validationFailures.Length == 0)
+                {
+                    return await innerHandler.Handle(query, cancellationToken);
+                }
+
+                return Result.Failure<TResponse>(CreateValidationError(validationFailures));
+            }
+        }
+
         private static async Task<ValidationFailure[]> ValidateAsync<TCommand>(
             TCommand command,
             IEnumerable<IValidator<TCommand>> validators)
