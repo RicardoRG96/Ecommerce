a2e9a326a4b8fbce9fab1ba57b910fd14f6a5b36
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-09 06:04:12 -0300

    Add CountryResponse to GetById use case

---
+++ src/Application/Users/Country/GetById/CountryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Users.Country.GetById
+{
+    public sealed class CountryResponse
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
