727ba867e360ed893247492c398f6da66fd4c9b1
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-09 06:32:22 -0300

    Add GetCountryByNameQueryHandler to GetByName use case

---
+++ src/Application/Users/Countries/GetByName/GetCountryByNameQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Countries.GetByName
+{
+    public sealed record GetCountryByNameQuery(string Name) : IQuery<CountryResponse>;
+}
--- src/Application/Users/Countries/GetByName/GetCountryByNameQueryHandler.cs
+++ src/Application/Users/Countries/GetByName/GetCountryByNameQueryHandler.cs
@@ -1,6 +1,36 @@
-﻿using Application.Abstractions.Messaging;
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Application.Abstractions.Messaging;
+using Domain.Entities.Users;
+using Domain.Errors.Users;
+using SharedKernel;
 
 namespace Application.Users.Countries.GetByName
 {
-    public sealed record GetCountryByNameQueryHandler(string Name) : IQuery<CountryResponse>;
+    internal sealed class GetCountryByNameQueryHandler : IQueryHandler<GetCountryByNameQuery, CountryResponse>
+    {
+        private readonly ICountryRepository _countryRepository;
+
+        public GetCountryByNameQueryHandler(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        public async Task<Result<CountryResponse>> Handle(GetCountryByNameQuery query, CancellationToken cancellationToken)
+        {
+            Country? country = await _countryRepository.GetByNameAsync(query.Name, cancellationToken);
+
+            if (country is null)
+            {
+                return Result.Failure<CountryResponse>(CountryErrors.NotFoundByName);
+            }
+
+            CountryResponse response = new()
+            {
+                Id = country.CountryId,
+                Name = country.Name
+            };
+
+            return Result.Success(response);
+        }
+    }
 }
