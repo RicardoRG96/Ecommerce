66342d1fb454c316e8663fd0a9a3d26d1484a734
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-12 09:11:31 -0300

    Add GetRegionsWithPaginationQuery

---
+++ src/Application/Users/Regions/GetWithPagination/RegionResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Users.Regions.GetWithPagination
+{
+    public sealed class RegionResponse
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
