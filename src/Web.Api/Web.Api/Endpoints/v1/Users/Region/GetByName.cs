5aa5838ae1eca80716b7ce69c9ec49c32eb09161
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-13 06:37:12 -0300

    Add GetByName region endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Region/GetByName.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.Regions.GetByName;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Users.Region
+{
+    public class GetByName : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("regions/name/{regionName}", async (
+                string regionName,
+                IQueryHandler<GetRegionByNameQuery, RegionResponse> handler,
+                CancellationToken cancellationToken) =>
+            {
+                GetRegionByNameQuery query = new(regionName);
+
+                Result<RegionResponse> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .WithTags(Tags.Regions);
+        }
+    }
+}
