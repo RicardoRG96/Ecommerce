86e4b75d4f5657e303c9f15a445a887f9cb843ba
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-15 06:32:19 -0300

    Add GetMunicipalityByNameQuery

---
+++ src/Application/Users/Municipalities/GetByName/GetMunicipalityByNameQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Municipalities.GetByName
+{
+    public sealed record GetMunicipalityByNameQuery(string Name) : IQuery<MunicipalityResponse>;
+}
