cf8ac9f3d6e12970920cabecc68ed8e00ae0c80e
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-15 06:16:27 -0300

    Add MunicipalityResponse to GetById municipality use case

---
+++ src/Application/Users/Municipalities/GetById/MunicipalityResponse.cs
@@ -0,0 +1,11 @@
+﻿using Application.Users.Regions.GetById;
+
+namespace Application.Users.Municipalities.GetById
+{
+    public sealed class MunicipalityResponse
+    {
+        public long Id { get; set; }
+        public RegionResponse? Region { get; set; }
+        public string Name { get; set; }
+    }
+}
