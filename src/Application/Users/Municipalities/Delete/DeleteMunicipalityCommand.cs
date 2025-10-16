28dca1bc5773bb1cf172d6a812a0b87f5b6a042a
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-16 06:17:43 -0300

    Add DeleteMunicipalityCommand

---
+++ src/Application/Users/Municipalities/Delete/DeleteMunicipalityCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Municipalities.Delete
+{
+    public sealed record DeleteMunicipalityCommand(long MunicipalityId) : ICommand;
+}
