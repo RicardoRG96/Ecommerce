abacfab9a26168c5fba25102fd125701ee334a0d
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-16 06:37:25 -0300

    Add GetById municipality endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Municipality/GetById.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.Municipalities.GetById;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Users.Municipality
+{
+    public class GetById : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("municipalities/{municipalityId}", async (
+                long municipalityId,
+                IQueryHandler<GetMunicipalityByIdQuery, MunicipalityResponse> handler,
+                CancellationToken cancellationToken) =>
+            {
+                GetMunicipalityByIdQuery query = new(municipalityId);
+
+                Result<MunicipalityResponse> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .WithTags(Tags.Municipalitites);
+        }
+    }
+}
