11059ec07d8597ea6ffb6a296eadfdf03ba1e7dd
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-16 06:25:55 -0300

    Add DeleteMunicipalityCommandValidator

---
+++ src/Application/Users/Municipalities/Delete/DeleteMunicipalityCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Municipalities.Delete
+{
+    public sealed class DeleteMunicipalityCommandValidator : AbstractValidator<DeleteMunicipalityCommand>
+    {
+        public DeleteMunicipalityCommandValidator()
+        {
+            RuleFor(c => c.MunicipalityId).NotEmpty();
+        }
+    }
+}
