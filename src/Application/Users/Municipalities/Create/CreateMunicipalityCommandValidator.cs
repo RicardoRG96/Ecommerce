25a05325cc1015c694ae0b3fcdf5277b86d84f9e
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-16 06:02:19 -0300

    Add CreateMunicipalityCommandValidator

---
+++ src/Application/Users/Municipalities/Create/CreateMunicipalityCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Municipalities.Create
+{
+    public sealed class CreateMunicipalityCommandValidator : AbstractValidator<CreateMunicipalityCommand>
+    {
+        public CreateMunicipalityCommandValidator()
+        {
+            RuleFor(c => c.RegionId).NotEmpty();
+            RuleFor(c => c.Name).NotEmpty().MaximumLength(70);
+        }
+    }
+}
