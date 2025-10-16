eb46f0bb19b87d2b0e1ff9a19ed9af9e5057e1ba
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-16 06:04:07 -0300

    Add UpdateMunicipalityCommand

---
+++ src/Application/Users/Municipalities/Update/UpdateMunicipalityCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Municipalities.Update
+{
+    public sealed class UpdateMunicipalityCommand : ICommand
+    {
+        public long RegionId { get; set; }
+        public string Name { get; set; }
+    }
+}
