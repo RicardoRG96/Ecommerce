2206c7ef9fbb73d1c89edb5e32f3bdde4da78078
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-25 09:03:38 -0300

    refactor to MunicipalityConfiguration

--- src/Infrastructure/Persistence/Database/Configurations/MunicipalityConfiguration.cs
+++ src/Infrastructure/Persistence/Database/Configurations/MunicipalityConfiguration.cs
@@ -22,12 +22,12 @@ namespace Infrastructure.Persistence.Database.Configurations
             // Relationships
             builder.HasMany(m => m.Addresses)
                 .WithOne(a => a.Municipality)
-                .HasForeignKey(a => a.MunicipalityId)
+                .HasForeignKey(m => m.AddressId)
                 .OnDelete(DeleteBehavior.Cascade);
 
             builder.HasOne(m => m.Region)
                 .WithMany(r => r.Municipalities)
-                .HasForeignKey(r => r.MunicipalityId)
+                .HasForeignKey(m => m.RegionId)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
