abc684e5db5ee3d5ef2cf41c475e4a374eb0677c
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-26 09:35:04 -0300

    Refactor to GetAddressByIdQueryHandler

--- src/Application/Users/Addresses/GetById/GetAddressByIdQueryHandler.cs
+++ src/Application/Users/Addresses/GetById/GetAddressByIdQueryHandler.cs
@@ -1,8 +1,5 @@
 ﻿using Application.Abstractions.Data.Repositories.Users;
 using Application.Abstractions.Messaging;
-using Application.Users.Countries.GetById;
-using Application.Users.Municipalities.GetById;
-using Application.Users.Regions.GetById;
 using Domain.Entities.Users;
 using Domain.Errors.Users;
 using SharedKernel;
@@ -27,46 +24,9 @@ namespace Application.Users.Addresses.GetById
                 return Result.Failure<AddressResponse>(AddressErrors.NotFound(query.AddressId));
             }
 
-            AddressResponse response = MapToAddressResponse(address);
+            AddressResponse response = AddressToAddressResponseMapper.Map(address);
 
             return Result.Success(response);
-        }
-
-        private AddressResponse MapToAddressResponse(Address address)
-        {
-            CountryResponse countryResponse = new()
-            {
-                Id = address.CountryId,
-                Name = address.Country.Name!
-            };
-
-            RegionResponse regionResponse = new()
-            {
-                Id = address.Municipality.Region.RegionId,
-                Name = address.Municipality.Region.Name!
-            };
-
-            MunicipalityResponse municipalityResponse = new()
-            {
-                Id = address.Municipality.MunicipalityId,
-                Region = regionResponse,
-                Name = address.Municipality.Name!
-            };
-
-            AddressResponse response = new()
-            {
-                Id = address.AddressId,
-                Country = countryResponse,
-                Municipality = municipalityResponse,
-                Title = address.Title!,
-                Street = address.Street!,
-                Number = address.Number!,
-                Apartament = address.Apartament!,
-                Reference = address.Reference!,
-                PostalCode = address.PostalCode!
-            };
-
-            return response;
         }
     }
 }
