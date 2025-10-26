f5e4036e171d4648490d31f0add0b3ed5594343f
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-26 08:47:59 -0300

    Add GetAddressByTitleQuery

---
+++ src/Application/Users/Addresses/GetByTitle/GetAddressByTitleQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Addresses.GetByTitle
+{
+    public sealed record GetAddressByTitleQuery(string title) : IQuery<AddressResponse>;
+}
