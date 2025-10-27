f615171563166f53e89e81e8d7ab630303508d33
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-27 07:49:49 -0300

    Add CreateAddressCommandHandler

---
+++ src/Application/Users/Addresses/Create/CreateAddressCommand.cs
@@ -0,0 +1,18 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Addresses.Create
+{
+    public sealed class CreateAddressCommand : ICommand<long>
+    {
+        public long CountryId { get; set; }
+        public long MunicipalityId { get; set; }
+        public string Title { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string? Apartament { get; set; }
+        public string? Reference { get; set; }
+        public string? PostalCode { get; set; }
+
+    }
+}
---
+++ src/Application/Users/Addresses/Create/CreateAddressCommandHandler.cs
@@ -0,0 +1,65 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Abstractions.Messaging;
+using Domain.Entities.Users;
+using Domain.Errors.Users;
+using SharedKernel;
+
+namespace Application.Users.Addresses.Create
+{
+    internal sealed class CreateAddressCommandHandler : ICommandHandler<CreateAddressCommand, long>
+    {
+        private readonly IAddressRepository _addressRepository;
+        private readonly ICountryRepository _countryRepository;
+        private readonly IMunicipalityRepository _municipalityRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateAddressCommandHandler(
+            IAddressRepository addressRepository,
+            ICountryRepository countryRepository,
+            IMunicipalityRepository municipalityRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _addressRepository = addressRepository;
+            _countryRepository = countryRepository;
+            _municipalityRepository = municipalityRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<long>> Handle(CreateAddressCommand command, CancellationToken cancellationToken)
+        {
+            Country? country = await _countryRepository.GetByIdAsync(command.CountryId, cancellationToken);
+
+            if (country is null)
+            {
+                return Result.Failure<long>(CountryErrors.NotFound(command.CountryId));
+            }
+
+            Municipality? municipality = await _municipalityRepository.GetByIdAsync(command.MunicipalityId, cancellationToken);
+
+            if (municipality is null)
+            {
+                return Result.Failure<long>(MunicipalityErrors.NotFound(command.MunicipalityId));
+            }
+
+            Address address = new()
+            {
+                Country = country,
+                Municipality = municipality,
+                Title = command.Title,
+                City = command.City,
+                Street = command.Street,
+                Number = command.Number,
+                Apartament = command.Apartament,
+                Reference = command.Reference,
+                PostalCode = command.PostalCode
+            };
+
+            await _addressRepository.AddAsync(address, cancellationToken);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success(address.AddressId);
+        }
+    }
+}
