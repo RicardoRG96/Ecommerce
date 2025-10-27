6ff3633addb13b8580ec4c27d5ecd157cce1e247
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-27 07:59:19 -0300

    Add UpdateAddressCommand

---
+++ src/Application/Users/Addresses/Update/UpdateAddressCommand.cs
@@ -0,0 +1,16 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Addresses.Update
+{
+    public sealed class UpdateAddressCommand : ICommand
+    {
+        public long MunicipalityId { get; set; }
+        public string Title { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string? Apartament { get; set; }
+        public string? Reference { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
