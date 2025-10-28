8ab1231c24e5f2a902ebc49f7676ef13ae1937ed
RicardoRG96 <ricardoretamal10@gmail.com>
2025-10-28 07:33:10 -0300

    Add Delete Address endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Address/Delete.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.Addresses.Delete;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Users.Address
+{
+    internal sealed class Delete : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPut("addresses/{addressId}", async (
+                long addressId,
+                ICommandHandler<DeleteAddressCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                DeleteAddressCommand command = new(addressId);
+
+                Result result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .WithTags(Tags.Addresses);
+        }
+    }
+}
