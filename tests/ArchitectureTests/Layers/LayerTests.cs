08a3263ca44e0bb4f8655d71a0b58dfeeb534986
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-05 07:24:44 -0300

    Add test method to LayerTests

---
+++ tests/ArchitectureTests/Layers/LayerTests.cs
@@ -0,0 +1,19 @@
+﻿using NetArchTest.Rules;
+using Shouldly;
+
+namespace ArchitectureTests.Layers
+{
+    public class LayerTests : BaseTest
+    {
+        [Fact]
+        public void DomaninLayer_Should_NotHaveDependencyOnApplication()
+        {
+            TestResult result = Types.InAssembly(DomainAssembly)
+                .Should()
+                .NotHaveDependencyOn("Application")
+                .GetResult();
+
+            result.IsSuccessful.ShouldBeTrue();
+        }
+    }
+}
