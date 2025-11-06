efd08e2f6a0ec09d01d2ef959c12d4bec88878ad
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-06 07:56:10 -0300

    Add test method to DeleteRegionCommandTests

---
+++ tests/Application.UnitTests/Users/Regions/Delete/DeleteRegionCommandTests.cs
@@ -0,0 +1,42 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Users.Regions.Delete;
+using Domain.Errors.Users;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using SharedKernel;
+
+namespace Application.UnitTests.Users.Regions.Delete
+{
+    public class DeleteRegionCommandTests
+    {
+        private static readonly DeleteRegionCommand _command = new(1L);
+        private readonly DeleteRegionCommandHandler _handler;
+        private readonly IRegionRepository _regionRepositoryMock;
+        private readonly IUnitOfWork _unitOfWorkMock;
+
+        public DeleteRegionCommandTests()
+        {
+            _regionRepositoryMock = Substitute.For<IRegionRepository>();
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+
+            _handler = new DeleteRegionCommandHandler(_regionRepositoryMock, _unitOfWorkMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenRegionDoesNotExist()
+        {
+            long notExistingRegionId = 2500L;
+            DeleteRegionCommand invalidCommand = _command with { RegionId = notExistingRegionId };
+
+            _regionRepositoryMock.GetByIdAsync(Arg.Is<long>(id => id == notExistingRegionId), Arg.Any<CancellationToken>())
+                .ReturnsNull();
+
+            Result result = await _handler.Handle(invalidCommand, default);
+
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().Be(RegionErrors.NotFound(notExistingRegionId));
+        }
+    }
+}
