8923771dc941bd4be6551c8434c5947fc519d002
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-06 07:23:22 -0300

    Add test method to CreateRegionCommandTests

---
+++ tests/Application.UnitTests/Users/Regions/Create/CreateRegionCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Users.Regions.Create;
+using FluentAssertions;
+using NSubstitute;
+using SharedKernel;
+
+namespace Application.UnitTests.Users.Regions.Create
+{
+    public class CreateRegionCommandTests
+    {
+        private static readonly CreateRegionCommand _command = new("testRegion");
+        private readonly CreateRegionCommandHandler _handler;
+        private readonly IRegionRepository _regionRepositoryMock;
+        private readonly IUnitOfWork _unitOfWorkMock;
+
+        public CreateRegionCommandTests()
+        {
+            _regionRepositoryMock = Substitute.For<IRegionRepository>();
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+
+            _handler = new CreateRegionCommandHandler(_regionRepositoryMock, _unitOfWorkMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnSuccess_WhenNameIsPresent()
+        {
+            Result<long> result = await _handler.Handle(_command, default);
+
+            result.IsSuccess.Should().BeTrue();
+        }
+    }
+}
