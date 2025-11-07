bfb3f31d030d402fbb4975ed265880a79cdc2e77
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-07 08:00:08 -0300

    Add test method to GetRegionsWithPaginationQueryTests

--- tests/Application.UnitTests/Users/Regions/GetWithPagination/GetRegionsWithPaginationQueryTests.cs
+++ tests/Application.UnitTests/Users/Regions/GetWithPagination/GetRegionsWithPaginationQueryTests.cs
@@ -51,5 +51,20 @@ namespace Application.UnitTests.Users.Regions.GetWithPagination
 
             result.Value.Items.Should().BeEmpty();
         }
+
+        [Fact]
+        public async Task Handle_Should_ReturnAListWithElements_WhenThereAreRegions()
+        {
+            _regionRepositoryMock
+                .GetAllAsync(
+                    Arg.Is<int>(pn => pn == _query.PageNumber),
+                    Arg.Is<int>(ps => ps == _query.PageSize),
+                    Arg.Any<CancellationToken>())
+                .Returns(_regions);
+
+            Result<PaginatedList<RegionResponse>> result = await _handler.Handle(_query, default);
+
+            result.Value.Items.Count.Should().Be(_items!.Count);
+        }
     }
 }
