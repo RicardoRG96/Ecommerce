dcc6d1f0bd618642b381cb9c59617570121d5021
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-07 06:32:21 -0300

    Add tests method to UpdateRegionCommandTests

--- src/Application/Users/Regions/Update/UpdateRegionCommandHandler.cs
+++ src/Application/Users/Regions/Update/UpdateRegionCommandHandler.cs
@@ -24,7 +24,7 @@ namespace Application.Users.Regions.Update
 
             if (region is null)
             {
-                return Result.Failure(UserErrors.NotFound(command.RegionId));
+                return Result.Failure(RegionErrors.NotFound(command.RegionId));
             }
 
             region.Name = command.Name;
