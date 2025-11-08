c7e01318f956b5ab75e1545bf8529bc780710468
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-08 07:39:04 -0300

    Add test method to GetCountryByNameQueryTests

---
+++ tests/Application.UnitTests/Users/Countries/GetByName/GetCountryByNameQueryTests.cs
@@ -0,0 +1,44 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Application.Users.Countries.GetByName;
+using Domain.Entities.Users;
+using Domain.Errors.Users;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using SharedKernel;
+
+namespace Application.UnitTests.Users.Countries.GetByName
+{
+    public class GetCountryByNameQueryTests
+    {
+        private static readonly GetCountryByNameQuery _query = new("TestCountry");
+        private readonly Country _country;
+        private readonly GetCountryByNameQueryHandler _handler;
+        private readonly ICountryRepository _countryRepositoryMock;
+
+        public GetCountryByNameQueryTests()
+        {
+            _countryRepositoryMock = Substitute.For<ICountryRepository>();
+
+            _country = new() { CountryId = 1L, Name = _query.Name };
+            _handler = new(_countryRepositoryMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenCountryDoesNotExist()
+        {
+            string notExistingCountryName = "invalidQuery";
+            GetCountryByNameQuery invalidQuery = _query with { Name = notExistingCountryName };
+
+            _countryRepositoryMock
+                .GetByNameAsync(Arg.Is<string>(name => name == invalidQuery.Name), Arg.Any<CancellationToken>())
+                .ReturnsNull();
+
+            Result<CountryResponse> result = await _handler.Handle(invalidQuery, default);
+
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(CountryErrors.NotFoundByName);
+        }
+    }
+}
