8ac034048c9a7eee25249cd3ba9b9915611855a2
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-08 07:08:56 -0300

    Add test method to UpdateCountryCommandTests

---
+++ tests/Application.UnitTests/Users/Countries/Update/UpdateCountryCommandTests.cs
@@ -0,0 +1,48 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Users.Countries.Delete;
+using Application.Users.Countries.Update;
+using Domain.Entities.Users;
+using Domain.Errors.Users;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using SharedKernel;
+
+namespace Application.UnitTests.Users.Countries.Update
+{
+    public class UpdateCountryCommandTests
+    {
+        private static readonly UpdateCountryCommand _command = new(1L, "UpdatedTestCountry");
+        private readonly Country _country;
+        private readonly UpdateCountryCommandHandler _handler;
+        private readonly ICountryRepository _countryRepositoryMock;
+        private readonly IUnitOfWork _unitOfWorkMock;
+
+        public UpdateCountryCommandTests()
+        {
+            _countryRepositoryMock = Substitute.For<ICountryRepository>();
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+
+            _country = new() { CountryId = _command.Id, Name = "TestCountry" };
+            _handler = new(_countryRepositoryMock, _unitOfWorkMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenCountryDoesNotExist()
+        {
+            long notExistingCountryId = 2500L;
+            UpdateCountryCommand invalidCommand = _command with { Id  = notExistingCountryId };
+
+            _countryRepositoryMock
+                .GetByIdAsync(Arg.Is<long>(id => id == invalidCommand.Id), Arg.Any<CancellationToken>())
+                .ReturnsNull();
+
+            Result result = await _handler.Handle(invalidCommand, default);
+
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(CountryErrors.NotFound(invalidCommand.Id));
+        }
+    }
+}
