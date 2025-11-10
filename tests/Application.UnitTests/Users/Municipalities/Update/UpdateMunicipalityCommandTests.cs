8b6be39702af7c93500d6de16c0bff8991aa6de1
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-10 09:07:19 -0300

    Add test method to UpdateMunicipalityCommandTests

--- tests/Application.UnitTests/Users/Municipalities/Update/UpdateMunicipalityCommandTests.cs
+++ tests/Application.UnitTests/Users/Municipalities/Update/UpdateMunicipalityCommandTests.cs
@@ -93,5 +93,23 @@ namespace Application.UnitTests.Users.Municipalities.Update
             result.IsFailure.Should().BeFalse();
             _municipality.Name.Should().Be(_command.Name);
         }
+
+        [Fact]
+        public async Task Handle_Should_CallRepository_WhenMunicipalityIdAndRegionIdExists()
+        {
+            _municipalityRepositoryMock
+                .GetByIdAsync(Arg.Is<long>(id => id == _command.MunicipalityId), Arg.Any<CancellationToken>())
+                .Returns(_municipality);
+
+            _regionRepositoryMock
+                .GetByIdAsync(Arg.Is<long>(id => id == _command.RegionId), Arg.Any<CancellationToken>())
+                .Returns(_region);
+
+            await _handler.Handle(_command, default);
+
+            _municipalityRepositoryMock
+                .Received(1)
+                .Update(_municipality);
+        }
     }
 }
