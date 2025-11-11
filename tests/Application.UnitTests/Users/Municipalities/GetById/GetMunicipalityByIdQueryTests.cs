d5cd2df551cf7da864aba3ba07828e5f9436e7f9
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-11 07:24:11 -0300

    Add test method to GetMunicipalityByIdQueryTests

---
+++ tests/Application.UnitTests/Users/Municipalities/GetById/GetMunicipalityByIdQueryTests.cs
@@ -0,0 +1,44 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Application.Users.Municipalities.GetById;
+using Domain.Entities.Users;
+using Domain.Errors.Users;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using SharedKernel;
+
+namespace Application.UnitTests.Users.Municipalities.GetById
+{
+    public class GetMunicipalityByIdQueryTests
+    {
+        private static readonly GetMunicipalityByIdQuery _query = new(1);
+        private readonly Municipality _municipality;
+        private readonly GetMunicipalityByIdQueryHandler _handler;
+        private readonly IMunicipalityRepository _municipalityRepositoryMock;
+
+        public GetMunicipalityByIdQueryTests()
+        {
+            _municipalityRepositoryMock = Substitute.For<IMunicipalityRepository>();
+
+            _municipality = new() { MunicipalityId = _query.MunicipalityId, Name = "TestName" };
+            _handler = new(_municipalityRepositoryMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenMunicipalityDoesNotExist()
+        {
+            long notExistingMunicipalityId = 2500;
+            GetMunicipalityByIdQuery invalidQuery = _query with { MunicipalityId = notExistingMunicipalityId };
+
+            _municipalityRepositoryMock
+                .GetByIdAsync(Arg.Is<long>(id => id == invalidQuery.MunicipalityId), Arg.Any<CancellationToken>())
+                .ReturnsNull();
+
+            Result result = await _handler.Handle(invalidQuery, default);
+
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(MunicipalityErrors.NotFound(invalidQuery.MunicipalityId));
+        }
+    }
+}
