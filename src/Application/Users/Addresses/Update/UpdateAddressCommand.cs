c6bbd36dd2465bad481990719b501dd9a959155b
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-12 07:25:12 -0300

    Add test method to UpdateAddressCommandTests

--- src/Application/Users/Addresses/Update/UpdateAddressCommand.cs
+++ src/Application/Users/Addresses/Update/UpdateAddressCommand.cs
@@ -2,16 +2,14 @@
 
 namespace Application.Users.Addresses.Update
 {
-    public sealed class UpdateAddressCommand : ICommand
-    {
-        public long AddressId { get; set; }
-        public long MunicipalityId { get; set; }
-        public string Title { get; set; }
-        public string City { get; set; }
-        public string Street { get; set; }
-        public string Number { get; set; }
-        public string? Apartament { get; set; }
-        public string? Reference { get; set; }
-        public string PostalCode { get; set; }
-    }
+    public sealed record UpdateAddressCommand(
+        long AddressId,
+        long MunicipalityId,
+        string Title,
+        string City,
+        string Street,
+        string Number,
+        string Apartament,
+        string Reference,
+        string PostalCode) : ICommand;
 }
---
+++ tests/Application.UnitTests/Users/Addresses/Update/UpdateAddressCommandTests.cs
@@ -0,0 +1,61 @@
+﻿using Application.Abstractions.Data.Repositories.Users;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Users.Addresses.Update;
+using Domain.Entities.Users;
+using Domain.Errors.Users;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using SharedKernel;
+
+namespace Application.UnitTests.Users.Addresses.Update
+{
+    public class UpdateAddressCommandTests
+    {
+        private static readonly UpdateAddressCommand _command =
+            new(1, 1, "UpdatedAddress", "UpdatedCity", "UpdatedStreet", "UpdatedNumber", "UpdatedApartament", "UpdatedReference", "UpdatedPostalCode");
+        private readonly Address _address;
+        private readonly Country _country;
+        private readonly Municipality _municipality;
+        private readonly UpdateAddressCommandHandler _handler;
+        private readonly IAddressRepository _addressRepositoryMock;
+        private readonly IMunicipalityRepository _municipalityRepositoryMock;
+        private readonly IUnitOfWork _unitOfWorkMock;
+
+        public UpdateAddressCommandTests()
+        {
+            _addressRepositoryMock = Substitute.For<IAddressRepository>();
+            _municipalityRepositoryMock = Substitute.For<IMunicipalityRepository>();
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+
+            _address = new()
+            {
+                AddressId = _command.AddressId,
+                CountryId = 1,
+                MunicipalityId = _command.MunicipalityId,
+                Title = "TestAddress",
+                City = "TestCity"
+            };
+            _country = new() { CountryId = 1, Name = "TestCountry" };
+            _municipality = new() { MunicipalityId = _command.MunicipalityId, RegionId = 1, Name = "TestMuni" };
+            _handler = new(_addressRepositoryMock, _municipalityRepositoryMock, _unitOfWorkMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenMunicipalityDoesNotExist()
+        {
+            long notExistingMunicipalityId = 2500;
+            UpdateAddressCommand invalidCommand = _command with { MunicipalityId =  notExistingMunicipalityId };
+
+            _addressRepositoryMock
+                .GetByIdAsync(Arg.Is<long>(id => id == invalidCommand.MunicipalityId), Arg.Any<CancellationToken>())
+                .ReturnsNull();
+
+            Result result = await _handler.Handle(invalidCommand, default);
+
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(AddressErrors.NotFound(invalidCommand.AddressId));
+        }
+    }
+}
