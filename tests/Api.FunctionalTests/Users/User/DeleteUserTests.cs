048b80ef242b461b8ac0d2748227a970385e4c9e
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-14 07:52:45 -0300

    Add DeleteUserTests

---
+++ tests/Api.FunctionalTests/Users/User/DeleteUserTests.cs
@@ -0,0 +1,23 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Api.FunctionalTests.Users.User
+{
+    public class DeleteUserTests : BaseFunctionalTest
+    {
+        public DeleteUserTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenUserIdIsMissing()
+        {
+            HttpResponseMessage response = await HttpClient.DeleteAsync("api/v1/users/");
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
