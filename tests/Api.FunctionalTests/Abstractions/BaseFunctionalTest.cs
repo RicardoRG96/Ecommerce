dea66bb4aaba0f178edc36aa02f893bac0546fce
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-14 07:22:20 -0300

    Add BaseFunctionalTest

---
+++ tests/Api.FunctionalTests/Abstractions/BaseFunctionalTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.FunctionalTests.Abstractions
+{
+    public class BaseFunctionalTest : IClassFixture<FunctionalTestWebAppFactory>
+    {
+        public BaseFunctionalTest(FunctionalTestWebAppFactory factory)
+        {
+            HttpClient = factory.CreateClient();
+        }
+
+        protected HttpClient HttpClient { get; init; }
+    }
+}
