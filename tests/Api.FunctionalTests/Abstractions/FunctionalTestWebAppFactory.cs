f90f49361b54b0e4f13a10e7893413e2b1d7fdd4
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-14 07:16:58 -0300

    Add FunctionalTestWebAppFactory

---
+++ tests/Api.FunctionalTests/Abstractions/FunctionalTestWebAppFactory.cs
@@ -0,0 +1,48 @@
+﻿using DotNet.Testcontainers.Builders;
+using Infrastructure.Persistence.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Testcontainers.MsSql;
+using Web.Api;
+
+namespace Api.FunctionalTests.Abstractions
+{
+    public class FunctionalTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private const ushort MsSqlPort = 1443;
+
+        private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+            .WithPassword("P@ssw0rd123")
+            .WithPortBinding(MsSqlPort)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilInternalTcpPortIsAvailable(MsSqlPort))
+            .Build();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer(_dbContainer.GetConnectionString());
+                });
+            });
+        }
+
+        public Task InitializeAsync()
+        {
+            return _dbContainer.StartAsync();
+        }
+
+        public new Task DisposeAsync()
+        {
+            return _dbContainer.StopAsync();
+        }
+    }
+}
