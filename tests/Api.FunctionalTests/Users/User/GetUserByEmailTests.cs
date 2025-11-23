04a2be6465db3fe9c4b969b0162ef3ffdca54a18
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-23 07:58:02 -0300

    Add test Should_ReturnNotFound_WhenEmailDoesNotExist

---
+++ tests/Api.FunctionalTests/Users/User/GetUserByEmailTests.cs
@@ -0,0 +1,24 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+
+namespace Api.FunctionalTests.Users.User
+{
+    public class GetUserByEmailTests : BaseFunctionalTest
+    {
+        public GetUserByEmailTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnNotFound_WhenEmailDoesNotExist()
+        {
+            string notExistingEmail = "no_email@example.com";
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{usersBaseUrl}/email/{notExistingEmail}");
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
