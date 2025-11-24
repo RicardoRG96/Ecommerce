5c17a9a2db0597d0ab1a8646c4b99abb2adb3ce6
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-24 07:33:36 -0300

    Add test Should_ReturnBadRequest_WhenCountryNameExceedsTheMaximumLength

--- tests/Api.FunctionalTests/Users/Country/UpdateCountryTests.cs
+++ tests/Api.FunctionalTests/Users/Country/UpdateCountryTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using System.Net;
 using System.Net.Http.Json;
+using Web.Api.Endpoints.v1.Users.Country.Create;
 using Web.Api.Endpoints.v1.Users.Country.Update;
 
 namespace Api.FunctionalTests.Users.Country
@@ -27,6 +28,17 @@ namespace Api.FunctionalTests.Users.Country
         public async Task Should_ReturnBadRequest_WhenCountryNameIsMissing()
         {
             UpdateCountryRequest invalidRequest = _request with { Name = "" };
+
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/1", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenCountryNameExceedsTheMaximumLength()
+        {
+            UpdateCountryRequest invalidRequest =
+                _request with { Name = "La República Federal del Crisantemo Esmeralda de los Montes Orientales del Viento" };
 
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/1", invalidRequest);
 
