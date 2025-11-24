d4ff88f0ad4996b1d48a1b00621b6b6cd7da859f
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-24 07:30:50 -0300

    Add test Should_ReturnBadRequest_WhenCountryNameIsMissing

--- tests/Api.FunctionalTests/Users/Country/UpdateCountryTests.cs
+++ tests/Api.FunctionalTests/Users/Country/UpdateCountryTests.cs
@@ -22,5 +22,15 @@ namespace Api.FunctionalTests.Users.Country
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenCountryNameIsMissing()
+        {
+            UpdateCountryRequest invalidRequest = _request with { Name = "" };
+
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/1", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
