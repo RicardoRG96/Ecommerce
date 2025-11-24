47f9628d597876afe0fe5c02c73d93c2a6c74550
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-24 08:03:34 -0300

    Add GetCountryByIdTests

---
+++ tests/Api.FunctionalTests/Users/Country/GetCountryByIdTests.cs
@@ -0,0 +1,32 @@
+﻿using Api.FunctionalTests.Abstractions;
+using Application.Users.Countries.GetById;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Api.FunctionalTests.Users.Country
+{
+    public class GetCountryByIdTests : BaseFunctionalTest
+    {
+        public GetCountryByIdTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnNotFound_WhenCountryIdDoesNotExist()
+        {
+            HttpResponseMessage response = await HttpClient.GetAsync($"{countriesBaseUrl}/2500");
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Should_ReturnOk_AndCountry_WhenCountryExists()
+        {
+            CountryResponse? country = await HttpClient.GetFromJsonAsync<CountryResponse>($"{countriesBaseUrl}/1");
+
+            country.Should().NotBeNull();
+        }
+    }
+}
