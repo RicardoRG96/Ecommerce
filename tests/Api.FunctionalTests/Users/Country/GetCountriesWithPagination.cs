1e5abbf1b0138fd9fad31883e1b25cdb6e5a70bd
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-25 06:23:07 -0300

    Add test Should_ReturnBadRequest_WhenPageNumberIsLessThan_1

---
+++ tests/Api.FunctionalTests/Users/Country/GetCountriesWithPagination.cs
@@ -0,0 +1,22 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+
+namespace Api.FunctionalTests.Users.Country
+{
+    public class GetCountriesWithPagination : BaseFunctionalTest
+    {
+        public GetCountriesWithPagination(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenPageNumberIsLessThan_1()
+        {
+            HttpResponseMessage response = await HttpClient.GetAsync($"{countriesBaseUrl}?pageNumber=0&pageSize=10");
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
