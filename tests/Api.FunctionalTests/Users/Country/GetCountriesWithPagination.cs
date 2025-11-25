2edd7bc7f4fb291d03ea5f096c67475274b78eae
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-25 06:33:42 -0300

    Add test Should_ReturnOK_And_Countries_WhenPageNumber_And_PageSize_Values_AreCorrect

--- tests/Api.FunctionalTests/Users/Country/GetCountriesWithPagination.cs
+++ tests/Api.FunctionalTests/Users/Country/GetCountriesWithPagination.cs
@@ -1,6 +1,9 @@
 ﻿using Api.FunctionalTests.Abstractions;
+using Application.Users.Countries.GetWithPagination;
 using FluentAssertions;
+using SharedKernel;
 using System.Net;
+using System.Net.Http.Json;
 
 namespace Api.FunctionalTests.Users.Country
 {
@@ -33,6 +36,19 @@ namespace Api.FunctionalTests.Users.Country
             HttpResponseMessage response = await HttpClient.GetAsync($"{countriesBaseUrl}?pageNumber=1&pageSize=101");
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Should_ReturnOK_And_Countries_WhenPageNumber_And_PageSize_Values_AreCorrect()
+        {
+            PaginatedList<CountryResponse>? countries =
+                await HttpClient.GetFromJsonAsync<PaginatedList<CountryResponse>>($"{countriesBaseUrl}?pageNumber=1&pageSize=10");
+
+            countries.Should().NotBeNull();
+            countries.Items.Count.Should().Be(2);
+            countries.TotalPages.Should().Be(1);
+            countries.HasNextPage.Should().BeFalse();
+            countries.HasPreviousPage.Should().BeFalse();
         }
     }
 }
