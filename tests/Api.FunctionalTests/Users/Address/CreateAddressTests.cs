07cf4eb5b08e8cc79a393103d779fdaf2d45ccfb
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-26 07:23:18 -0300

    Add test Should_ReturnBadRequest_WhenMunicipalityIdIsMissing

--- tests/Api.FunctionalTests/Users/Address/CreateAddressTests.cs
+++ tests/Api.FunctionalTests/Users/Address/CreateAddressTests.cs
@@ -25,5 +25,15 @@ namespace Api.FunctionalTests.Users.Address
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenMunicipalityIdIsMissing()
+        {
+            CreateAddressRequest invalidRequest = _request with { MunicipalityId = 0 };
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync(addressesBaseUrl, invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
