0adde656e87a67bddce99f1ff2bb7b9ce063b686
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-26 07:53:33 -0300

    Add test Should_ReturnBadRequest_WhenAddressIdIsMissing

---
+++ tests/Api.FunctionalTests/Users/Address/UpdateAddressTests.cs
@@ -0,0 +1,35 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using Web.Api.Endpoints.v1.Users.Address.Create;
+using Web.Api.Endpoints.v1.Users.Address.Update;
+
+namespace Api.FunctionalTests.Users.Address
+{
+    public class UpdateAddressTests : BaseFunctionalTest
+    {
+        private static readonly UpdateAddressRequest _request = new(
+            1, 
+            "UpdatedTitle", 
+            "UpdatedCity", 
+            "UpdatedStreet", 
+            "UpdatedNumber", 
+            "UpdatedApartament", 
+            "UpdatedReference", 
+            "UpdatedPostalCode");
+
+        public UpdateAddressTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenAddressIdIsMissing()
+        {
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{addressesBaseUrl}/0", _request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
