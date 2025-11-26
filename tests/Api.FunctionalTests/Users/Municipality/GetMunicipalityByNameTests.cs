cc4f2cf1909e6c0872912d7d272c454e55206c58
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-26 07:03:22 -0300

    Add GetMunicipalityByNameTests

---
+++ tests/Api.FunctionalTests/Users/Municipality/GetMunicipalityByNameTests.cs
@@ -0,0 +1,33 @@
+﻿using Api.FunctionalTests.Abstractions;
+using Application.Users.Municipalities.GetByName;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Api.FunctionalTests.Users.Municipality
+{
+    public class GetMunicipalityByNameTests : BaseFunctionalTest
+    {
+        public GetMunicipalityByNameTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnNotFound_WhenMunicipalityNameDoesNotExist()
+        {
+            HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}/name/noMuni");
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Should_ReturnOk_And_Municipality_WhenMunicipalityNameExists()
+        {
+            MunicipalityResponse? municipality =
+                await HttpClient.GetFromJsonAsync<MunicipalityResponse>($"{municipalitiesBaseUrl}/name/Santiago");
+
+            municipality.Should().NotBeNull();
+        }
+    }
+}
