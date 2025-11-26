451a06980a41e392e00783853fc1b4eae6bcbff3
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-26 07:59:26 -0300

    Add test Should_ReturnBadRequest_WhenAddressCityIsMissing

--- tests/Api.FunctionalTests/Users/Address/UpdateAddressTests.cs
+++ tests/Api.FunctionalTests/Users/Address/UpdateAddressTests.cs
@@ -62,5 +62,15 @@ namespace Api.FunctionalTests.Users.Address
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenAddressCityIsMissing()
+        {
+            UpdateAddressRequest invalidRequest = _request with { City = "" };
+
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{addressesBaseUrl}/1", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
