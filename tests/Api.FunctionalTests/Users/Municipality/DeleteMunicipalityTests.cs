2a562fc06ac24643df8b5a76a4e6148ac2621c31
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-26 06:47:41 -0300

    Add test Should_ReturnBadRequest_WhenMunicipalityIdIsMissing

---
+++ tests/Api.FunctionalTests/Users/Municipality/DeleteMunicipalityTests.cs
@@ -0,0 +1,22 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+
+namespace Api.FunctionalTests.Users.Municipality
+{
+    public class DeleteMunicipalityTests : BaseFunctionalTest
+    {
+        public DeleteMunicipalityTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenMunicipalityIdIsMissing()
+        {
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{municipalitiesBaseUrl}/0");
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
