b18e921fefe5581de44ef2fea1c8075e5d93ca08
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-26 07:28:36 -0300

    Add test Should_ReturnBadRequest_WhenAddressCityExceedsTheMaximumLength

--- tests/Api.FunctionalTests/Users/Address/CreateAddressTests.cs
+++ tests/Api.FunctionalTests/Users/Address/CreateAddressTests.cs
@@ -66,5 +66,16 @@ namespace Api.FunctionalTests.Users.Address
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenAddressCityExceedsTheMaximumLength()
+        {
+            CreateAddressRequest invalidRequest =
+                _request with { City = "La República Federal del Crisantemo Esmeralda de los Montes Orientales del Viento" };
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync(addressesBaseUrl, invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
