627e87de45e21abb24c65919d9e598a860595f77
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-26 07:40:30 -0300

    Add test Should_ReturnBadRequest_WhenAddressPostalCodeExceedsTheMaximumLength

--- tests/Api.FunctionalTests/Users/Address/CreateAddressTests.cs
+++ tests/Api.FunctionalTests/Users/Address/CreateAddressTests.cs
@@ -151,5 +151,16 @@ namespace Api.FunctionalTests.Users.Address
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenAddressPostalCodeExceedsTheMaximumLength()
+        {
+            CreateAddressRequest invalidRequest =
+                _request with { PostalCode = "La República Federal del Crisantemo Esmeralda de los Montes Orientales del Viento" };
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync(addressesBaseUrl, invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
