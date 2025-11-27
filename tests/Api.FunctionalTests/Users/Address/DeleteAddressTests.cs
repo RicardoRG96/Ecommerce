1de9fe51114db4271d600bd44fe8737e1ad8db0c
RicardoRG96 <ricardoretamal10@gmail.com>
2025-11-27 06:47:47 -0300

    Add test Should_ReturnBadRequest_WhenAddressIdIsMissing

---
+++ tests/Api.FunctionalTests/Users/Address/DeleteAddressTests.cs
@@ -0,0 +1,22 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+
+namespace Api.FunctionalTests.Users.Address
+{
+    public class DeleteAddressTests : BaseFunctionalTest
+    {
+        public DeleteAddressTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenAddressIdIsMissing()
+        {
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{addressesBaseUrl}/0");
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
