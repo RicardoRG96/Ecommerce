5056d69617287fd41ff9e3b712f87bd35cb6ba1d
RicardoRG96 <ricardoretamal10@gmail.com>
2025-12-03 07:04:29 -0300

    Add IDomainUser

---
+++ src/Domain/Entities/Users/IDomainUser.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities.Users
+{
+    public interface IDomainUser
+    {
+        public string? Avatar { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public IList<Address> Addresses { get; set; }
+        public bool HasLegalAge() => (DateTime.UtcNow - DateOfBirth).TotalDays / 365.25 >= 18; // Legal age in Chile is 18
+    }
+}
