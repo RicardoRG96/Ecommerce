9a4a51e18718b5a9a485ccc5cb3a0bc4c7702a99
RicardoRG96 <ricardoretamal10@gmail.com>
2025-12-03 07:19:30 -0300

    Add ApplicationUser

--- src/Domain/Entities/Users/IDomainUser.cs
+++ src/Domain/Entities/Users/IDomainUser.cs
@@ -2,16 +2,16 @@
 {
     public interface IDomainUser
     {
-        public long UserId { get; set; }
+        public long Id { get; set; }
         public string? Avatar { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? Username { get; set; }
+        public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? PasswordHash { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string? PhoneNumber { get; set; }
         public IList<Address> Addresses { get; set; }
-        public bool HasLegalAge() => (DateTime.UtcNow - DateOfBirth).TotalDays / 365.25 >= 18; // Legal age in Chile is 18
+        public bool HasLegalAge();
     }
 }
---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<long>, IDomainUser
+    {
+        public string? Avatar { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public IList<Address> Addresses { get; set; } = new List<Address>();
+        public bool HasLegalAge() => (DateTime.UtcNow - DateOfBirth).TotalDays / 365.25 >= 18; // Legal age in Chile is 18
+    }
+}
