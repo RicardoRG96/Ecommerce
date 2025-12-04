506b55577bbf4fc7f03d2994c252ee49445ea18f
RicardoRG96 <ricardoretamal10@gmail.com>
2025-12-04 07:38:10 -0300

    Add IIdentityService

---
+++ src/Application/Abstractions/Common/IIdentityService.cs
@@ -0,0 +1,19 @@
+﻿using SharedKernel;
+
+namespace Application.Abstractions.Common
+{
+    public interface IIdentityService
+    {
+        Task<string?> GetUserNameAsync(string userId);
+
+        Task<bool> IsInRoleAsync(string userId, string role);
+
+        Task<bool> AuthorizeAsync(string userId, string policyName);
+
+        Task<Result<long>> CreateUserAsync(string userName, string email, string password);
+
+        Task<Result> LoginUserAsync(string email, string password, bool rememberMe);
+
+        Task<Result> DeleteUserAsync(string userId);
+    }
+}
