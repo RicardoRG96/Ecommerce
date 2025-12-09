0a8f24638f2af6a93e64e43d2172c58f686a5dbb
RicardoRG96 <ricardoretamal10@gmail.com>
2025-12-09 08:00:35 -0300

    Add UserContext

--- src/Infrastructure/Authentication/UserContext.cs
+++ src/Infrastructure/Authentication/UserContext.cs
@@ -1,6 +1,22 @@
-﻿namespace Infrastructure.Authentication
+﻿using Application.Abstractions.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Authentication
 {
-    internal class UserContext
+    internal sealed class UserContext : IUserContext
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserContext(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public long UserId =>
+            _httpContextAccessor
+                .HttpContext?
+                .User
+                .GetUserId() ??
+            throw new ApplicationException("User context is unavailable");
     }
 }
