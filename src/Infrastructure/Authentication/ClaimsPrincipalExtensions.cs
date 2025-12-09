47d79294892fa5fbf0a809bd27a67bb6f61e6a07
RicardoRG96 <ricardoretamal10@gmail.com>
2025-12-09 07:55:01 -0300

    Add ClaimsPrincipalExtensions

---
+++ src/Infrastructure/Authentication/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace Infrastructure.Authentication
+{
+    internal static class ClaimsPrincipalExtensions
+    {
+        public static long GetUserId(this ClaimsPrincipal? principal)
+        {
+            string? userId = principal?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return long.TryParse(userId, out long parsedUserId) ?
+                parsedUserId :
+                throw new ApplicationException("Use id is unavailable");
+        }
+    }
+}
