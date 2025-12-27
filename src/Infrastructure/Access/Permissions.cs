473605a6528656ececdf2a4eeec988ad38c2bc9a
RicardoRG96 <ricardoretamal10@gmail.com>
2025-12-27 07:46:22 -0300

    Add Permissions class to Infrastructure project

---
+++ src/Infrastructure/Access/Permissions.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Access
+{
+    public static class Permissions
+    {
+        public const string UsersRead = "user:read";
+        public const string UsersUpdate = "user:update";
+        public const string UsersDelete = "user:delete";
+    }
+}
