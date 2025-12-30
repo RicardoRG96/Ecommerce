62fbb8961bbbabb001091399588020f09886edaa
RicardoRG96 <ricardoretamal10@gmail.com>
2025-12-30 06:30:37 -0300

    Add RefreshToken entity

---
+++ src/Domain/Entities/Users/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities.Users
+{
+    public sealed class RefreshToken
+    {
+        public long Id { get; set; }
+        public string Token { get; set; }
+        public long UserId { get; set; }
+        public DateTime ExpiresOnUtc { get; set; }
+        public IDomainUser User { get; set; }
+    }
+}
