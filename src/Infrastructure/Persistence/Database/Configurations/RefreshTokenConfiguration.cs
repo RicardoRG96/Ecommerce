79f114b20b2b6388fe70fc427433323510074f70
RicardoRG96 <ricardoretamal10@gmail.com>
2025-12-30 06:34:50 -0300

    Add RefreshTokenConfiguration

---
+++ src/Infrastructure/Persistence/Database/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Database.Configurations
+{
+    internal sealed class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.Token)
+                .HasMaxLength(200);
+
+            builder.HasIndex(r => r.Token)
+                .IsUnique();
+
+            builder.HasOne(r => r.User)
+                .WithMany()
+                .HasForeignKey(r => r.User.Id);
+        }
+    }
+}
