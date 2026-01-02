1c4a90c76aefae2756282f3bb862763c9d894daa
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-02 05:51:48 -0300

    Add CreateUserCommand

---
+++ src/Application/Users/Users/Create/CreateUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Users.Create
+{
+    public sealed record CreateUserCommand(
+        string Email, 
+        string Password) : ICommand<long>;
+}
