1ec831ba1eb285eae7c04ee30eb1af779fb333e0
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-02 05:55:11 -0300

    Add CreateUserCommandValidator

---
+++ src/Application/Users/Users/Create/CreateUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Users.Create
+{
+    internal sealed class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(c => c.Email).NotEmpty().EmailAddress().MaximumLength(150);
+            RuleFor(c => c.Password).NotEmpty().MaximumLength(150);
+        }
+    }
+}
