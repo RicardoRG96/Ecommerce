98c42d027c7ba37bb586f619cea61a9e09326e54
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-03 07:40:10 -0300

    Add LoginWithRefreshTokenCommandValidator

---
+++ src/Application/Users/RefreshTokens/Login/LoginWithRefreshTokenCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Users.RefreshTokens.Login
+{
+    internal sealed class LoginWithRefreshTokenCommandValidator : AbstractValidator<LoginWithRefreshTokenCommand>
+    {
+        public LoginWithRefreshTokenCommandValidator()
+        {
+            RuleFor(c => c.RefreshToken).NotEmpty();
+        }
+    }
+}
