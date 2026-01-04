78a9244358dbcac6d58f4fd8d0b2f77db832f375
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-04 08:13:02 -0300

    Add Login user endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/User/Login/Login.cs
@@ -0,0 +1,28 @@
+﻿
+using Application.Abstractions.Messaging;
+using Application.Users.Users.Login;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Users.User.Login
+{
+    internal sealed class Login : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("users/login", async (
+                LoginUserRequest request,
+                ICommandHandler<LoginUserCommand, Dictionary<string, string>> handler,
+                CancellationToken cancellationToken) =>
+            {
+                LoginUserCommand command = new(request.Email, request.Password);
+
+                Result<Dictionary<string, string>> result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .WithTags(Tags.Users);
+        }
+    }
+}
