9bf6961a805bada8980e3041261c566eb9c7adec
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-06 16:58:42 -0300

    Refactor to DeleteUserCommandTests

--- tests/Application.UnitTests/Users/Users/Delete/DeleteUserCommandTests.cs
+++ tests/Application.UnitTests/Users/Users/Delete/DeleteUserCommandTests.cs
@@ -1,4 +1,4 @@
-﻿using Application.Abstractions.Data.Repositories.Users;
+﻿using Application.Abstractions.Common;
 using Application.Abstractions.Data.UnitOfWork;
 using Application.Users.Users.Delete;
 using Domain.Entities.Users;
@@ -16,16 +16,16 @@ namespace Application.UnitTests.Users.Users.Delete
         private static readonly DeleteUserCommand _command = new(1L);
         private readonly IDomainUser _user;
         private readonly DeleteUserCommandHandler _handler;
-        private readonly IUserRepository _userRepositoryMock;
+        private readonly IIdentityService _identityService;
         private readonly IUnitOfWork _unitOfWorkMock;
 
         public DeleteUserCommandTests()
         {
-            _userRepositoryMock = Substitute.For<IUserRepository>();
+            _identityService = Substitute.For<IIdentityService>();
             _unitOfWorkMock = Substitute.For<IUnitOfWork>();
 
             _user = new ApplicationUser { Id = _command.UserId, FirstName = "Test", LastName = "User" };
-            _handler = new(_userRepositoryMock, _unitOfWorkMock);
+            _handler = new(_identityService, _unitOfWorkMock);
         }
 
         [Fact]
@@ -34,8 +34,8 @@ namespace Application.UnitTests.Users.Users.Delete
             long notExistingUserId = 2500L;
             DeleteUserCommand invalidCommand = _command with { UserId = notExistingUserId };
 
-            _userRepositoryMock
-                .GetByIdAsync(Arg.Is<long>(id => id == invalidCommand.UserId), Arg.Any<CancellationToken>())
+            _identityService
+                .GetUserByIdAsync(Arg.Is<long>(id => id == invalidCommand.UserId), Arg.Any<CancellationToken>())
                 .ReturnsNull();
 
             Result result = await _handler.Handle(invalidCommand, default);
@@ -48,8 +48,8 @@ namespace Application.UnitTests.Users.Users.Delete
         [Fact]
         public async Task Handle_Should_ReturnSuccess_WhenUserExists()
         {
-            _userRepositoryMock
-                .GetByIdAsync(Arg.Is<long>(id => id == _command.UserId), Arg.Any<CancellationToken>())
+            _identityService
+                .GetUserByIdAsync(Arg.Is<long>(id => id == _command.UserId), Arg.Any<CancellationToken>())
                 .Returns(_user);
 
             Result result = await _handler.Handle(_command, default);
@@ -59,24 +59,24 @@ namespace Application.UnitTests.Users.Users.Delete
         }
 
         [Fact]
-        public async Task Handle_Should_CallRepository_WhenUserExists()
+        public async Task Handle_Should_CallIdentityService_WhenUserExists()
         {
-            _userRepositoryMock
-                .GetByIdAsync(Arg.Is<long>(id => id == _command.UserId), Arg.Any<CancellationToken>())
+            _identityService
+                .GetUserByIdAsync(Arg.Is<long>(id => id == _command.UserId), Arg.Any<CancellationToken>())
                 .Returns(_user);
 
             await _handler.Handle(_command, default);
 
-            _userRepositoryMock
+            await _identityService
                 .Received(1)
-                .Delete(_user);
+                .DeleteUserAsync(_user.Id);
         }
 
         [Fact]
         public async Task Handle_Should_CallUnitOfWork_WhenUserExists()
         {
-            _userRepositoryMock
-                .GetByIdAsync(Arg.Is<long>(id => id == _command.UserId), Arg.Any<CancellationToken>())
+            _identityService
+                .GetUserByIdAsync(Arg.Is<long>(id => id == _command.UserId), Arg.Any<CancellationToken>())
                 .Returns(_user);
 
             await _handler.Handle(_command, default);
