eb5029d7b7bd75c98de9425b9fa13d88079004c0
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-08 17:34:07 -0300

    Add HasPermission to Create Country endpoint

--- src/Web.Api/Web.Api/Endpoints/v1/Users/Country/Create/Create.cs
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Country/Create/Create.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Abstractions.Messaging;
 using Application.Users.Countries.Create;
+using Infrastructure.Access;
 using SharedKernel;
 using Web.Api.Extensions;
 using Web.Api.Infrastructure;
@@ -21,6 +22,7 @@ namespace Web.Api.Endpoints.v1.Users.Country.Create
 
                 return result.Match(Results.Ok, CustomResults.Problem);
             })
+            .HasPermission(Permissions.Countries.Create)
             .WithTags(Tags.Countries);
         }
     }
