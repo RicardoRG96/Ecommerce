22dfb64e1368b0a2a7299bb94f6332a01bf5b7c2
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-08 15:24:47 -0300

    Refactor to GetAddressByIdQueryHandler

--- src/Application/Users/Addresses/GetById/GetAddressByIdQueryHandler.cs
+++ src/Application/Users/Addresses/GetById/GetAddressByIdQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using Application.Abstractions.Data.Repositories.Users;
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Data.Repositories.Users;
 using Application.Abstractions.Messaging;
 using Domain.Entities.Users;
 using Domain.Errors.Users;
@@ -9,10 +10,14 @@ namespace Application.Users.Addresses.GetById
     internal sealed class GetAddressByIdQueryHandler : IQueryHandler<GetAddressByIdQuery, AddressResponse>
     {
         private readonly IAddressRepository _addressRepository;
+        private readonly IUserContext _userContext;
 
-        public GetAddressByIdQueryHandler(IAddressRepository addressRepository)
+        public GetAddressByIdQueryHandler(
+            IAddressRepository addressRepository,
+            IUserContext userContext)
         {
             _addressRepository = addressRepository;
+            _userContext = userContext;
         }
 
         public async Task<Result<AddressResponse>> Handle(GetAddressByIdQuery query, CancellationToken cancellationToken)
@@ -22,6 +27,13 @@ namespace Application.Users.Addresses.GetById
             if (address is null)
             {
                 return Result.Failure<AddressResponse>(AddressErrors.NotFound(query.AddressId));
+            }
+
+            long userId = address.AddressUsers.Select(au => au.ApplicationUserId).FirstOrDefault();
+
+            if (userId != _userContext.UserId)
+            {
+                return Result.Failure<AddressResponse>(UserErrors.Unauthorized());
             }
 
             AddressResponse response = AddressToAddressResponseMapper.Map(address);
