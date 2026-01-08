307f2027e71fba3cf52dcb22191366cd83ce7387
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-08 16:04:23 -0300

    Refactor to AddressToAddressResponseMapper

--- src/Application/Users/Addresses/AddressToAddressResponseMapper.cs
+++ src/Application/Users/Addresses/AddressToAddressResponseMapper.cs
@@ -7,6 +7,44 @@ namespace Application.Users.Addresses
 {
     internal static class AddressToAddressResponseMapper
     {
+        public static AddressResponse Map(Address address)
+        {
+            CountryResponse countryResponse = new()
+            {
+                Id = address.CountryId,
+                Name = address.Country.Name!
+            };
+
+            RegionResponse regionResponse = new()
+            {
+                Id = address.Municipality.Region.RegionId,
+                Name = address.Municipality.Region.Name!
+            };
+
+            MunicipalityResponse municipalityResponse = new()
+            {
+                Id = address.Municipality.MunicipalityId,
+                Region = regionResponse,
+                Name = address.Municipality.Name!
+            };
+
+            AddressResponse response = new()
+            {
+                Id = address.AddressId,
+                Country = countryResponse,
+                Municipality = municipalityResponse,
+                Title = address.Title!,
+                City = address.City!,
+                Street = address.Street!,
+                Number = address.Number!,
+                Apartament = address.Apartament!,
+                Reference = address.Reference!,
+                PostalCode = address.PostalCode!
+            };
+
+            return response;
+        }
+
         public static AddressResponse Map(AddressUser addressUser)
         {
             CountryResponse countryResponse = new()
