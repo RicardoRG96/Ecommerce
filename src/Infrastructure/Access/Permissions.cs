25b160c52ecd90fe9d57628b5ea54d1ae7a83f67
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-08 09:44:01 -0300

    Add permissions

--- src/Infrastructure/Access/Permissions.cs
+++ src/Infrastructure/Access/Permissions.cs
@@ -2,9 +2,46 @@
 {
     public static class Permissions
     {
-        public const string UsersRead = "user:read";
-        public const string UsersUpdate = "user:update";
-        public const string UsersDelete = "user:delete";
-        public const string UsersCreate = "user:create";
+        public static class Users
+        {
+            public const string Read = "user:read";
+            public const string Update = "user:update";
+            public const string Delete = "user:delete";
+            public const string Create = "user:create";
+            public const string AssignRole = "user:assignRole";
+            public const string AssignPermission = "user:assignPermission";
+        }
+
+        public static class Addresses
+        {
+            public const string Read = "address:read";
+            public const string Update = "address:update";
+            public const string Delete = "address:delete";
+            public const string Create = "address:create";
+        }
+
+        public static class Countries
+        {
+            public const string Read = "country:read";
+            public const string Update = "country:update";
+            public const string Delete = "country:delete";
+            public const string Create = "country:create";
+        }
+
+        public static class Regions
+        {
+            public const string Read = "region:read";
+            public const string Update = "region:update";
+            public const string Delete = "region:delete";
+            public const string Create = "region:create";
+        }
+
+        public static class Municipalities
+        {
+            public const string Read = "municipality:read";
+            public const string Update = "municipality:update";
+            public const string Delete = "municipality:delete";
+            public const string Create = "remunicipalitygion:create";
+        }
     }
 }
