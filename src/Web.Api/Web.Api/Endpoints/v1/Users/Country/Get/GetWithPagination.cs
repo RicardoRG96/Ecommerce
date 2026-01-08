53b8cc42d70ee1fdb70b31366de13add52a66d57
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-08 17:35:45 -0300

    Add HasPermission to Get Country endpoints

--- src/Web.Api/Web.Api/Endpoints/v1/Users/Country/Get/GetWithPagination.cs
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Country/Get/GetWithPagination.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Abstractions.Messaging;
 using Application.Users.Countries.GetWithPagination;
+using Infrastructure.Access;
 using SharedKernel;
 using Web.Api.Extensions;
 using Web.Api.Infrastructure;
@@ -22,6 +23,7 @@ namespace Web.Api.Endpoints.v1.Users.Country.Get
 
                 return result.Match(Results.Ok, CustomResults.Problem);
             })
+            .HasPermission(Permissions.Countries.Read)
             .WithTags(Tags.Countries);
         }
     }
