59636eb8ee70764d86e6bcaba224942357e2b094
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-08 17:40:55 -0300

    Add HasPermission to Create Region endpoint

--- src/Web.Api/Web.Api/Endpoints/v1/Users/Region/Create/Create.cs
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Region/Create/Create.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Abstractions.Messaging;
 using Application.Users.Regions.Create;
+using Infrastructure.Access;
 using SharedKernel;
 using Web.Api.Extensions;
 using Web.Api.Infrastructure;
@@ -21,6 +22,7 @@ namespace Web.Api.Endpoints.v1.Users.Region.Create
 
                 return result.Match(Results.Ok, CustomResults.Problem);
             })
+            .HasPermission(Permissions.Regions.Create)
             .WithTags(Tags.Regions);
         }
     }
