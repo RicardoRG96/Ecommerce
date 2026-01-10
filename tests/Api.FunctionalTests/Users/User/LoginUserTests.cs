1792c734d57908d8f9d72fc055e9dd62f285863d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-10 06:56:38 -0300

    Add test Should_ReturnBadRequest_WhenEmailExceedsTheMaximumLength

--- tests/Api.FunctionalTests/Users/User/LoginUserTests.cs
+++ tests/Api.FunctionalTests/Users/User/LoginUserTests.cs
@@ -3,6 +3,7 @@ using Api.FunctionalTests.Common;
 using FluentAssertions;
 using System.Net;
 using System.Net.Http.Json;
+using Web.Api.Endpoints.v1.Users.User.Create;
 using Web.Api.Endpoints.v1.Users.User.Login;
 
 namespace Api.FunctionalTests.Users.User
@@ -27,9 +28,20 @@ namespace Api.FunctionalTests.Users.User
         }
 
         [Fact]
-        public async Task Should_Return_BadRequest_WhenEmailIsInvalid()
+        public async Task Should_ReturnBadRequest_WhenEmailIsInvalid()
         {
             LoginUserRequest invalidRequest = _request with { Email = Constants.InvalidEmail };
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync($"{usersBaseUrl}/login", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenEmailExceedsTheMaximumLength()
+        {
+            LoginUserRequest invalidRequest =
+                _request with { Email = Constants.ExceededMaximumLengthEmail };
 
             HttpResponseMessage response = await HttpClient.PostAsJsonAsync($"{usersBaseUrl}/login", invalidRequest);
 
