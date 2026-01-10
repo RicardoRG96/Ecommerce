d5de49abc81c0556745f22f407ea6ff766a8e439
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-10 08:43:00 -0300

    Add test Should_ReturnBadRequest_WhenRefreshTokenIsEmpty

--- tests/Api.FunctionalTests/Users/RefreshToken/LoginWithRefreshTokenTests.cs
+++ tests/Api.FunctionalTests/Users/RefreshToken/LoginWithRefreshTokenTests.cs
@@ -1,14 +1,28 @@
 ﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using Web.Api.Endpoints.v1.Users.RefreshToken.Login;
 
 namespace Api.FunctionalTests.Users.RefreshToken
 {
     public class LoginWithRefreshTokenTests : BaseFunctionalTest
     {
+        private static readonly LoginWithRefreshTokenRequest _request = new("refreshToken", 1);
+
         public LoginWithRefreshTokenTests(FunctionalTestWebAppFactory factory) 
             : base(factory)
         {
         }
 
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenRefreshTokenIsEmpty()
+        {
+            LoginWithRefreshTokenRequest invalidRequest = _request with { RefreshToken = "" };
 
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync($"{usersBaseUrl}/refresh-tokens", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
