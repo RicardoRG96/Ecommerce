b6893d6d574b8159f94744ee3ca8313ab6b5914d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-10 07:17:23 -0300

    Add test Should_ReturnBadRequest_WhenPasswordDoesNotMatch

--- tests/Api.FunctionalTests/Users/User/LoginUserTests.cs
+++ tests/Api.FunctionalTests/Users/User/LoginUserTests.cs
@@ -10,7 +10,10 @@ namespace Api.FunctionalTests.Users.User
 {
     public class LoginUserTests : BaseFunctionalTest
     {
-        private static readonly LoginUserRequest _request = new("test@example.com", "Test1234");
+        private static readonly CreateUserRequest _createUserRequest =
+                new("", "TestName", "TestLastName", "Test", "test@example.com", "Test1234", new DateTime(2000, 10, 10), "+56923147859");
+
+        private static readonly LoginUserRequest _request = new(_createUserRequest.Email, _createUserRequest.Password);
 
         public LoginUserTests(FunctionalTestWebAppFactory factory) 
             : base(factory)
@@ -73,6 +76,18 @@ namespace Api.FunctionalTests.Users.User
         public async Task Should_ReturnNotFound_WhenUserDoesNotExist()
         {
             LoginUserRequest invalidRequest = _request with { Email = Constants.NotExistingEmail };
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync($"{usersBaseUrl}/login", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenPasswordDoesNotMatch()
+        {
+            await HttpClient.PostAsJsonAsync(usersBaseUrl, _request);
+
+            LoginUserRequest invalidRequest = _request with { Password = "notMatching123" };
 
             HttpResponseMessage response = await HttpClient.PostAsJsonAsync($"{usersBaseUrl}/login", invalidRequest);
 
