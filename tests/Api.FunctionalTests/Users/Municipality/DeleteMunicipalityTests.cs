470bae4538bf3f8d4912d213801b945479a39c12
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-12 11:34:37 -0300

    Refactor to DeleteMunicipalityTests

--- tests/Api.FunctionalTests/Users/Municipality/DeleteMunicipalityTests.cs
+++ tests/Api.FunctionalTests/Users/Municipality/DeleteMunicipalityTests.cs
@@ -1,4 +1,5 @@
 ﻿using Api.FunctionalTests.Abstractions;
+using Api.FunctionalTests.Common;
 using FluentAssertions;
 using System.Net;
 
@@ -22,7 +23,7 @@ namespace Api.FunctionalTests.Users.Municipality
         [Fact]
         public async Task Should_ReturnNotFound_WhenMunicipalityIdDoesNotExist()
         {
-            HttpResponseMessage response = await HttpClient.DeleteAsync($"{municipalitiesBaseUrl}/2500");
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{municipalitiesBaseUrl}/{Constants.NotExistingId}");
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
         }
