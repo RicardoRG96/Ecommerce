a8dfc1f8987190891743034f48f2b64f44862e53
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-12 06:12:42 -0300

    Add AssingRolesToUserCommand

---
+++ src/Application/Users/Roles/Assign/AssingRolesToUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Roles.Assign
+{
+    public sealed record AssingRolesToUserCommand(
+        List<Role> Roles,
+        long UserId) : ICommand;
+}
