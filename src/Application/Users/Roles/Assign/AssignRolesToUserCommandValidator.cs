28aa02a4d048078617b2c4f713fd8344cb6185cf
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-12 06:17:08 -0300

    Add AssignRolesToUserCommandValidator

---
+++ src/Application/Users/Roles/Assign/AssignRolesToUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Roles.Assign
+{
+    internal sealed class AssignRolesToUserCommandValidator : AbstractValidator<AssingRolesToUserCommand>
+    {
+        public AssignRolesToUserCommandValidator()
+        {
+            RuleFor(c => c.Roles).NotEmpty();
+            RuleFor(c => c.UserId).NotEmpty();
+        }
+    }
+}
