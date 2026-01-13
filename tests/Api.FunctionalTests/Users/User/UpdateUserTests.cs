f061f3276dcb6743e2f8d5f33ef0c1996b50b2b3
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 15:03:56 -0300

    Refactor to UpdateUserTests

--- tests/Api.FunctionalTests/Users/User/UpdateUserTests.cs
+++ tests/Api.FunctionalTests/Users/User/UpdateUserTests.cs
@@ -3,6 +3,7 @@ using Api.FunctionalTests.Common;
 using Application.Users.Users.GetById;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using Web.Api.Endpoints.v1.Users.User.Update;
 
@@ -20,6 +21,8 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_ReturnBadRequest_WhenUserIdIsMissing()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/0", _request);
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
@@ -28,6 +31,8 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_ReturnBadRequest_WhenFirstNameIsMissing()
         {
+            SetAdminAuthentication();
+
             UpdateUserRequest invalidRequest = _request with { FirstName = "" };
 
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/1", invalidRequest);
@@ -38,6 +43,8 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_ReturnBadRequest_WhenLastNameIsMissing()
         {
+            SetAdminAuthentication();
+
             UpdateUserRequest invalidRequest = _request with { LastName = "" };
 
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/1", invalidRequest);
@@ -48,6 +55,8 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_ReturnBadRequest_WhenPhoneNumberIsMissing()
         {
+            SetAdminAuthentication();
+
             UpdateUserRequest invalidRequest = _request with { PhoneNumber = "" };
 
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/1", invalidRequest);
@@ -58,6 +67,8 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_ReturnNotFound_WhenUserIdDoesNotExist()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/{Constants.NotExistingId}", _request);
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
@@ -66,6 +77,8 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_ReturnNoContent_WhenUserIdExists()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/1", _request);
 
             response.StatusCode.Should().Be(HttpStatusCode.NoContent);
@@ -74,11 +87,34 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_UpdateLastName_WhenUserIdExistsAndRequestIsValid()
         {
+            SetAdminAuthentication();
+
             await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/1", _request);
 
             UserResponse? user = await HttpClient.GetFromJsonAsync<UserResponse>($"{usersBaseUrl}/1");
 
             user!.LastName.Should().Be(_request.LastName);
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/1", _request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            SetCustomerUserAuthentication();
+
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/1", _request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
