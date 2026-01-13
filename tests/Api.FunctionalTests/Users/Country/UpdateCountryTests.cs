7f15fb525422909e3e9523a792e1aa4db0ad10f8
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 13:41:51 -0300

    Refactor to UpdateCountryTests

--- tests/Api.FunctionalTests/Users/Country/UpdateCountryTests.cs
+++ tests/Api.FunctionalTests/Users/Country/UpdateCountryTests.cs
@@ -3,6 +3,7 @@ using Api.FunctionalTests.Common;
 using Application.Users.Countries.GetById;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using Web.Api.Endpoints.v1.Users.Country.Update;
 
@@ -20,6 +21,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnBadRequest_WhenCountryIdIsMissing()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/0", _request);
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
@@ -28,6 +31,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnBadRequest_WhenCountryNameIsMissing()
         {
+            SetAdminAuthentication();
+
             UpdateCountryRequest invalidRequest = _request with { Name = "" };
 
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/1", invalidRequest);
@@ -38,6 +43,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnBadRequest_WhenCountryNameExceedsTheMaximumLength()
         {
+            SetAdminAuthentication();
+
             UpdateCountryRequest invalidRequest =
                 _request with { Name = Constants.ExceededMaximumLengthField };
 
@@ -49,6 +56,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnNotFound_WhenCountryIdDoesNotExist()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/{Constants.NotExistingId}", _request);
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
@@ -57,6 +66,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnNoContent_WhenRequestIsValid_And_CountryIdExists()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/1", _request);
 
             response.StatusCode.Should().Be(HttpStatusCode.NoContent);
@@ -65,11 +76,34 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_UpdateName_WhenRequestIsValid_And_CountryIdExists()
         {
+            SetAdminAuthentication();
+
             await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/1", _request);
 
             CountryResponse? country = await HttpClient.GetFromJsonAsync<CountryResponse>($"{countriesBaseUrl}/1");
 
             country!.Name.Should().Be(_request.Name);
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/1", _request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            SetCustomerUserAuthentication();
+
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{countriesBaseUrl}/1", _request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
