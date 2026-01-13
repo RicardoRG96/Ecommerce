3edd3d641b21103f5a1d440d28ba3dadba40ee1f
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 13:57:45 -0300

    Refactor to GetMunicipalityByNameTests

--- tests/Api.FunctionalTests/Users/Municipality/GetMunicipalityByNameTests.cs
+++ tests/Api.FunctionalTests/Users/Municipality/GetMunicipalityByNameTests.cs
@@ -2,6 +2,7 @@
 using Application.Users.Municipalities.GetByName;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace Api.FunctionalTests.Users.Municipality
@@ -16,6 +17,8 @@ namespace Api.FunctionalTests.Users.Municipality
         [Fact]
         public async Task Should_ReturnNotFound_WhenMunicipalityNameDoesNotExist()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}/name/noMuni");
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
@@ -24,10 +27,33 @@ namespace Api.FunctionalTests.Users.Municipality
         [Fact]
         public async Task Should_ReturnOk_And_Municipality_WhenMunicipalityNameExists()
         {
+            SetAdminAuthentication();
+
             MunicipalityResponse? municipality =
                 await HttpClient.GetFromJsonAsync<MunicipalityResponse>($"{municipalitiesBaseUrl}/name/Santiago");
 
             municipality.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}/name/Santiago");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            SetCustomerUserAuthentication();
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}/name/Santiago");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
