2b72068bb306b6065ddf7d562d7d9ef2d4198286
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 13:53:28 -0300

    Refactor to GetMunicipalitiesWithPaginationTests

--- tests/Api.FunctionalTests/Users/Municipality/GetMunicipalitiesWithPaginationTests.cs
+++ tests/Api.FunctionalTests/Users/Municipality/GetMunicipalitiesWithPaginationTests.cs
@@ -3,6 +3,7 @@ using Application.Users.Municipalities.GetWithPagination;
 using FluentAssertions;
 using SharedKernel;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace Api.FunctionalTests.Users.Municipality
@@ -17,6 +18,8 @@ namespace Api.FunctionalTests.Users.Municipality
         [Fact]
         public async Task Should_ReturnBadRequest_WhenPageNumberIsLessThan_1()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}?pageNumber=0&pageSize=10");
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
@@ -25,6 +28,8 @@ namespace Api.FunctionalTests.Users.Municipality
         [Fact]
         public async Task Should_ReturnBadRequest_WhenPageSizeIsLessThan_1()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}?pageNumber=1&pageSize=0");
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
@@ -33,6 +38,8 @@ namespace Api.FunctionalTests.Users.Municipality
         [Fact]
         public async Task Should_ReturnBadRequest_WhenPageSizeIsGreaterThan_100()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}?pageNumber=1&pageSize=101");
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
@@ -41,6 +48,8 @@ namespace Api.FunctionalTests.Users.Municipality
         [Fact]
         public async Task Should_ReturnOK_And_Municipalities_WhenPageNumber_And_PageSize_Values_AreCorrect()
         {
+            SetAdminAuthentication();
+
             PaginatedList<MunicipalityResponse>? municipalities =
                 await HttpClient.GetFromJsonAsync<PaginatedList<MunicipalityResponse>>($"{municipalitiesBaseUrl}?pageNumber=1&pageSize=10");
 
@@ -49,6 +58,27 @@ namespace Api.FunctionalTests.Users.Municipality
             municipalities.TotalPages.Should().Be(3);
             municipalities.HasNextPage.Should().BeTrue();
             municipalities.HasPreviousPage.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}?pageNumber=1&pageSize=10");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            SetCustomerUserAuthentication();
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{municipalitiesBaseUrl}?pageNumber=1&pageSize=10");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
