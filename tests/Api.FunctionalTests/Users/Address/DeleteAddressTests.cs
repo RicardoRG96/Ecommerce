e9a724d6127d627317ff1badf36c1596e5bb23b3
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 10:32:37 -0300

    Refactor to DeleteAddressTests

--- tests/Api.FunctionalTests/Users/Address/DeleteAddressTests.cs
+++ tests/Api.FunctionalTests/Users/Address/DeleteAddressTests.cs
@@ -2,19 +2,34 @@
 using Api.FunctionalTests.Common;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Web.Api.Endpoints.v1.Users.Address.Create;
 
 namespace Api.FunctionalTests.Users.Address
 {
     public class DeleteAddressTests : BaseFunctionalTest
     {
-        public DeleteAddressTests(FunctionalTestWebAppFactory factory) 
+        public DeleteAddressTests(FunctionalTestWebAppFactory factory)
             : base(factory)
         {
+        }
+
+        private async Task<long> CreateAddressForAdminUser()
+        {
+           CreateAddressRequest request =
+                new(1, 1, "AdminAddress", "TestCity", "TestStreet", "1010", "TestApartament", "TestReference", "TestPostalCode");
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync(addressesBaseUrl, request);
+
+            return await response.Content.ReadFromJsonAsync<long>();
         }
 
         [Fact]
         public async Task Should_ReturnBadRequest_WhenAddressIdIsMissing()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.DeleteAsync($"{addressesBaseUrl}/0");
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
@@ -23,6 +38,8 @@ namespace Api.FunctionalTests.Users.Address
         [Fact]
         public async Task Should_ReturnNotFound_WhenAddressIsDoesNotExist()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.DeleteAsync($"{addressesBaseUrl}/{Constants.NotExistingId}");
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
@@ -31,9 +48,34 @@ namespace Api.FunctionalTests.Users.Address
         [Fact]
         public async Task Should_ReturnNoContent_WhenAddressIdExists()
         {
-            HttpResponseMessage response = await HttpClient.DeleteAsync($"{addressesBaseUrl}/1");
+            SetAdminAuthentication();
+
+            long createdAdderssId = await CreateAddressForAdminUser();
+
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{addressesBaseUrl}/{createdAdderssId}");
 
             response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{addressesBaseUrl}/1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnInternalServerError_WhenTheLoggedInUserId_DoesNotMatchTheOneSent()
+        {
+            SetAdminAuthentication();
+
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{addressesBaseUrl}/1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
         }
     }
 }
