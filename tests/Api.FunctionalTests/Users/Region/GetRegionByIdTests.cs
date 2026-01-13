041bc60224ae724a75bcc5a792b5815195522a0e
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 14:09:33 -0300

    Refactor to GetRegionByIdTests

--- tests/Api.FunctionalTests/Users/Region/GetRegionByIdTests.cs
+++ tests/Api.FunctionalTests/Users/Region/GetRegionByIdTests.cs
@@ -3,6 +3,7 @@ using Api.FunctionalTests.Common;
 using Application.Users.Regions.GetById;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace Api.FunctionalTests.Users.Region
@@ -16,6 +17,8 @@ namespace Api.FunctionalTests.Users.Region
         [Fact]
         public async Task Should_ReturnNotFound_WhenRegionIdDoesNotExist()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.GetAsync($"{regionsBaseUrl}/{Constants.NotExistingId}");
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
@@ -24,9 +27,32 @@ namespace Api.FunctionalTests.Users.Region
         [Fact]
         public async Task Should_ReturnOk_And_Region_WhenRegionIdExists()
         {
+            SetAdminAuthentication();
+
             RegionResponse? region = await HttpClient.GetFromJsonAsync<RegionResponse>($"{regionsBaseUrl}/1");
 
             region.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{regionsBaseUrl}/1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            SetCustomerUserAuthentication();
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{regionsBaseUrl}/1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
