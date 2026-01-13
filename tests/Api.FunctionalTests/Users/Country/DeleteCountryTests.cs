f41d1e4322319f67e6332f5b3d162204f648a8be
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 13:28:50 -0300

    Refactor to DeleteCountryTests

--- tests/Api.FunctionalTests/Users/Country/DeleteCountryTests.cs
+++ tests/Api.FunctionalTests/Users/Country/DeleteCountryTests.cs
@@ -2,6 +2,7 @@
 using Api.FunctionalTests.Common;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace Api.FunctionalTests.Users.Country
 {
@@ -15,6 +16,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnBadRequest_WhenCountryIdIsMissing()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.DeleteAsync($"{countriesBaseUrl}/0");
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
@@ -23,6 +26,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnNotFound_WhenCountryIdDoesNotExist()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.DeleteAsync($"{countriesBaseUrl}/{Constants.NotExistingId}");
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
@@ -31,9 +36,32 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnNoContent_WhenRequestIsValid()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.DeleteAsync($"{countriesBaseUrl}/2");
 
             response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{countriesBaseUrl}/2");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            SetCustomerUserAuthentication();
+
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{countriesBaseUrl}/2");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
