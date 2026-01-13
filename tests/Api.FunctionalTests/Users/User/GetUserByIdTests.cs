0a44414df0516412c5ac9f81f2b925771e4459a2
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 14:43:36 -0300

    Refactor to GetUserByIdTests

--- tests/Api.FunctionalTests/Users/User/GetUserByIdTests.cs
+++ tests/Api.FunctionalTests/Users/User/GetUserByIdTests.cs
@@ -3,6 +3,7 @@ using Api.FunctionalTests.Common;
 using Application.Users.Users.GetById;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace Api.FunctionalTests.Users.User
@@ -17,6 +18,8 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_ReturnNotFound_WhenUserIdDoesNotExist()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.GetAsync($"{usersBaseUrl}/{Constants.NotExistingId}");
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
@@ -25,9 +28,32 @@ namespace Api.FunctionalTests.Users.User
         [Fact]
         public async Task Should_RetrurnOK_And_User_WhenUserExists()
         {
+            SetAdminAuthentication();
+
             UserResponse? user = await HttpClient.GetFromJsonAsync<UserResponse>($"{usersBaseUrl}/1");
 
             user.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{usersBaseUrl}/1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            SetCustomerUserAuthentication();
+
+            HttpResponseMessage response = await HttpClient.GetAsync($"{usersBaseUrl}/1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
