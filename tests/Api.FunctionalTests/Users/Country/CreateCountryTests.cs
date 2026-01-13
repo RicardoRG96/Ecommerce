3de65528b41ecf8c64db92a6df267fc7fb98f97f
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 13:25:11 -0300

    Refactor to CreateCountryTests

--- tests/Api.FunctionalTests/Users/Country/CreateCountryTests.cs
+++ tests/Api.FunctionalTests/Users/Country/CreateCountryTests.cs
@@ -2,6 +2,7 @@
 using Api.FunctionalTests.Common;
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using Web.Api.Endpoints.v1.Users.Country.Create;
 
@@ -19,6 +20,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnBadRequest_WhenCountryNameIsMissing()
         {
+            SetAdminAuthentication();
+
             CreateCountryRequest invalidRequest = _request with { Name = "" };
 
             HttpResponseMessage response = await HttpClient.PostAsJsonAsync($"{countriesBaseUrl}", invalidRequest);
@@ -29,6 +32,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnBadRequest_WhenCountryNameExceedsTheMaximumLength()
         {
+            SetAdminAuthentication();
+
             CreateCountryRequest invalidRequest =
                 _request with { Name = Constants.ExceededMaximumLengthField };
 
@@ -40,6 +45,8 @@ namespace Api.FunctionalTests.Users.Country
         [Fact]
         public async Task Should_ReturnOk_WhenRequestIsValid()
         {
+            SetAdminAuthentication();
+
             HttpResponseMessage response = await HttpClient.PostAsJsonAsync(countriesBaseUrl, _request);
 
             response.StatusCode.Should().Be(HttpStatusCode.OK);
@@ -47,6 +54,27 @@ namespace Api.FunctionalTests.Users.Country
             long countryId = await response.Content.ReadFromJsonAsync<long>();
 
             countryId.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync(countriesBaseUrl, _request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            SetCustomerUserAuthentication();
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync(countriesBaseUrl, _request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
