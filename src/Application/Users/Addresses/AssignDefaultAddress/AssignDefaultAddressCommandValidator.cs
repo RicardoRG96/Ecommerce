4054fa44eea06ce2ed4010313bd1c8eda8f61002
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-13 16:44:36 -0300

    Add AssignDefaultAddressCommandValidator

---
+++ src/Application/Users/Addresses/AssignDefaultAddress/AssignDefaultAddressCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Addresses.AssignDefaultAddress
+{
+    public class AssignDefaultAddressCommandValidator : AbstractValidator<AssignDefaultAddressCommand>
+    {
+        public AssignDefaultAddressCommandValidator()
+        {
+            RuleFor(c => c.UserId).NotEmpty();
+            RuleFor(c => c.AddressId).NotEmpty();
+        }
+    }
+}
