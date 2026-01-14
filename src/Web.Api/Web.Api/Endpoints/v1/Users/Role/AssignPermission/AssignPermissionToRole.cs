89555d307d44508d459643f209fbca93b5ef5b9c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-14 11:04:52 -0300

    Add AssignPermissionToRole endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Role/AssignPermission/AssignPermissionToRole.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.Roles.AssignPermission;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Users.Role.AssignPermission
+{
+    internal sealed class AssignPermissionToRole : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("admin/permissions/assign", async (
+                AssignPermissionToRoleRequest request,
+                ICommandHandler<AssignPermissionToRoleCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                AssignPermissionToRoleCommand command = new(request.RoleName, request.PermissionName);
+
+                Result result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Roles.AssignPermission)
+            .WithTags(Tags.Roles);
+        }
+    }
+}
