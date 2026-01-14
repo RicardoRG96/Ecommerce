fa89d5f7a528cdc527e8dd121592d5a27195702c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-14 10:16:27 -0300

    Add AssignPermissionCommandValidator

---
+++ src/Application/Users/Roles/AssignPermission/AssignPermissionCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Roles.AssignPermission
+{
+    internal sealed class AssignPermissionCommandValidator : AbstractValidator<AssignPermissionCommand>
+    {
+        public AssignPermissionCommandValidator()
+        {
+            RuleFor(c => c.Role).NotEmpty().MaximumLength(100);
+            RuleFor(c => c.Permission).NotEmpty().MaximumLength(100);
+        }
+    }
+}
