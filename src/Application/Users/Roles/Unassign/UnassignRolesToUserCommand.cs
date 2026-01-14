45569a891f452d0b64c94181b4eb802191347b97
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-14 09:00:20 -0300

    Add UnassignRolesToUserCommand

---
+++ src/Application/Users/Roles/Unassign/UnassignRolesToUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Roles.Unassign
+{
+    public sealed record UnassignRolesToUserCommand(
+        IEnumerable<string> Roles,
+        long UserId) : ICommand;
+}
