a3ad1c39c69b525e05ba3e921776edd90c29966a
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-14 10:14:26 -0300

    Add AssignPermissionCommand

---
+++ src/Application/Users/Roles/AssignPermission/AssignPermissionCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Roles.AssignPermission
+{
+    public sealed record AssignPermissionCommand(
+        string Role,
+        string Permission) : ICommand;
+}
