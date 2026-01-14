9aa5abe77fb0bc2f452426bbdada887344d6c966
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-14 09:29:52 -0300

    Add UnassignRolesToUser endpoint

--- src/Web.Api/Web.Api/Endpoints/v1/Users/Role/UnassignRoles/UnassignRolesToUser.cs
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Role/UnassignRoles/UnassignRolesToUser.cs
@@ -1,29 +1,29 @@
 ﻿using Application.Abstractions.Messaging;
-using Application.Users.Roles.Assign;
+using Application.Users.Roles.Unassign;
 using Infrastructure.Access;
 using SharedKernel;
 using Web.Api.Extensions;
 using Web.Api.Infrastructure;
 
-namespace Web.Api.Endpoints.v1.Users.Role.AssignRoles
+namespace Web.Api.Endpoints.v1.Users.Role.UnassignRoles
 {
-    internal sealed class AssignRolesToUser : IEndpoint
+    internal sealed class UnassignRolesToUser : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder app)
         {
-            app.MapPost("admin/roles/assign/user/{userId:long}", async (
+            app.MapPost("admin/roles/unassign/user/{userId:long}", async (
                 long userId,
-                AssignRolesToUserRequest request,
-                ICommandHandler<AssignRolesToUserCommand> handler,
+                UnassignRolesToUserRequest request,
+                ICommandHandler<UnassignRolesToUserCommand> handler,
                 CancellationToken cancellationToken) =>
             {
-                AssignRolesToUserCommand command = new(request.Roles, userId);
+                UnassignRolesToUserCommand command = new(request.Roles, userId);
 
                 Result result = await handler.Handle(command, cancellationToken);
 
                 return result.Match(Results.NoContent, CustomResults.Problem);
             })
-            .HasPermission(Permissions.Users.AssignRole)
+            .HasPermission(Permissions.Users.UnassignRole)
             .WithTags(Tags.Roles);
         }
     }
