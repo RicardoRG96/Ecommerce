59963e1fb53a32a4094ce4bc39ccab8d1a0e25f8
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-14 06:04:41 -0300

    Add UpdatePasswordTests

---
+++ tests/Api.FunctionalTests/Users/User/UpdatePasswordTests.cs
@@ -0,0 +1,26 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using Web.Api.Endpoints.v1.Users.User.UpdatePassword;
+
+namespace Api.FunctionalTests.Users.User
+{
+    public class UpdatePasswordTests : BaseFunctionalTest
+    {
+        private static readonly UpdatePasswordRequest _request = new("Test1234", "Test.123");
+
+        public UpdatePasswordTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenUserIdIsMissing()
+        {
+            HttpResponseMessage response = await HttpClient.PutAsJsonAsync($"{usersBaseUrl}/me/password/0", _request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
