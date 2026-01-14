7346e7265ff02864ec97f98e5048618b027a3a16
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-14 10:55:05 -0300

    Add AssignPermissionCommandHandler

---
+++ src/Application/Users/Roles/AssignPermission/AssignPermissionCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions.Common;
+using Application.Abstractions.Messaging;
+using SharedKernel;
+
+namespace Application.Users.Roles.AssignPermission
+{
+    internal sealed class AssignPermissionCommandHandler : ICommandHandler<AssignPermissionCommand>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AssignPermissionCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<Result> Handle(AssignPermissionCommand command, CancellationToken cancellationToken)
+        {
+            return await _identityService.AddPermissionToRole(command, cancellationToken);
+        }
+    }
+}
