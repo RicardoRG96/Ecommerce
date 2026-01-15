00a0c686161a8d41548fc770e35e199807873685
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 14:15:12 -0300

    Add DeleteRoleTests

---
+++ tests/Api.FunctionalTests/Users/Role/DeleteRoleTests.cs
@@ -0,0 +1,25 @@
+﻿using Api.FunctionalTests.Abstractions;
+using Api.FunctionalTests.Common;
+using FluentAssertions;
+using System.Net;
+
+namespace Api.FunctionalTests.Users.Role
+{
+    public class DeleteRoleTests : BaseFunctionalTest
+    {
+        public DeleteRoleTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenRoleIdIsMissing()
+        {
+            SetAdminAuthentication();
+
+            HttpResponseMessage response = await HttpClient.DeleteAsync($"{rolesBaseUrl}/0");
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
