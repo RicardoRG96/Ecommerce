9185cc8abf53fa0351ca10d2ed0c663b29d5881b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 16:31:12 -0300

    Add GetById role endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Users/Role/Get/GetById.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.Roles.GetById;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Users.Role.Get
+{
+    internal sealed class GetById : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("admin/roles/{roleId:long}", async (
+                long roleId,
+                IQueryHandler<GetRoleByIdQuery, RoleResponse> handler,
+                CancellationToken cancellationToken) =>
+            {
+                GetRoleByIdQuery query = new(roleId);
+
+                Result<RoleResponse> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Roles.Read)
+            .WithTags(Tags.Roles);
+        }
+    }
+}
