39b9d889f206e7e9478a2a73ac72241dcf71e1f4
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 15:01:49 -0300

    Add UpdateRoleCommand

---
+++ src/Application/Users/Roles/Update/UpdateRoleCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Roles.Update
+{
+    public sealed record UpdateRoleCommand(
+        long Id,
+        string Name) : ICommand;
+}
