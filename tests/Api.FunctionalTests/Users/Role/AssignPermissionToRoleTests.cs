1be7558fa081c6d8f3fbafa86fe679d2e439e2d9
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 10:05:19 -0300

    Add test to AssignPermissionToRoleTests

--- tests/Api.FunctionalTests/Users/Role/AssignPermissionToRoleTests.cs
+++ tests/Api.FunctionalTests/Users/Role/AssignPermissionToRoleTests.cs
@@ -50,5 +50,16 @@ namespace Api.FunctionalTests.Users.Role
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenPermissionNameExceedsTheMaximumLength()
+        {
+            AssignPermissionToRoleRequest invalidRequest = _request with { PermissionName = Constants.ExceededMaximumLengthField };
+
+            HttpResponseMessage response =
+                await HttpClient.PostAsJsonAsync($"{rolesBaseUrl}/permissions/assign", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
