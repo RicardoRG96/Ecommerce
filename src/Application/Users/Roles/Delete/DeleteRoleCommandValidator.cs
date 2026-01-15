1de5eae5da9cf9d20d1ecef0e86eb3d4dfcc8073
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 13:54:54 -0300

    Add DeleteRoleCommandValidator

---
+++ src/Application/Users/Roles/Delete/DeleteRoleCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Roles.Delete
+{
+    internal sealed class DeleteRoleCommandValidator : AbstractValidator<DeleteRoleCommand>
+    {
+        public DeleteRoleCommandValidator()
+        {
+            RuleFor(c => c.Id).NotEmpty();
+        }
+    }
+}
