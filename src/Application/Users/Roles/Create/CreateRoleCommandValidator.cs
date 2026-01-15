29899f7650a56ed410aca4472d867856c7057e1b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 12:30:56 -0300

    Add CreateRoleCommandValidator

---
+++ src/Application/Users/Roles/Create/CreateRoleCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Roles.Create
+{
+    internal sealed class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
+    {
+        public CreateRoleCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().MaximumLength(100);
+        }
+    }
+}
