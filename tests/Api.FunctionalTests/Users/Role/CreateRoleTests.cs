8ae0f817833ee5324d080e1f3fc04fcc1676186e
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 13:41:45 -0300

    Add CreateRoleTests

---
+++ tests/Api.FunctionalTests/Users/Role/CreateRoleTests.cs
@@ -0,0 +1,30 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using Web.Api.Endpoints.v1.Users.Role.Create;
+
+namespace Api.FunctionalTests.Users.Role
+{
+    public class CreateRoleTests : BaseFunctionalTest
+    {
+        private static readonly CreateRoleRequest _request = new("InventoryManager");
+
+        public CreateRoleTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenRoleNameIsMissing()
+        {
+            SetAdminAuthentication();
+
+            CreateRoleRequest invalidRequest = _request with { RoleName = "" };
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync($"{rolesBaseUrl}", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
