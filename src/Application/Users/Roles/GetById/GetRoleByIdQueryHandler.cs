512f64c6cb6ab8f37695c6b89ea47bac1c015d9d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 16:24:18 -0300

    Add GetRoleByIdQueryHandler

---
+++ src/Application/Users/Roles/GetById/GetRoleByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Abstractions.Common;
+using Application.Abstractions.Messaging;
+using Domain.Entities.Users;
+using Domain.Errors.Users;
+using SharedKernel;
+
+namespace Application.Users.Roles.GetById
+{
+    internal sealed class GetRoleByIdQueryHandler : IQueryHandler<GetRoleByIdQuery, RoleResponse>
+    {
+        private readonly IIdentityService _identityService;
+
+        public GetRoleByIdQueryHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<Result<RoleResponse>> Handle(GetRoleByIdQuery query, CancellationToken cancellationToken)
+        {
+            Role? role = await _identityService.GetRoleByIdAsync(query.Id, cancellationToken);
+
+            if (role is null)
+            {
+                return Result.Failure<RoleResponse>(RoleErrors.NotFound(query.Id));
+            }
+
+            RoleResponse response = new()
+            {
+                Id = role.Id,
+                Name = role.Name
+            };
+
+            return Result.Success(response);
+        }
+    }
+}
