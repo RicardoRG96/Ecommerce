f49b1c3ca3f67e6b3460ca41193d1a1f04163851
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 18:30:42 -0300

    Add GetRolesWithPaginationQuery

---
+++ src/Application/Users/Roles/GetWithPagination/GetRolesWithPaginationQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.Roles.GetWithPagination
+{
+    public sealed record GetRolesWithPaginationQuery(
+        int PageNumber,
+        int PageSize) : IQuery<RoleResponse>;
+}
