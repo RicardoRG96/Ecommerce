ca0500799ddfa3e4e0f0e56a1b698661c7a51b94
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-15 12:02:42 -0300

    Add UnassignPermissionToRoleTests

---
+++ tests/Api.FunctionalTests/Users/Role/UnassignPermissionToRoleTests.cs
@@ -0,0 +1,34 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using Infrastructure.Access;
+using System.Net;
+using System.Net.Http.Json;
+using Web.Api.Endpoints.v1.Users.Role.AssignPermission;
+using Web.Api.Endpoints.v1.Users.Role.UnassignPermission;
+
+namespace Api.FunctionalTests.Users.Role
+{
+    public class UnassignPermissionToRoleTests : BaseFunctionalTest
+    {
+        private static readonly UnassignPermissionToRoleRequest _request =
+            new(Roles.CustomerSupport, Permissions.Products.Read);
+
+        public UnassignPermissionToRoleTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenRoleNameIsMissing()
+        {
+            SetAdminAuthentication();
+
+            UnassignPermissionToRoleRequest invalidRequest = _request with { RoleName = "" };
+
+            HttpResponseMessage response =
+                await HttpClient.PostAsJsonAsync($"{rolesBaseUrl}/permissions/unassign", invalidRequest);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
