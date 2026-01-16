5f17b71cf0106dad3d4829e6fd6b48b23e6f70e9
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-16 10:25:24 -0300

    Refactor to Brand entity

--- src/Domain/Entities/Products/Brand.cs
+++ src/Domain/Entities/Products/Brand.cs
@@ -6,7 +6,16 @@ namespace Domain.Entities.Products
     {
         public long BrandId { get; set; }
         public string? Name { get; set; }
+        public string? Slug { get; set; }
         public string? Description { get; set; }
         public string? LogoUrl { get; set; }
+        public string? BannerUrl { get; set; }
+        public string? WebsiteUrl { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsFeatured { get; set; }
+        public bool DisplayOrder { get; set; }
+        public string? MetaTitle { get; set; }
+        public string? MetaDescription { get; set; }
+        public string? MetaKeywords { get; set; }
     }
 }
