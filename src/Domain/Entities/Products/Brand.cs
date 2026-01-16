3b28953aef72d3f8e4cf1e8591a64ac6b81fdf91
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-16 09:18:55 -0300

    Add Brand entity to Domain layer

---
+++ src/Domain/Entities/Products/Brand.cs
@@ -0,0 +1,12 @@
+﻿using SharedKernel;
+
+namespace Domain.Entities.Products
+{
+    public sealed class Brand : BaseAuditableEntity
+    {
+        public long BrandId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? LogoUrl { get; set; }
+    }
+}
