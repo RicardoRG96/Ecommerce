60668bbbc8c154d269d4eb9aec52b540881f6da4
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-19 09:42:51 -0300

    Add Discount entity

---
+++ src/Domain/Entities/Products/Discount.cs
@@ -0,0 +1,23 @@
+﻿using SharedKernel;
+
+namespace Domain.Entities.Products
+{
+    public sealed class Discount : BaseAuditableEntity
+    {
+        public long Id { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? DiscountType { get; set; }
+        public decimal Value { get; set; }
+        public decimal MaxDiscountAmount { get; set; }
+        public bool IsStackable { get; set; }
+        public int Priority { get; set; }
+        public DateTime StartsAt { get; set; }
+        public DateTime EndsAt { get; set; }
+        public decimal MinOrderAmount { get; set; }
+        public int UsageLimit { get; set; }
+        public int UsageLimitPerUser { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
