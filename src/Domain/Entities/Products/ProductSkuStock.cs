a21c9258a067868a63c34a181a20999be26bd0ba
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-19 09:30:03 -0300

    Add ProductSkuStock entity

---
+++ src/Domain/Entities/Products/ProductSkuStock.cs
@@ -0,0 +1,18 @@
+﻿using SharedKernel;
+
+namespace Domain.Entities.Products
+{
+    public sealed class ProductSkuStock : BaseAuditableEntity
+    {
+        public long Id { get; set; }
+        public long ProductSkuId { get; set; }
+        public long WarehouseId { get; set; }
+        public int Stock { get; set; }
+        public int ReservedStock { get; set; }
+        public int MinStock { get; set; }
+        public bool IsActive { get; set; }
+        public byte[] RowVersion { get; set; } = Array.Empty<byte>();
+        public ProductSku ProductSku { get; set; } = null!;
+        public Warehouse Warehouse { get; set; } = null!;
+    }
+}
