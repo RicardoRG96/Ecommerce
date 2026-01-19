b444262050940937d7a7c4c14f7c86c725ce3389
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-19 06:27:48 -0300

    Add ProductTaxCategory entity

---
+++ src/Domain/Entities/Products/ProductTaxCategory.cs
@@ -0,0 +1,12 @@
+﻿using SharedKernel;
+
+namespace Domain.Entities.Products
+{
+    public sealed class ProductTaxCategory : BaseAuditableEntity
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+    }
+}
