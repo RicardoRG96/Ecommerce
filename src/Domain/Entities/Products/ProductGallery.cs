de9619f0e03075636a3e0338282e14e4ac58f898
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-19 07:14:42 -0300

    Add entity ProductGallery

---
+++ src/Domain/Entities/Products/ProductGallery.cs
@@ -0,0 +1,19 @@
+﻿using SharedKernel;
+
+namespace Domain.Entities.Products
+{
+    public sealed class ProductGallery : BaseAuditableEntity
+    {
+        public long Id { get; set; }
+        public long ProductId { get; set; }
+        public long ProductSkuId { get; set; }
+        public string? MediaUrl { get; set; }
+        public string? MediaType { get; set; }
+        public string? MimeType { get; set; }
+        public bool IsPrimary { get; set; }
+        public int DisplayOrder { get; set; }
+        public string? AltText { get; set; }
+        public Product Product { get; set; } = null!;
+        public ProductSku ProductSku { get; set; } = null!;
+    }
+}
