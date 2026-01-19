a3c9c97efc9851a67aadbbe57a8867a73201c7c3
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-19 06:42:18 -0300

    Add TaxRate entity

---
+++ src/Domain/Entities/Products/TaxRate.cs
@@ -0,0 +1,18 @@
+﻿using SharedKernel;
+
+namespace Domain.Entities.Products
+{
+    public sealed class TaxRate : BaseAuditableEntity
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Code { get; set; }
+        public char CountryCode { get; set; }
+        public string? TaxType { get; set; }
+        public decimal Rate { get; set; }
+        public bool IsCompound { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+    }
+}
