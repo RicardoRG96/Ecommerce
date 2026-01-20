e159ba2c37a0eadb0d7aed91dbb3f77cff97b07c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-20 17:30:18 -0300

    Add ProductTaxCategoryConfiguration

--- src/Domain/Entities/Products/ProductTaxCategory.cs
+++ src/Domain/Entities/Products/ProductTaxCategory.cs
@@ -8,6 +8,7 @@ namespace Domain.Entities.Products
         public string? Name { get; set; }
         public string? Code { get; set; }
         public string? Description { get; set; }
+        public bool IsActive { get; set; }
         public ICollection<ProductTaxCategoryRate> ProductTaxCategoryRates { get; set; } = new List<ProductTaxCategoryRate>();
         public ICollection<Product> Products { get; set; } = new List<Product>();
     }
