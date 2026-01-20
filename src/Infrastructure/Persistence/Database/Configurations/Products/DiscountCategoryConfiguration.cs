fb009fec6fcee796ec5845b6bbb1760f1780545a
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-20 14:51:24 -0300

    Add DiscountCategoryConfiguration

---
+++ src/Infrastructure/Persistence/Database/Configurations/Products/DiscountCategoryConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Database.Configurations.Products
+{
+    public class DiscountCategoryConfiguration : IEntityTypeConfiguration<DiscountCategory>
+    {
+        public void Configure(EntityTypeBuilder<DiscountCategory> builder)
+        {
+            builder.ToTable("DiscountCategory");
+
+            builder.HasKey(dc => new { dc.DiscountId, dc.CategoryId });
+
+            builder.Property(dc => dc.DiscountId)
+                .IsRequired();
+
+            builder.Property(dc => dc.CategoryId)
+                .IsRequired();
+
+            builder.HasOne(dc => dc.Discount)
+                .WithMany(d => d.DiscountCategories)
+                .HasForeignKey(dc => dc.DiscountId);
+
+            builder.HasOne(dc => dc.Category)
+                .WithMany(c => c.DiscountCategories)
+                .HasForeignKey(dc => dc.CategoryId);
+        }
+    }
+}
