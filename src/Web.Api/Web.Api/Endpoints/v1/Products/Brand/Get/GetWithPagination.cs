1b270836e1cd67625439e96665e988f7b9807e84
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-21 10:53:19 -0300

    Add GetWithPagination brand endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Brand/Get/GetWithPagination.cs
@@ -0,0 +1,30 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.Brands.GetWithPagination;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Brand.Get
+{
+    internal sealed class GetWithPagination : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products/brands", async (
+                int pageNumber,
+                int pageSize,
+                IQueryHandler<GetBrandsWithPaginationQuery, PaginatedList<BrandResponse>> handler,
+                CancellationToken cancellationToken) =>
+            {
+                GetBrandsWithPaginationQuery query = new(pageNumber, pageSize);
+
+                Result<PaginatedList<BrandResponse>> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Brands.Read)
+            .WithTags(Tags.Brands);
+        }
+    }
+}
