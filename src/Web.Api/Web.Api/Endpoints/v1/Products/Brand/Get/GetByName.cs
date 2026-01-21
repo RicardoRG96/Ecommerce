c59525f7f59698fb94a4b6495c4b9a252215c6ed
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-21 10:48:06 -0300

    Add GetByName brand endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Brand/Get/GetByName.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.Brands.GetByName;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Brand.Get
+{
+    internal sealed class GetByName : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products/brands/name/{brandName}", async (
+                string brandName,
+                IQueryHandler<GetBrandByNameQuery, BrandResponse> handler,
+                CancellationToken cancellationToken) =>
+            {
+                GetBrandByNameQuery query = new(brandName);
+
+                Result<BrandResponse> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Brands.Read)
+            .WithTags(Tags.Brands);
+        }
+    }
+}
