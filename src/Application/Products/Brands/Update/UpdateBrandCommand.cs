6de9152624f65dd6b5902d20d7f3e0b5605eb15b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-21 07:23:08 -0300

    Add UpdateBrandCommand

---
+++ src/Application/Products/Brands/Update/UpdateBrandCommand.cs
@@ -0,0 +1,17 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.Brands.Update
+{
+    public sealed record UpdateBrandCommand(
+        string Name,
+        string Description,
+        string LogoUrl,
+        string BannerUrl,
+        string WebsiteUrl,
+        bool IsActive,
+        bool IsFeatured,
+        int DisplayOrder,
+        string MetaTitle,
+        string MetaDescription,
+        string MetaKeywords) : ICommand;
+}
