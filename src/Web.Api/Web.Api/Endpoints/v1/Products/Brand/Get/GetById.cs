10ff0375640338cd8f67f1a095e86f105e0d349d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-21 10:42:50 -0300

    Add GetById brand endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Brand/Get/GetById.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.Brands.GetById;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Brand.Get
+{
+    internal sealed class GetById : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products/brands/{brandId:long", async (
+                long brandId,
+                IQueryHandler<GetBrandByIdQuery, BrandResponse> handler,
+                CancellationToken cancellationToken) =>
+            {
+                GetBrandByIdQuery query = new(brandId);
+
+                Result<BrandResponse> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Brands.Read)
+            .WithTags(Tags.Brands);
+        }
+    }
+}
