36615d8b9947356a416880402c8ca5e819af6588
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 18:21:48 -0300

    Add CategoryTreeResponse

---
+++ src/Application/Products/Categories/GetCategoryTree/CategoryTreeResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Products.Categories.GetCategoryTree
+{
+    public sealed class CategoryTreeResponse
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Slug { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Icon { get; set; }
+        public int DisplayOrder { get; set; }
+        public List<CategoryTreeResponse>? Children { get; set; }
+    }
+}
