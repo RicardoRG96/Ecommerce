fb8a71b9579c1703c999e84524005dd9efb1e95f
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 15:20:05 -0300

    Refactor to Constants

---
+++ tests/Api.FunctionalTests/Products/Brand/CreateBrandTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Brand
+{
+    public class CreateBrandTests : BaseFunctionalTest
+    {
+        public CreateBrandTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
