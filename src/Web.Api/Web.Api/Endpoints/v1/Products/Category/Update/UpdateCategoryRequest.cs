00c6777e5b104c3c6c5e84b972ae28e20dff028d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 18:44:51 -0300

    Add UpdateCategoryRequest

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Category/Update/UpdateCategoryRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Api.Endpoints.v1.Products.Category.Update
+{
+    public sealed record UpdateCategoryRequest(
+        long CategoryId,
+        string Name,
+        string Description,
+        string ImageUrl,
+        string Icon,
+        int DisplayOrder,
+        bool IsVisibleInMenu,
+        string MetaTitle,
+        string MetaDescription,
+        string MetaKeywords);
+}
