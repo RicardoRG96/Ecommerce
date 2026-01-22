05a0c848b6061090bbc9386e7960e12f978ad992
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 14:12:02 -0300

    Add DeactivateBrandCommandTests

---
+++ tests/Application.UnitTests/Products/Brands/Deactivate/DeactivateBrandCommandTests.cs
@@ -0,0 +1,41 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Products.Brands.Deactivate;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using SharedKernel;
+
+namespace Application.UnitTests.Products.Brands.Deactivate
+{
+    public class DeactivateBrandCommandTests
+    {
+        private static readonly DeactivateBrandCommand _command = new(1);
+        private readonly DeactivateBrandCommandHandler _handler;
+        private readonly IBrandRepository _brandRepositoryMock;
+        private readonly IUnitOfWork _unitOfWorkMock;
+
+        public DeactivateBrandCommandTests()
+        {
+            _brandRepositoryMock = Substitute.For<IBrandRepository>();
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+            _handler = new(_brandRepositoryMock, _unitOfWorkMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenBrandIdDoesNotExist()
+        {
+            long notExistingBrandId = 2500;
+            DeactivateBrandCommand invalidCommand = _command with { BrandId = notExistingBrandId };
+
+            _brandRepositoryMock
+                .GetByIdAsync(Arg.Is<long>(id => id == invalidCommand.BrandId), Arg.Any<CancellationToken>())
+                .ReturnsNull();
+
+            Result result = await _handler.Handle(invalidCommand, default);
+
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailure.Should().BeTrue();
+        }
+    }
+}
