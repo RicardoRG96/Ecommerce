4abd7dc4ef13f060783d193556eed9bb10faa0c1
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 18:04:59 -0300

    Add ChangeCategoryParentCommandValidator

---
+++ src/Application/Products/Categories/ChangeCategoryParent/ChangeCategoryParentCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Categories.ChangeCategoryParent
+{
+    public sealed class ChangeCategoryParentCommandValidator : AbstractValidator<ChangeCategoryParentCommand>
+    {
+        public ChangeCategoryParentCommandValidator()
+        {
+            RuleFor(c => c.CategoryId)
+                .NotEmpty().WithMessage("El ID de la categoría no puede estar vacío.");
+        }
+    }
+}
