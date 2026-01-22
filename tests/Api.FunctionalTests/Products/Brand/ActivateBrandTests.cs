47f694f1f4e82a08ff761335aa8b08a7070c2509
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 14:47:19 -0300

    Add ActivateBrandTests

---
+++ tests/Api.FunctionalTests/Products/Brand/ActivateBrandTests.cs
@@ -0,0 +1,24 @@
+﻿using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+using System.Net;
+
+namespace Api.FunctionalTests.Products.Brand
+{
+    public class ActivateBrandTests : BaseFunctionalTest
+    {
+        public ActivateBrandTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldReturnBadRequest_WhenBrandIdIsMissing()
+        {
+            SetAdminAuthentication();
+
+            HttpResponseMessage response = await HttpClient.PatchAsync($"{brandsBaseUrl}/0/activate", null!);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
