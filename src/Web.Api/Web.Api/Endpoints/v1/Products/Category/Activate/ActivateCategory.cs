669bfae405a011a12519631df8b11306341d5da5
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 18:57:29 -0300

    Add ActivateCategory endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Category/Activate/ActivateCategory.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.Categories.Activate;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Category.Activate
+{
+    internal sealed class ActivateCategory : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPatch("categories/{id:long}/activate", async (
+                long id,
+                ICommandHandler<ActivateCategoryCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                ActivateCategoryCommand command = new(id);
+
+                Result result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Categories.Update)
+            .WithTags(Tags.Categories);
+        }
+    }
+}
