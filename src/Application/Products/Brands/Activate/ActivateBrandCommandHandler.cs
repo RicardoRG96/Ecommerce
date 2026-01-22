640d8ff262dfdc93e0ebab8873ab2f263e6e1083
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 13:19:40 -0300

    Add ActivateBrandCommandHandler

---
+++ src/Application/Products/Brands/Activate/ActivateBrandCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Abstractions.Messaging;
+using Domain.Entities.Products;
+using Domain.Errors.Products;
+using SharedKernel;
+
+namespace Application.Products.Brands.Activate
+{
+    internal sealed class ActivateBrandCommandHandler : ICommandHandler<ActivateBrandCommand>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ActivateBrandCommandHandler(
+            IBrandRepository brandRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _brandRepository = brandRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(ActivateBrandCommand command, CancellationToken cancellationToken)
+        {
+            Brand? brand =  await _brandRepository.GetByIdAsync(command.BrandId, cancellationToken);
+            
+            if (brand is null)
+            {
+                return Result.Failure(BrandErrors.NotFound(command.BrandId));
+            }
+
+            if (brand.IsActive)
+            {
+                return Result.Success();
+            }
+
+            brand.IsActive = true;
+
+            _brandRepository.Update(brand);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
