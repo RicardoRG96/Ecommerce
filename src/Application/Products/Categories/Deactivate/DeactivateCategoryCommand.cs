bc3cfa52d73aab67be9339f27338be654746e02f
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 18:13:32 -0300

    Add DeactivateCategoryCommand

---
+++ src/Application/Products/Categories/Deactivate/DeactivateCategoryCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.Categories.Deactivate
+{
+    public sealed record DeactivateCategoryCommand(long CategoryId) : ICommand;
+}
