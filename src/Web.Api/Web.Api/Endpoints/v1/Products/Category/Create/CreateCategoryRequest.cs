97efc65f99980d9ea9e5d0ba771c128b418e3544
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 18:35:10 -0300

    Add CreateCategoryRequest

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Category/Create/CreateCategoryRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Web.Api.Endpoints.v1.Products.Category.Create
+{
+    public sealed record CreateCategoryRequest(
+        long ParentId,
+        string Name,
+        string? Description,
+        string? ImageUrl,
+        string? Icon,
+        int DisplayOrder,
+        bool IsActive,
+        bool IsVisibleInMenu,
+        string? MetaTitle,
+        string? MetaDescription,
+        string? MetaKeywords
+    );
+}
