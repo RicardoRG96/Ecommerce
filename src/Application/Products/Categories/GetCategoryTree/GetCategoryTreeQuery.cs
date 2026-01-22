2d82d2ba606652ce141888d8d4ccce053178463e
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 18:19:21 -0300

    Add GetCategoryTreeQuery

---
+++ src/Application/Products/Categories/GetCategoryTree/GetCategoryTreeQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.Categories.GetCategoryTree
+{
+    public sealed record GetCategoryTreeQuery(long CategoryId) : IQuery<CategoryTreeResponse>;
+}
