c2d446a5049f508b0a219897d7dd43fd292c8a9e
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-22 18:55:04 -0300

    Add ChangeParent category endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Category/ChangeParent/ChangeParent.cs
@@ -0,0 +1,33 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.Categories.ChangeCategoryParent;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Category.ChangeParent
+{
+    internal sealed class ChangeParent : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPatch("categories/{id:long}/parent", async (
+                long id,
+                ChangeParentRequest request,
+                ICommandHandler<ChangeCategoryParentCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                ChangeCategoryParentCommand command = new(
+                    id,
+                    request.NewParentId
+                );
+
+                Result result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Categories.Update)
+            .WithTags(Tags.Categories);
+        }
+    }
+}
