c03d48f13cc4a3cfc32173d2a95eb9a2801e2636
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-23 07:16:16 -0300

    Add tests to DeactivateCategoryTests

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,5 @@
+# Copilot Instructions
+
+## General Guidelines
+- Always use English comments in code.
+- Follow the AAA (Arrange-Act-Assert) pattern for all test methods.
--- tests/Api.FunctionalTests/Products/Category/DeactivateCategoryTests.cs
+++ tests/Api.FunctionalTests/Products/Category/DeactivateCategoryTests.cs
@@ -1,4 +1,8 @@
 ﻿using Api.FunctionalTests.Abstractions;
+using Api.FunctionalTests.Common;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace Api.FunctionalTests.Products.Category
 {
@@ -9,6 +13,86 @@ namespace Api.FunctionalTests.Products.Category
         {
         }
 
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenCategoryIdIsMissing()
+        {
+            // Arrange
+            SetAdminAuthentication();
 
+            // Act
+            HttpResponseMessage response = await HttpClient.PatchAsync($"{categoriesBaseUrl}/0/deactivate", null!);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Should_ReturnNotFound_WhenCategoryIdDoesNotExist()
+        {
+            // Arrange
+            SetAdminAuthentication();
+
+            // Act
+            HttpResponseMessage response = await HttpClient.PatchAsync($"{categoriesBaseUrl}/{Constants.NotExistingId}/deactivate", null!);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Should_ReturnNoContent_WhenCategoryIdExists()
+        {
+            // Arrange
+            SetAdminAuthentication();
+
+            // Act - Assumes that a category with ID 1 exists in seed data
+            HttpResponseMessage response = await HttpClient.PatchAsync($"{categoriesBaseUrl}/1/deactivate", null!);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Should_ReturnNoContent_WhenCategoryIsAlreadyInactive()
+        {
+            // Arrange
+            SetAdminAuthentication();
+
+            // Act - First deactivation
+            await HttpClient.PatchAsync($"{categoriesBaseUrl}/1/deactivate", null!);
+
+            // Act - Second deactivation (category is already inactive)
+            HttpResponseMessage response = await HttpClient.PatchAsync($"{categoriesBaseUrl}/1/deactivate", null!);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Should_ReturnUnauthorized_WhenUserIsNotLoggedIn()
+        {
+            // Arrange
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "");
+
+            // Act
+            HttpResponseMessage response = await HttpClient.PatchAsync($"{categoriesBaseUrl}/1/deactivate", null!);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Should_ReturnForbidden_WhenUserHasNotPermission()
+        {
+            // Arrange
+            SetCustomerUserAuthentication();
+
+            // Act
+            HttpResponseMessage response = await HttpClient.PatchAsync($"{categoriesBaseUrl}/1/deactivate", null!);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
     }
 }
