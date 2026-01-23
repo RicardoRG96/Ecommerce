c32aae9f2179666b096a68a202de138c881edaf7
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-23 07:04:59 -0300

    Add migration to change parentId nullable for Category table

---
+++ src/Infrastructure/Persistence/Database/Migrations/20260123100218_Change_ParentId_Nullable_Category_Table.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_ParentId_Nullable_Category_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ParentId",
+                table: "Category",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ParentId",
+                table: "Category",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
