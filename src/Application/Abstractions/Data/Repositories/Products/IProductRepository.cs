47c0f0d03995e1cb9d8aa4cd8ceb718edab8fb7c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-23 10:14:24 -0300

    Add ProductRepository and IProductRepository

---
+++ src/Application/Abstractions/Data/Repositories/Products/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities.Products;
+
+namespace Application.Abstractions.Data.Repositories.Products
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<Product?> GetProductDetailByIdAsync(long id, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Infrastructure/Persistence/Repositories/Products/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Domain.Entities.Products;
+using Infrastructure.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories.Products
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext context) 
+            : base(context)
+        {
+        }
+
+        public Task<Product?> GetProductDetailByIdAsync(long id, CancellationToken cancellationToken)
+        {
+            return _context.Products
+                .Where(p => p.Id == id)
+                .Include(p => p.Brand)
+                .Include(p => p.Category)
+                .Include(p => p.ProductSkus)
+                .Include(p => p.ProductGalleries)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+    }
+}
