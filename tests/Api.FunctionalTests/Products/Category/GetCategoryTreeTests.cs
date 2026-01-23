5042fc96a49dd1978d17a0cb7bca9b956c7f9ae3
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-23 09:43:40 -0300

    Add GetCategoryTreeTests

---
+++ tests/Api.FunctionalTests/Products/Category/GetCategoryTreeTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Category
+{
+    public class GetCategoryTreeTests : BaseFunctionalTest
+    {
+        public GetCategoryTreeTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
