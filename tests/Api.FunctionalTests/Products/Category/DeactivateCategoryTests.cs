82d77ddbd51f1f2d4cba5397dcefbfdd0dcf64f5
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-23 07:07:50 -0300

    Add DeactivateCategoryTests

---
+++ tests/Api.FunctionalTests/Products/Category/DeactivateCategoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Category
+{
+    public class DeactivateCategoryTests : BaseFunctionalTest
+    {
+        public DeactivateCategoryTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
