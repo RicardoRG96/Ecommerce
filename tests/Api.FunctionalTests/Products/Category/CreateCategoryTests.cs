4689be7e27e6180d4250c0c50e7c166675e576d6
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-23 07:44:17 -0300

    Add CreateCategoryTests

---
+++ tests/Api.FunctionalTests/Products/Category/CreateCategoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Category
+{
+    public class CreateCategoryTests : BaseFunctionalTest
+    {
+        public CreateCategoryTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
