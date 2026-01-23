95b4f7ebe7185119dbaa3597773da4e361df049f
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-23 09:21:42 -0300

    Add UpdateCategoryTests

---
+++ tests/Api.FunctionalTests/Products/Category/UpdateCategoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Category
+{
+    public class UpdateCategoryTests : BaseFunctionalTest
+    {
+        public UpdateCategoryTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
