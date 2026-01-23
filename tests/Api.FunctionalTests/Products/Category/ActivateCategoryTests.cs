183510f26c58590eb25e7c1c06420047feb8fe0d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-23 06:43:46 -0300

    Add ActivateCategoryTests

---
+++ tests/Api.FunctionalTests/Products/Category/ActivateCategoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Category
+{
+    public class ActivateCategoryTests : BaseFunctionalTest
+    {
+        public ActivateCategoryTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
