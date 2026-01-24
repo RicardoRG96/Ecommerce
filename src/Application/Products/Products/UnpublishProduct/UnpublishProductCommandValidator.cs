9fc12434995c065b569db1ff2e76c025c4344719
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-24 13:03:07 -0300

    Add UnpublishProductCommandValidator

---
+++ src/Application/Products/Products/UnpublishProduct/UnpublishProductCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Products.UnpublishProduct
+{
+    public sealed class UnpublishProductCommandValidator : AbstractValidator<UnpublishProductCommand>
+    {
+        public UnpublishProductCommandValidator()
+        {
+            RuleFor(c => c.ProductId).NotEmpty().WithMessage("Product ID is required.")
+                .GreaterThan(0).WithMessage("Product ID must be a positive number.");
+        }
+    }
+}
