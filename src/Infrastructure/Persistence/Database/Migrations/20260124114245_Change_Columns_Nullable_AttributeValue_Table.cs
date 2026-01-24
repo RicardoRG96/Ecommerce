5cceabd352a4c123633e73f705b1d5680c74657b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-24 08:43:46 -0300

    Add migration to make nullable som properties of AttributeValue entity

---
+++ src/Infrastructure/Persistence/Database/Migrations/20260124114245_Change_Columns_Nullable_AttributeValue_Table.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_Columns_Nullable_AttributeValue_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "NumericValue",
+                table: "AttributeValue",
+                type: "decimal(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)",
+                oldPrecision: 18,
+                oldScale: 4);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "BooleanValue",
+                table: "AttributeValue",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "NumericValue",
+                table: "AttributeValue",
+                type: "decimal(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)",
+                oldPrecision: 18,
+                oldScale: 4,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "BooleanValue",
+                table: "AttributeValue",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
