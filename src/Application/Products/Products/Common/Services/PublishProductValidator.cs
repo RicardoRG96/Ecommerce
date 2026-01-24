e3f7a92790ba455e8351c4eb9cc7ae539c964f27
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-24 11:25:39 -0300

    Add PublishProductValidator

---
+++ src/Application/Products/Products/Common/Services/PublishProductValidator.cs
@@ -0,0 +1,14 @@
+﻿using SharedKernel;
+
+namespace Application.Products.Products.Common.Services
+{
+    internal sealed class PublishProductValidator : IPublishProductValidator
+    {
+
+
+        public Task<Result> ValidateCanBePublishedAsync(long productId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
