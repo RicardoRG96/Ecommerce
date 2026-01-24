b312883c9a5cd67efa91c8138421796e110e9eae
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-24 09:19:33 -0300

    Add CreateProductCommand

---
+++ src/Application/Products/Products/Create/CreateProductCommand.cs
@@ -0,0 +1,18 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.Products.Create
+{
+    public sealed record CreateProductCommand(
+        string Name,
+        string Description,
+        string ShortDescription,
+        long BrandId,
+        long CategoryId,
+        long ProductTaxCategoryId,
+        bool IsActive,
+        bool IsFeatured,
+        bool IsDigital,
+        string MetaTitle,
+        string MetaDescription,
+        string MetaKeywords) : ICommand<long>;
+}
