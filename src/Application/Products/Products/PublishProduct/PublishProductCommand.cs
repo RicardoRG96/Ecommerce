8ca2b38047b28d7123d48395e6009d5337ad819a
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-24 11:18:10 -0300

    Add PublishProductCommand

---
+++ src/Application/Products/Products/PublishProduct/PublishProductCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.Products.PublishProduct
+{
+    public sealed record PublishProductCommand(
+        long ProductId, 
+        bool IsActive) : ICommand;
+}
