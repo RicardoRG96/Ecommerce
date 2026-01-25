2f8e067acd7d0466c36d2031e9249de6002af14b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-25 06:40:00 -0300

    Add UpdateProductRequest

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Product/Update/UpdateProductRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.Api.Endpoints.v1.Products.Product.Update
+{
+    public sealed record UpdateProductRequest(
+        string Name,
+        string Description,
+        string ShortDescription,
+        long BrandId,
+        long CategoryId,
+        long ProductTaxCategoryId,
+        bool IsFeatured,
+        bool IsDigital,
+        string MetaTitle,
+        string MetaDescription,
+        string MetaKeywords);
+}
