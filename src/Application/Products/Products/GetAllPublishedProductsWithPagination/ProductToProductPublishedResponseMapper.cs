f8566e5186194cf1072cf3aa41d10bbdcaf2d15b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-25 07:28:30 -0300

    Add ProductToProductPublishedResponseMapper

---
+++ src/Application/Products/Products/GetAllPublishedProductsWithPagination/ProductToProductPublishedResponseMapper.cs
@@ -0,0 +1,72 @@
+﻿using Domain.Entities.Products;
+
+namespace Application.Products.Products.GetAllPublishedProductsWithPagination
+{
+    internal static class ProductToProductPublishedResponseMapper
+    {
+        public static PublishedProductResponse Map(Product product)
+        {
+            return new PublishedProductResponse()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Slug = product.Slug,
+                Description = product.Description,
+                ShortDescription = product.ShortDescription,
+                IsActive = product.IsActive,
+                MetaTitle = product.MetaTitle,
+                MetaDescription = product.MetaDescription,
+
+                Brand = product.Brand is not null
+                    ? new BrandResponse
+                    {
+                        Id = product.Brand.Id,
+                        Name = product.Brand.Name,
+                        Slug = product.Brand.Slug,
+                        LogoUrl = product.Brand.LogoUrl
+                    }
+                    : null,
+
+                Category = product.Category is not null
+                    ? new CategoryResponse
+                    {
+                        Id = product.Category.Id,
+                        Name = product.Category.Name,
+                        Slug = product.Category.Slug
+                    }
+                    : null,
+
+                ProductSkus = product.ProductSkus
+                    .Select(sku => new ProductSkuResponse
+                    {
+                        Id = sku.Id,
+                        SkuCode = sku.SkuCode,
+                        Price = sku.Price,
+                        ComparedAtPrice = sku.ComparedAtPrice,
+                        IsActive = sku.IsActive,
+                        DisplayOrder = sku.DisplayOrder,
+                        ProductGalleriesResponse = sku.ProductGalleries
+                            .Select(gallery => new ProductGalleryResponse
+                            {
+                                MediaUrl = gallery.MediaUrl,
+                                IsPrimary = gallery.IsPrimary,
+                                DisplayOrder = gallery.DisplayOrder,
+                                AltText = gallery.AltText
+                            })
+                            .ToList()
+                    })
+                    .ToList(),
+
+                ProductGalleries = product.ProductGalleries
+                    .Select(gallery => new ProductGalleryResponse
+                    {
+                        MediaUrl = gallery.MediaUrl,
+                        IsPrimary = gallery.IsPrimary,
+                        DisplayOrder = gallery.DisplayOrder,
+                        AltText = gallery.AltText
+                    })
+                    .ToList()
+            };
+        }
+    }
+}
