08609d11a6b4aef1b4b98602be194090fca3f09b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-25 06:56:49 -0300

    Add GetById product endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Product/Get/GetById.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.Products.GetProductDetailById;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Product.Get
+{
+    internal sealed class GetById : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products/{id:long}", async (
+                long id,
+                IQueryHandler<GetProductDetailByIdQuery, ProductDetailResponse> handler,
+                CancellationToken cancellationToken) =>
+            {
+                GetProductDetailByIdQuery query = new(id);
+
+                Result<ProductDetailResponse> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Products.Read)
+            .WithTags(Tags.Products);
+        }
+    }
+}
