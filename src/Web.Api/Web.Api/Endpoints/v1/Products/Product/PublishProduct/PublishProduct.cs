472fa0ca808d4fff74092655622c9c0d21a57ce3
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-25 06:48:12 -0300

    Add PublishProduct endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Product/PublishProduct/PublishProduct.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.Products.PublishProduct;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Product.PublishProduct
+{
+    internal sealed class PublishProduct : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPatch("products/{id:long}/publish", async (
+                long id,
+                ICommandHandler<PublishProductCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                PublishProductCommand command = new(id);
+
+                Result result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Products.Publish)
+            .WithTags(Tags.Products);
+        }
+    }
+}
