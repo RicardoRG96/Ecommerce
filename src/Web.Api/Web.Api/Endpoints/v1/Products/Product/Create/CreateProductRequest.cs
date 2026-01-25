0d1064246322a2bafe03bc934de53b992a6fb19b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-25 06:32:57 -0300

    Add CreateProductRequest

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Product/Create/CreateProductRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Web.Api.Endpoints.v1.Products.Product.Create
+{
+    public sealed record CreateProductRequest(
+        string Name,
+        string Description,
+        string ShortDescription,
+        long BrandId,
+        long CategoryId,
+        long ProductTaxCategoryId,
+        bool IsActive,
+        bool IsFeatured,
+        bool IsDigital,
+        string MetaTitle,
+        string MetaDescription,
+        string MetaKeywords);
+}
