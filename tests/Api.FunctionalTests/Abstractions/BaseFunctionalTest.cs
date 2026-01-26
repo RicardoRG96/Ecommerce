b3e9049855cffffc5abfacd88844dbc9b1f58207
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-26 07:54:24 -0300

    Refactor to ActivateBrandTests

--- tests/Api.FunctionalTests/Abstractions/BaseFunctionalTest.cs
+++ tests/Api.FunctionalTests/Abstractions/BaseFunctionalTest.cs
@@ -16,8 +16,6 @@ namespace Api.FunctionalTests.Abstractions
 
         protected const string rolesBaseUrl = "api/v1/admin/roles";
 
-        protected const string brandsBaseUrl = "api/v1/brands";
-
         public BaseFunctionalTest(FunctionalTestWebAppFactory factory)
         {
             HttpClient = factory.AuthenticatedClient;
