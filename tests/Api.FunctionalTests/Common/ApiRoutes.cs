e06913fe4bd5ebe2352ab59a1e6c2930d53e1d23
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-26 07:41:19 -0300

    Add ApiRoutes

---
+++ tests/Api.FunctionalTests/Common/ApiRoutes.cs
@@ -0,0 +1,47 @@
+﻿namespace Api.FunctionalTests.Common
+{
+    public static class ApiRoutes
+    {
+        private const string ApiBase = "api/v1";
+
+        public static class Users
+        {
+            public const string Base = $"{ApiBase}/users";
+            public const string ById = $"{Base}/{{id}}";
+        }
+
+        public static class Products
+        {
+            public const string Base = $"{ApiBase}/products";
+            public const string ById = $"{Base}/{{id}}";
+        }
+
+        public static class Addresses
+        {
+            public const string Base = $"{ApiBase}/addresses";
+            public const string ById = $"{Base}/{{id}}";
+        }
+
+        public static class Locations
+        {
+            public const string Countries = $"{ApiBase}/countries";
+            public const string Regions = $"{ApiBase}/regions";
+            public const string Municipalities = $"{ApiBase}/municipalities";
+        }
+
+        public static class Admin
+        {
+            public const string Roles = $"{ApiBase}/admin/roles";
+        }
+
+        public static class Brands
+        {
+            public const string Base = $"{ApiBase}/brands";
+        }
+
+        public static class Categories
+        {
+            public const string Base = $"{ApiBase}/categories";
+        }
+    }
+}
