28374ffd894361cf4df6f9bf2ea4473f7bf1ba36
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 18:07:20 -0300

    Add Create attribute endpoint

--- src/Web.Api/Web.Api/Endpoints/v1/Products/Attribute/Create/CreateAttributeRequest.cs
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Attribute/Create/CreateAttributeRequest.cs
@@ -1,15 +1,13 @@
 ﻿namespace Web.Api.Endpoints.v1.Products.Attribute.Create
 {
-    public sealed class CreateAttributeRequest
-    {
-        public string? Code { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public string? DataType { get; set; }
-        public bool IsVariant { get; set; }
-        public bool IsFilterable { get; set; }
-        public bool IsRequired { get; set; }
-        public int DisplayOrder { get; set; }
-        public bool IsActive { get; set; }
-    }
+    public sealed record CreateAttributeRequest(
+        string Code,
+        string Name,
+        string Description,
+        string DataType,
+        bool IsVariant,
+        bool IsFilterable,
+        bool IsRequired,
+        int DisplayOrder,
+        bool IsActive);
 }
