f76b9dddb5a8d86a4ab096062a9cf01dbe1b0612
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 18:02:17 -0300

    Add CreateAttributeRequest

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Attribute/Create/CreateAttributeRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.Api.Endpoints.v1.Products.Attribute.Create
+{
+    public sealed class CreateAttributeRequest
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? DataType { get; set; }
+        public bool IsVariant { get; set; }
+        public bool IsFilterable { get; set; }
+        public bool IsRequired { get; set; }
+        public int DisplayOrder { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
