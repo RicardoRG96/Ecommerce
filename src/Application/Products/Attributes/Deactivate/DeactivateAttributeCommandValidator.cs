1c5253aa7434915b2556532f8ac5b94238484ce7
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 16:26:31 -0300

    Add DeactivateAttributeCommandValidator

---
+++ src/Application/Products/Attributes/Deactivate/DeactivateAttributeCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Attributes.Deactivate
+{
+    public sealed class DeactivateAttributeCommandValidator : AbstractValidator<DeactivateAttributeCommand>
+    {
+        public DeactivateAttributeCommandValidator()
+        {
+            RuleFor(c => c.AttributeId)
+                .NotEmpty().WithMessage("AttributeId is required.")
+                .GreaterThan(0).WithMessage("AttributeId must be greater than zero.");
+        }
+    }
+}
