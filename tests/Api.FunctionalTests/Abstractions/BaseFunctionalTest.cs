d90c14a89c0514e744cc99a82675c9001a1a4f3b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 10:00:40 -0300

    Refactor to Countries functional tests

--- tests/Api.FunctionalTests/Abstractions/BaseFunctionalTest.cs
+++ tests/Api.FunctionalTests/Abstractions/BaseFunctionalTest.cs
@@ -4,10 +4,6 @@ namespace Api.FunctionalTests.Abstractions
 {
     public class BaseFunctionalTest : IClassFixture<FunctionalTestWebAppFactory>
     {
-        protected const string usersBaseUrl = "api/v1/users";
-
-        protected const string countriesBaseUrl = "api/v1/countries";
-
         public BaseFunctionalTest(FunctionalTestWebAppFactory factory)
         {
             HttpClient = factory.AuthenticatedClient;
