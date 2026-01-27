313f1ce67ede70351aad4e5180b227895cf49023
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 15:20:54 -0300

    Add CreateAttributeCommand

---
+++ src/Application/Products/Attributes/Create/CreateAttributeCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.Attributes.Create
+{
+    public sealed record CreateAttributeCommand(
+        string Code,
+        string Name,
+        string Description,
+        string DataType,
+        bool IsVariant,
+        bool IsFilterable,
+        bool IsRequired,
+        int DisplayOrder,
+        bool IsActive) : ICommand<long>;
+}
