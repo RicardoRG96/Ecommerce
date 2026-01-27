62e3464f2e77f582bf88109b2ce8f2fd81f219c1
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 18:58:30 -0300

    Add DeactivateAttribute endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Attribute/Deactivate/DeactivateAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.Attributes.Deactivate;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Attribute.Deactivate
+{
+    internal sealed class DeactivateAttribute : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPatch("attributes/{id:long}/deactivate", async (
+                long id,
+                ICommandHandler<DeactivateAttributeCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                DeactivateAttributeCommand command = new(id);
+
+                Result result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Attributes.Deactivate)
+            .WithTags(Tags.Attributes);
+        }
+    }
+}
