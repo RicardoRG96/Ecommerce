648ae8b380c3ccb0fbad3375580d887d82488506
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 15:24:31 -0300

    Add CreateAttributeCommandValidator

---
+++ src/Application/Products/Attributes/Create/CreateAttributeCommandValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Attributes.Create
+{
+    public class CreateAttributeCommandValidator : AbstractValidator<CreateAttributeCommand>
+    {
+        public CreateAttributeCommandValidator()
+        {
+            RuleFor(x => x.Code)
+                .NotEmpty().WithMessage("Code is required.")
+                .MaximumLength(50).WithMessage("Code must not exceed 50 characters.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(255).WithMessage("Description must not exceed 255 characters.");
+
+            RuleFor(x => x.DataType)
+                .NotEmpty().WithMessage("DataType is required.")
+                .MaximumLength(20).WithMessage("DataType must not exceed 20 characters.");
+
+            RuleFor(x => x.IsVariant)
+                .NotNull().WithMessage("IsVariant is required.");
+
+            RuleFor(x => x.IsFilterable)
+                .NotNull().WithMessage("IsFilterable is required.");
+
+            RuleFor(x => x.IsRequired)
+                .NotNull().WithMessage("IsRequired is required.");
+
+            RuleFor(x => x.DisplayOrder).NotNull()
+                .GreaterThanOrEqualTo(0).WithMessage("DisplayOrder must be greater than or equal to 0.");
+
+            RuleFor(x => x.IsActive)
+                .NotNull().WithMessage("IsActive is required.");
+        }
+    }
+}
