973fbab9b25b24807976635d3b3ca73aa9bbccd0
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 15:15:11 -0300

    Add AttributeRepository

---
+++ src/Application/Abstractions/Data/Repositories/Products/IAttributeRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Abstractions.Data.Repositories.Products
+{
+    public interface IAttributeRepository : IRepository<Domain.Entities.Products.Attribute>
+    {
+        Task<List<Domain.Entities.Products.Attribute>> GetAllAsync();
+    }
+}
---
+++ src/Infrastructure/Persistence/Repositories/Products/AttributeRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Infrastructure.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories.Products
+{
+    public class AttributeRepository : Repository<Domain.Entities.Products.Attribute>, IAttributeRepository
+    {
+        public AttributeRepository(ApplicationDbContext context) 
+            : base(context)
+        {
+        }
+
+        public async Task<List<Domain.Entities.Products.Attribute>> GetAllAsync()
+        {
+            return await _context.Attributes
+                .Include(a => a.AttributeValues)
+                .ToListAsync();
+        }
+    }
+}
