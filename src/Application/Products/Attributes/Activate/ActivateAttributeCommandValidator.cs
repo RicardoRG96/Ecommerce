3963a22918394b046f985d590c782f3c2d4376b1
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-27 16:20:51 -0300

    Add ActivateAttributeCommandValidator

---
+++ src/Application/Products/Attributes/Activate/ActivateAttributeCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Attributes.Activate
+{
+    public sealed class ActivateAttributeCommandValidator : AbstractValidator<ActivateAttributeCommand>
+    {
+        public ActivateAttributeCommandValidator()
+        {
+            RuleFor(x => x.AttributeId).NotEmpty().WithMessage("AttributeId is required.")
+                .GreaterThan(0).WithMessage("AttributeId must be greater than zero.");
+        }
+    }
+}
