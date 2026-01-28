f95c02ab8e74fd458dcbd8dce74d0c0dfba9e377
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 11:14:50 -0300

    Add implementation of CreateAttributeValueCommandHandler

--- src/Application/Products/AttributeValues/Create/CreateAttributeValueCommandHandler.cs
+++ src/Application/Products/AttributeValues/Create/CreateAttributeValueCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Abstractions.Data.Repositories.Products;
 using Application.Abstractions.Data.UnitOfWork;
 using Application.Abstractions.Messaging;
+using Application.Products.AttributeValues.Common.Services;
+using Domain.Entities.Products;
 using SharedKernel;
 
 namespace Application.Products.AttributeValues.Create
@@ -8,11 +10,53 @@ namespace Application.Products.AttributeValues.Create
     internal sealed class CreateAttributeValueCommandHandler : ICommandHandler<CreateAttributeValueCommand, long>
     {
         private readonly IAttributeValueRepository _attributeValueRepository;
+        private readonly IAttributeValueValidator _validator;
         private readonly IUnitOfWork _unitOfWork;
 
-        public Task<Result<long>> Handle(CreateAttributeValueCommand command, CancellationToken cancellationToken)
+        public CreateAttributeValueCommandHandler(
+            IAttributeValueRepository attributeValueRepository, 
+            IAttributeValueValidator validator, 
+            IUnitOfWork unitOfWork)
         {
-            throw new NotImplementedException();
+            _attributeValueRepository = attributeValueRepository;
+            _validator = validator;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<long>> Handle(CreateAttributeValueCommand command, CancellationToken cancellationToken)
+        {
+            Result attributeEntityValidation = await _validator.ValidateAttributeIsActiveAsync(
+                command.AttributeId, 
+                cancellationToken);
+
+            if (attributeEntityValidation.IsFailure)
+            {
+                return Result.Failure<long>(attributeEntityValidation.Error);
+            }
+
+            Result valueValidation = await _validator.ValidateAttributeValueIsUniqueAsync(
+                command.AttributeId, 
+                command.Value, 
+                cancellationToken: cancellationToken);
+
+            if (valueValidation.IsFailure)
+            {
+                return Result.Failure<long>(valueValidation.Error);
+            }
+
+            AttributeValue attributeValue = AttributeValue.Create(
+                command.AttributeId, 
+                command.Value, 
+                command.NumericValue, 
+                command.BooleanValue, 
+                command.DisplayOrder, 
+                command.IsActive);
+
+            await _attributeValueRepository.AddAsync(attributeValue, cancellationToken);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success(attributeValue.Id);
         }
     }
 }
