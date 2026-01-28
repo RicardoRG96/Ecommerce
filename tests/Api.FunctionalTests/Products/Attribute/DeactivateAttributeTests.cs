5564de3c86d44dd7a6f5a97b8f90e00fd6aace19
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 07:15:16 -0300

    Add DeactivateAttributeTests

---
+++ tests/Api.FunctionalTests/Products/Attribute/DeactivateAttributeTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Attribute
+{
+    public class DeactivateAttributeTests : BaseFunctionalTest
+    {
+        public DeactivateAttributeTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
