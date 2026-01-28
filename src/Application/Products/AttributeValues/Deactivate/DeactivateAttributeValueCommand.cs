bef91410f28f8a9f2b0095d3b7984a44dab9e23f
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 11:33:58 -0300

    Add DeactivateAttributeValueCommand

---
+++ src/Application/Products/AttributeValues/Deactivate/DeactivateAttributeValueCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.AttributeValues.Deactivate
+{
+    public sealed record DeactivateAttributeValueCommand(long Id) : ICommand;
+}
