991b153c8a93f960052c096b268e972ab8ce93e4
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 11:17:49 -0300

    Add UpdateAttributeValueCommand

---
+++ src/Application/Products/AttributeValues/Update/UpdateAttributeValueCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.AttributeValues.Update
+{
+    public sealed record UpdateAttributeValueCommand(
+        long Id,
+        long AttributeId,
+        string Value,
+        decimal? NumericValue,
+        bool? BooleanValue,
+        int DisplayOrder) : ICommand;
+}
