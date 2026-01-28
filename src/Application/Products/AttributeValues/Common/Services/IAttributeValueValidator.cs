e0e4c3652a09b810203b75a7e06a5192c26eaf0a
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 10:24:08 -0300

    Add IAttributeValueValidator

---
+++ src/Application/Products/AttributeValues/Common/Services/IAttributeValueValidator.cs
@@ -0,0 +1,22 @@
+﻿using SharedKernel;
+
+namespace Application.Products.AttributeValues.Common.Services
+{
+    public interface IAttributeValueValidator
+    {
+        Task<Result> ValidateAttributeIsActiveAsync(
+            long attributeId,
+            long? excludeAttributeValueId = null,
+            CancellationToken cancellationToken = default);
+
+        Task<Result> ValidateAttributeValueIsUniqueAsync(
+            long attributeId,
+            string value,
+            long? excludeAttributeValueId = null,
+            CancellationToken cancellationToken = default);
+
+        Task<Result> ValidateSkuIsDisabledBeforeDeactivation(
+            long attributeValueId,
+            CancellationToken cancellationToken);
+    }
+}
---
+++ src/Application/Products/AttributeValues/Create/CreateAttributeValueCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Abstractions.Messaging;
+using SharedKernel;
+
+namespace Application.Products.AttributeValues.Create
+{
+    internal sealed class CreateAttributeValueCommandHandler : ICommandHandler<CreateAttributeValueCommand, long>
+    {
+        private readonly IAttributeValueRepository _attributeValueRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Task<Result<long>> Handle(CreateAttributeValueCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
