ce21a54374d231d631330ea2ad4da8bf6de42f42
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 11:56:49 -0300

    Add GetValuesByAttributeQuery

---
+++ src/Application/Products/AttributeValues/GetValuesByAttribute/GetValuesByAttributeQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.AttributeValues.Common.Mappers;
+
+namespace Application.Products.AttributeValues.GetValuesByAttribute
+{
+    public sealed record GetValuesByAttributeQuery(long AttributeId) 
+        : IQuery<List<AttributeValueResponse>>;
+}
