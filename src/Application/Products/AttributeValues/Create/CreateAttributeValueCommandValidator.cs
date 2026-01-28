b4216a90cedbcd9b1ffb68ed451c7d7dbd7f5d0a
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 09:59:57 -0300

    Add CreateAttributeValueCommandValidator

---
+++ src/Application/Products/AttributeValues/Create/CreateAttributeValueCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace Application.Products.AttributeValues.Create
+{
+    public sealed class CreateAttributeValueCommandValidator : AbstractValidator<CreateAttributeValueCommand>
+    {
+        public CreateAttributeValueCommandValidator()
+        {
+            RuleFor(c => c.AttributeId)
+                .NotEmpty().WithMessage("AttributeId is required.")
+                .GreaterThan(0).WithMessage("AttributeId must be greater than zero.");
+
+            RuleFor(c => c.Value)
+                .NotEmpty().WithMessage("Value is required.")
+                .MaximumLength(100).WithMessage("Value must not exceed 100 characters.");
+
+            RuleFor(c => c.DisplayOrder)
+                .NotEmpty().WithMessage("DisplayOrder is required.")
+                .GreaterThanOrEqualTo(0).WithMessage("DisplayOrder must be zero or greater.");
+
+            RuleFor(c => c.IsActive)
+                .NotNull().WithMessage("IsActive is required.");
+        }
+    }
+}
