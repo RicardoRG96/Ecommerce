87e042c62cd5377b2672a8bff24ab6fb3ec472a5
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 07:57:54 -0300

    Add GetAllAttributesTests

---
+++ tests/Api.FunctionalTests/Products/Attribute/GetAllAttributesTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Attribute
+{
+    public class GetAllAttributesTests : BaseFunctionalTest
+    {
+        public GetAllAttributesTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
