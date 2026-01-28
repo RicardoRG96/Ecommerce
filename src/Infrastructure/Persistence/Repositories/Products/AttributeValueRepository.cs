dac18e42eb7855975468a77709b133fbbcd3a8cf
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 09:47:31 -0300

    Add AttributeValueRepository

---
+++ src/Infrastructure/Persistence/Repositories/Products/AttributeValueRepository.cs
@@ -0,0 +1,23 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Domain.Entities.Products;
+using Infrastructure.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories.Products
+{
+    public class AttributeValueRepository : Repository<AttributeValue>, IAttributeValueRepository
+    {
+        public AttributeValueRepository(ApplicationDbContext context) 
+            : base(context)
+        {
+        }
+
+        public async Task<List<AttributeValue>> GetValuesByAttributeAsync(long attributeId, CancellationToken cancellationToken)
+        {
+            return await _context.AttributeValues
+                .Where(av => av.AttributeId == attributeId)
+                .Include(av => av.ProductAttributeValues)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
