1fb02a78a55e4e109ffce7c2a18e44dda3479f74
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-28 11:27:02 -0300

    Add ActivateAttributeValueCommandValidator

---
+++ src/Application/Products/AttributeValues/Activate/ActivateAttributeValueCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Products.AttributeValues.Activate
+{
+    public sealed class ActivateAttributeValueCommandValidator : AbstractValidator<ActivateAttributeValueCommand>
+    {
+        public ActivateAttributeValueCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotEmpty().WithMessage("Attribute value ID must be provided.")
+                .GreaterThan(0).WithMessage("Attribute value ID must be greater than zero.");
+        }
+    }
+}
