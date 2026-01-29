219f741fa1a9f71e31014c2b2f78718a8a40bd69
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 18:34:53 -0300

    Add SkuGenerationContext

---
+++ src/Application/Products/ProductSkus/Common/Services/SkuGenerationContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Products.ProductSkus.Common.Services
+{
+    public sealed class SkuGenerationContext
+    {
+        public long ProductSkuId { get; set; }
+        public string? CategoryCode { get; set; } = default!;
+        public string? BrandCode { get; init; } = default!;
+        public IReadOnlyDictionary<string, string>? Variants { get; init; }
+    }
+}
