60cd65a876fe05fd0bdf2efa7a463be8cd0e5194
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 14:50:31 -0300

    Add GetAttributeValueByIdTests

---
+++ tests/Api.FunctionalTests/Products/AttributeValue/GetAttributeValueByIdTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.AttributeValue
+{
+    public class GetAttributeValueByIdTests : BaseFunctionalTest
+    {
+        public GetAttributeValueByIdTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
