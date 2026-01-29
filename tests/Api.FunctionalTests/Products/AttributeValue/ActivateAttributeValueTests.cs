edf88f0cb9cd64fa7e37a80e0c107f3e5c0beecb
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 13:45:29 -0300

    Add ActivateAttributeValueTests

---
+++ tests/Api.FunctionalTests/Products/AttributeValue/ActivateAttributeValueTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.AttributeValue
+{
+    public class ActivateAttributeValueTests : BaseFunctionalTest
+    {
+        public ActivateAttributeValueTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
