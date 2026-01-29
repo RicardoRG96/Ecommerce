ed30b2e1cd6dab44cf4f5d0f4c3ac3c195623fc9
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 18:15:17 -0300

    Add CreateProductSkuCommand

---
+++ src/Application/Products/ProductSkus/Create/CreateProductSkuCommand.cs
@@ -0,0 +1,17 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.ProductSkus.Create
+{
+    public sealed record CreateProductSkuCommand(
+        long ProductId,
+        string SkuCode,
+        string? BarCode,
+        decimal Price,
+        decimal? Cost,
+        decimal? Weight,
+        decimal? Length,
+        decimal? Width,
+        decimal? Height,
+        bool IsActive,
+        int DisplayOrder) : ICommand<long>;
+}
