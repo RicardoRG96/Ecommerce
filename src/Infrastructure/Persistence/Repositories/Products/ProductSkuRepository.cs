cbcc026fb736a764c3c8bbe4a0293dea388dbeb4
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 15:38:29 -0300

    Add ProductSkuRepository

---
+++ src/Infrastructure/Persistence/Repositories/Products/ProductSkuRepository.cs
@@ -0,0 +1,43 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Domain.Entities.Products;
+using Infrastructure.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories.Products
+{
+    public class ProductSkuRepository : Repository<ProductSku>, IProductSkuRepository
+    {
+        public ProductSkuRepository(ApplicationDbContext context) 
+            : base(context)
+        {
+        }
+
+        public Task<ProductSku?> GetByIdWithRelatedEntitiesAsync(long id, CancellationToken cancellationToken)
+        {
+            return _context.ProductSkus
+                .Where(sku => sku.Id == id)
+                .Include(sku => sku.ProductAttributeValues)
+                .ThenInclude(attrValue => attrValue.AttributeValue)
+                .Include(sku => sku.DiscountSkus)
+                .ThenInclude(discountSku => discountSku.Discount)
+                .Include(sku => sku.ProductGalleries)
+                .Include(sku => sku.ProductSkuStocks)
+                .ThenInclude(stock => stock.Warehouse)
+                .FirstOrDefaultAsync(cancellationToken);    
+        }
+
+        public async Task<List<ProductSku>?> GetSkusByProductIdAsync(long productId, CancellationToken cancellationToken)
+        {
+            return await _context.ProductSkus
+                .Where(sku => sku.ProductId == productId)
+                .Include(sku => sku.ProductAttributeValues)
+                .ThenInclude(attrValue => attrValue.AttributeValue)
+                .Include(sku => sku.DiscountSkus)
+                .ThenInclude(discountSku => discountSku.Discount)
+                .Include(sku => sku.ProductGalleries)
+                .Include(sku => sku.ProductSkuStocks)
+                .ThenInclude(stock => stock.Warehouse)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
