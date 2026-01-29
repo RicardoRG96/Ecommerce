9e588a3b1c117953f8746cad0abcbe01d8c0a706
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 12:19:42 -0300

    Add CreateAttributeValueTests

---
+++ tests/Api.FunctionalTests/Products/AttributeValue/CreateAttributeValueTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.AttributeValue
+{
+    public class CreateAttributeValueTests : BaseFunctionalTest
+    {
+        public CreateAttributeValueTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
