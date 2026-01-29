c691415b0aa9d4fb14d720cc7abae0a468bdfce1
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 12:46:08 -0300

    Add UpdateAttributeValueTests

---
+++ tests/Api.FunctionalTests/Products/AttributeValue/UpdateAttributeValueTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.AttributeValue
+{
+    public class UpdateAttributeValueTests : BaseFunctionalTest
+    {
+        public UpdateAttributeValueTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
