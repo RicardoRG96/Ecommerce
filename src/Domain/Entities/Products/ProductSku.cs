195ef316ac5595f3b4382cee205127b57ae9a028
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 15:57:40 -0300

    Refactor to ProductSku

--- src/Domain/Entities/Products/ProductSku.cs
+++ src/Domain/Entities/Products/ProductSku.cs
@@ -9,12 +9,12 @@ namespace Domain.Entities.Products
         public string? SkuCode { get; set; }
         public string? BarCode { get; set; }
         public decimal Price { get; set; }
-        public decimal ComparedAtPrice { get; set; }
-        public decimal Cost { get; set; }
-        public decimal Weight { get; set; }
-        public decimal Length { get; set; }
-        public decimal Width { get; set; }
-        public decimal Height { get; set; }
+        public decimal? ComparedAtPrice { get; set; }
+        public decimal? Cost { get; set; }
+        public decimal? Weight { get; set; }
+        public decimal? Length { get; set; }
+        public decimal? Width { get; set; }
+        public decimal? Height { get; set; }
         public bool IsActive { get; set; }
         public int DisplayOrder { get; set; }
         public Product Product { get; set; } = null!;
