811d2541d2167c5401cd2931f1c7f86734b18aaf
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 17:44:13 -0300

    Add IProductSkuValidator

---
+++ src/Application/Products/ProductSkus/Common/Services/IProductSkuValidator.cs
@@ -0,0 +1,29 @@
+﻿using SharedKernel;
+
+namespace Application.Products.ProductSkus.Common.Services
+{
+    public interface IProductSkuValidator
+    {
+        Task<Result> ValidateProductIsPublishedAsync(
+            long productId, 
+            CancellationToken cancellationToken);
+
+        Task<Result> ValidateSkuCodeIsUnique(
+            string skuCode, 
+            long? excludeProductSkuId = null, 
+            CancellationToken cancellationToken = default);
+
+        Task<Result> ValidateBarCodeIsUnique(
+            string? barCode,
+            long? excludeProductSkuId = null,
+            CancellationToken cancellationToken = default);
+
+        Task<Result> ValidateAttributeValueIsActive(
+            long productSkuId,
+            CancellationToken cancellationToken);
+
+        Task<Result> ValidateSkuHasNoActiveOrders(
+            long productSkuId,
+            CancellationToken cancellationToken);
+    }
+}
