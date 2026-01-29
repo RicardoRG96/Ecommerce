e311c65ebe83770036c893aa47338a413974b947
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-29 11:47:38 -0300

    Add CreateAttributeValueRequest

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/AttributeValue/Create/CreateAttributeValueRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Api.Endpoints.v1.Products.AttributeValue.Create
+{
+    public sealed record CreateAttributeValueRequest(
+        long AttributeId,
+        string Value,
+        decimal? NumericValue,
+        bool? BooleanValue,
+        int DisplayOrder,
+        bool IsActive);
+}
