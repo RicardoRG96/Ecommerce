3648cb45b7ea4d962feae96d2b239b0874566706
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-30 07:41:31 -0300

    Add UpdateProductSkuCommand

---
+++ src/Application/Products/ProductSkus/Update/UpdateProductSkuCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.ProductSkus.Update
+{
+    public sealed record UpdateProductSkuCommand(
+        long ProductId,
+        string? BarCode,
+        decimal Price,
+        decimal? Cost,
+        decimal? Weight,
+        decimal? Length,
+        decimal? Width,
+        decimal? Height,
+        int DisplayOrder) : ICommand;
+}
