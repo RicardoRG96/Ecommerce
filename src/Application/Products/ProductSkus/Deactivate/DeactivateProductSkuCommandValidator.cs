b624bf4455dbdf2691fe59b692f5fbb4ddc057ad
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-30 08:18:42 -0300

    Add DeactivateProductSkuCommandValidator

---
+++ src/Application/Products/ProductSkus/Deactivate/DeactivateProductSkuCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Products.ProductSkus.Deactivate
+{
+    public sealed class DeactivateProductSkuCommandValidator : AbstractValidator<DeactivateProductSkuCommand>
+    {
+        public DeactivateProductSkuCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotEmpty().WithMessage("Product SKU ID is required.")
+                .GreaterThan(0).WithMessage("Product SKU ID must be greater than zero.");
+        }
+    }
+}
