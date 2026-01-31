3d90b99d20e702de0ec170e1adb0a1b6f44cccb1
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-31 07:44:35 -0300

    Add GetProductSkuByIdQuery

---
+++ src/Application/Products/ProductSkus/GetById/GetProductSkuByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.ProductSkus.GetById
+{
+    public sealed record GetProductSkuByIdQuery(long Id) : IQuery<ProductSkuResponse>;
+}
