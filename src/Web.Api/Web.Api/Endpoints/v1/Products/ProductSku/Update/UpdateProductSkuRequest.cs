8e5642010238686777d7e99d22b514fd574688bf
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-31 12:48:07 -0300

    Add UpdateProductSkuRequest

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/ProductSku/Update/UpdateProductSkuRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Api.Endpoints.v1.Products.ProductSku.Update
+{
+    public sealed record UpdateProductSkuRequest(
+        long ProductId,
+        string? BarCode,
+        decimal Price,
+        decimal? Cost,
+        decimal? Weight,
+        decimal? Length,
+        decimal? Width,
+        decimal? Height,
+        int DisplayOrder);
+}
