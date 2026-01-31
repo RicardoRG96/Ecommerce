0d2b63144bdc3c1f7fc16b99a4fe99e5660e59c8
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-31 11:51:40 -0300

    Add Create productSku endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/ProductSku/Create/Create.cs
@@ -0,0 +1,39 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.ProductSkus.Create;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.ProductSku.Create
+{
+    internal sealed class Create : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("skus", async (
+                CreateProductSkuRequest request,
+                ICommandHandler<CreateProductSkuCommand, long> handler,
+                CancellationToken cancellationToken) =>
+            {
+                CreateProductSkuCommand command = new(
+                    ProductId: request.ProductId,
+                    BarCode: request.BarCode,
+                    Price: request.Price,
+                    Cost: request.Cost,
+                    Weight: request.Weight,
+                    Length: request.Length,
+                    Width: request.Width,
+                    Height: request.Height,
+                    IsActive: request.IsActive,
+                    DisplayOrder: request.DisplayOrder);
+
+                Result<long> result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.ProductSkus.Create)
+            .WithTags(Tags.ProductSkus);
+        }
+    }
+}
