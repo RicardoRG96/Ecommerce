5b19010743821e7bc96916d8e4458235e13830b7
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-31 11:52:33 -0300

    Add CreateProductSkuTests

---
+++ tests/Api.FunctionalTests/Products/ProductSku/CreateProductSkuTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.ProductSku
+{
+    public class CreateProductSkuTests : BaseFunctionalTest
+    {
+        public CreateProductSkuTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
