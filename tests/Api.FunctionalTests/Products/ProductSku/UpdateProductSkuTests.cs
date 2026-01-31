cf4c783ddbf52a30becdf8e8ab1c8c9579875554
RicardoRG96 <ricardoretamal10@gmail.com>
2026-01-31 12:57:25 -0300

    Add UpdateProductSkuTests

---
+++ tests/Api.FunctionalTests/Products/ProductSku/UpdateProductSkuTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.ProductSku
+{
+    public class UpdateProductSkuTests : BaseFunctionalTest
+    {
+        public UpdateProductSkuTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
