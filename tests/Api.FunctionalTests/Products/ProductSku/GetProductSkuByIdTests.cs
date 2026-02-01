b02430e8232462d0ef6dd492e1792b9dd70c447b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-01 08:13:02 -0300

    Add GetProductSkuByIdTests

---
+++ tests/Api.FunctionalTests/Products/ProductSku/GetProductSkuByIdTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.ProductSku
+{
+    public class GetProductSkuByIdTests : BaseFunctionalTest
+    {
+        public GetProductSkuByIdTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
