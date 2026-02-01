d6a19376d7a1d6775484f47a4b70200dbadfd68d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-01 07:29:22 -0300

    Add ActivateProductSkuTests

---
+++ tests/Api.FunctionalTests/Products/ProductSku/ActivateProductSkuTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.ProductSku
+{
+    public class ActivateProductSkuTests : BaseFunctionalTest
+    {
+        public ActivateProductSkuTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
