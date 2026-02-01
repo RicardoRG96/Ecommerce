d3fdbf0174cf932c4bb287716a45d91b659d775d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-01 07:52:21 -0300

    Add DeactivateProductSkuTests

---
+++ tests/Api.FunctionalTests/Products/ProductSku/DeactivateProductSkuTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.ProductSku
+{
+    public class DeactivateProductSkuTests : BaseFunctionalTest
+    {
+        public DeactivateProductSkuTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
