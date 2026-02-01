eae28a37d7456b4f6a90ba2c0e6a2847dc55a0a0
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-01 07:28:36 -0300

    Add ActivateProductSku endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/ProductSku/Activate/ActivateProductSku.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Products.ProductSkus.Activate;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.ProductSku.Activate
+{
+    internal sealed class ActivateProductSku : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPatch("skus/{id:long}/activate", async (
+                long id,
+                ICommandHandler<ActivateProductSkuCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                ActivateProductSkuCommand command = new(id);
+
+                Result result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.ProductSkus.Activate)
+            .WithTags(Tags.ProductSkus);
+        }
+    }
+}
