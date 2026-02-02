c30a54f8c844296695d987eaf420c4948c89fe2a
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 14:41:00 -0300

    Add ISearchService

---
+++ src/Application/Abstractions/Search/ISearchService.cs
@@ -0,0 +1,28 @@
+﻿namespace Application.Abstractions.Search
+{
+    public interface ISearchService
+    {
+        Task InitializeIndexAsync(CancellationToken cancellationToken = default);
+
+        Task IndexProductsAsync(
+            IEnumerable<ProductSearchModel> products, 
+            CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<ProductSearchModel>> SearchProductsAsync(
+            string query, 
+            CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<ProductSearchModel>> FilterByBrandAsync(
+            List<string> brands, 
+            CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<ProductSearchModel>> FilterByCategoryAsync(
+            List<string> categories, 
+            CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<ProductSearchModel>> FilterByPriceRange(
+            decimal minPrice,
+            decimal maxPrice,
+            CancellationToken cancellationToken = default);
+    }
+}
