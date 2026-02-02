144934a59aba5e870cec9ea7a1130f0cd6039416
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 10:50:08 -0300

    Add AssignAttributeValueToSkuTests

---
+++ tests/Api.FunctionalTests/Products/ProductAttributeValue/AssignAttributeValueToSkuTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.ProductAttributeValue
+{
+    public class AssignAttributeValueToSkuTests : BaseFunctionalTest
+    {
+        public AssignAttributeValueToSkuTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
