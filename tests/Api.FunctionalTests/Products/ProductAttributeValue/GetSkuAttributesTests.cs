2eb91890449a009dc68ae7e9fbf3abe6b712e205
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 11:36:35 -0300

    Add GetSkuAttributesTests

---
+++ tests/Api.FunctionalTests/Products/ProductAttributeValue/GetSkuAttributesTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.ProductAttributeValue
+{
+    public class GetSkuAttributesTests : BaseFunctionalTest
+    {
+        public GetSkuAttributesTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
