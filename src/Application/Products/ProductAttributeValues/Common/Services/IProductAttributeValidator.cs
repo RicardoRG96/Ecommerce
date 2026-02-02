835be8a2abac9a9ffde40e0f9f8470c815a86edc
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 06:20:52 -0300

    Add IProductAttributeValidator

---
+++ src/Application/Products/ProductAttributeValues/Common/Services/IProductAttributeValidator.cs
@@ -0,0 +1,10 @@
+﻿using SharedKernel;
+
+namespace Application.Products.ProductAttributeValues.Common.Services
+{
+    public interface IProductAttributeValidator
+    {
+        Task<Result> ValidateAttributeValueIsActive(long attributeValueId, CancellationToken cancellationToken);
+        Task<Result> ValidateSkuIsActive(long productSkuId, CancellationToken cancellationToken);
+    }
+}
