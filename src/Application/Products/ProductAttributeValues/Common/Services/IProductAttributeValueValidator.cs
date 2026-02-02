ea6617b267ad80ff41047334db93f225e8040b3e
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 07:01:29 -0300

    Add ProductAttributeValueValidator

--- src/Application/Products/ProductAttributeValues/Common/Services/IProductAttributeValueValidator.cs
+++ src/Application/Products/ProductAttributeValues/Common/Services/IProductAttributeValueValidator.cs
@@ -2,9 +2,13 @@
 
 namespace Application.Products.ProductAttributeValues.Common.Services
 {
-    public interface IProductAttributeValidator
+    public interface IProductAttributeValueValidator
     {
         Task<Result> ValidateAttributeValueIsActive(long attributeValueId, CancellationToken cancellationToken);
         Task<Result> ValidateSkuIsActive(long productSkuId, CancellationToken cancellationToken);
+        Task<Result> ValidateAttributeValueIsNotAlreadyAssignedToSku(
+            long productSkuId, 
+            long attributeValueId, 
+            CancellationToken cancellationToken);
     }
 }
