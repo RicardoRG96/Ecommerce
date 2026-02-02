2219e53cad667a3cc7b910959256f56604506e50
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 11:15:37 -0300

    Add RemoveAttributeValueFromSkuTests

---
+++ tests/Api.FunctionalTests/Products/ProductAttributeValue/RemoveAttributeValueFromSkuTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.ProductAttributeValue
+{
+    public class RemoveAttributeValueFromSkuTests : BaseFunctionalTest
+    {
+        public RemoveAttributeValueFromSkuTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
