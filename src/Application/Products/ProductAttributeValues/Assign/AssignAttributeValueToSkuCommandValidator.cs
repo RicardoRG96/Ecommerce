d3ac1ba153306271fe073b8b5583cb1b44ea0e6c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 06:16:14 -0300

    Add AssignAttributeValueToSkuCommandValidator

---
+++ src/Application/Products/ProductAttributeValues/Assign/AssignAttributeValueToSkuCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Application.Products.ProductAttributeValues.Assign
+{
+    public sealed class AssignAttributeValueToSkuCommandValidator 
+        : AbstractValidator<AssignAttributeValueToSkuCommand>
+    {
+        public AssignAttributeValueToSkuCommandValidator()
+        {
+            RuleFor(c => c.SkuId)
+                .NotEmpty().WithMessage("SKU ID must be provided.")
+                .GreaterThan(0).WithMessage("SKU ID must be greater than zero.");
+
+            RuleFor(c => c.AttributeValueId)
+                .NotEmpty().WithMessage("Attribute Value ID must be provided.")
+                .GreaterThan(0).WithMessage("Attribute Value ID must be greater than zero.");
+        }
+    }
+}
