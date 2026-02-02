266d917c74187d990f6c761107f3d33f4ac03d30
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 14:48:40 -0300

    Add ProductSearchModel

---
+++ src/Application/Abstractions/Search/ProductSearchModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Abstractions.Search
+{
+    public sealed class ProductSearchModel
+    {
+        public required string ObjectID { get; init; }
+        public required string Name { get; init; }
+        public required string  Slug { get; init; }
+        public required string Description { get; init; }
+        public string? SkuCode { get; init; }
+        public string? Brand { get; init; }
+        public string? Category { get; init; }
+        public decimal Price { get; init; }
+        public string? ImageUrl { get; init; }
+        public Dictionary<string, object>? Attributes { get; init; }
+    }
+}
