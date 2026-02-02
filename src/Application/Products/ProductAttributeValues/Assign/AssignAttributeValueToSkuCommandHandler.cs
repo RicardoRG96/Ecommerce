f13cfb9170f565d5700363bb8f91cd2a70754ca7
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-02 06:18:21 -0300

    Add AssignAttributeValueToSkuCommandHandler

---
+++ src/Application/Products/ProductAttributeValues/Assign/AssignAttributeValueToSkuCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Application.Abstractions.Messaging;
+using SharedKernel;
+
+namespace Application.Products.ProductAttributeValues.Assign
+{
+    internal sealed class AssignAttributeValueToSkuCommandHandler : ICommandHandler<AssignAttributeValueToSkuCommand>
+    {
+        public Task<Result> Handle(AssignAttributeValueToSkuCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
