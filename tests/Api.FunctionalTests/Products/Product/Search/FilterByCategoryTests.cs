b89699bc5786010d60c89cb7dfeb108bf5a7262c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 17:35:28 -0300

    Add FilterByCategoryTests

---
+++ tests/Api.FunctionalTests/Products/Product/Search/FilterByCategoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Product.Search
+{
+    public class FilterByCategoryTests : BaseFunctionalTest
+    {
+        public FilterByCategoryTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
