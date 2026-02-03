2ef4e2da205faa2c1c38b1c3e7625ea384cd2815
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 13:57:38 -0300

    Add FilterByPriceRangeQuery

---
+++ src/Application/Products/Products/FilterByPriceRange/FilterByPriceRangeQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Abstractions.Search;
+
+namespace Application.Products.Products.FilterByPriceRange
+{
+    public sealed record FilterByPriceRangeQuery(
+        int MinPrice,
+        int MaxPrice) : IQuery<IEnumerable<ProductSearchModel>>;
+}
