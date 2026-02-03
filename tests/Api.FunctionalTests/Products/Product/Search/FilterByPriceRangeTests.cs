eb51467e0a63d167242a95287330ecbd6d63dd5c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 17:54:08 -0300

    Add FilterByPriceRangeTests

---
+++ tests/Api.FunctionalTests/Products/Product/Search/FilterByPriceRangeTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Product.Search
+{
+    public class FilterByPriceRangeTests : BaseFunctionalTest
+    {
+        public FilterByPriceRangeTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
