227d15f7ce7ed60ed918e84495c521f1addb60e1
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 12:49:32 -0300

    Add SearchProductQueryValidator

---
+++ src/Application/Products/Products/SearchProduct/SearchProductQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Products.SearchProduct
+{
+    public sealed class SearchProductQueryValidator : AbstractValidator<SearchProductQuery>
+    {
+        public SearchProductQueryValidator()
+        {
+            RuleFor(q => q.Query)
+                .NotEmpty().WithMessage("Search query must not be empty.")
+                .MaximumLength(120).WithMessage("Search query must not exceed 100 characters.");
+        }
+    }
+}
