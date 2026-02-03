11bec4b8d7c70b0d1459a1418d749ccbda71342f
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 13:04:27 -0300

    Add FilterByBrandQuery

---
+++ src/Application/Products/Products/FilterByBrand/FilterByBrandQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Abstractions.Search;
+
+namespace Application.Products.Products.FilterByBrand
+{
+    public sealed record FilterByBrandQuery(List<string> Brands) : IQuery<IEnumerable<ProductSearchModel>>;
+}
--- src/Web.Api/Web.Api/Endpoints/v1/Products/Product/SearchProduct/SearchProduct.cs
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Product/SearchProduct/SearchProduct.cs
@@ -1,7 +1,6 @@
 ﻿using Application.Abstractions.Messaging;
 using Application.Abstractions.Search;
 using Application.Products.Products.SearchProduct;
-using Infrastructure.Access;
 using SharedKernel;
 using Web.Api.Extensions;
 using Web.Api.Infrastructure;
