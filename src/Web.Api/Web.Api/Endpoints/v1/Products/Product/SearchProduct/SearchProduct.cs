db944e7b59103381d63ab454559105ba45267b4c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 12:45:45 -0300

    Add SearchProduct endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Product/SearchProduct/SearchProduct.cs
@@ -0,0 +1,30 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Abstractions.Search;
+using Application.Products.Products.SearchProduct;
+using Infrastructure.Access;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Product.SearchProduct
+{
+    internal sealed class SearchProduct : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products/search", async (
+                string searchQuery,
+                IQueryHandler<SearchProductQuery, IEnumerable<ProductSearchModel>> handler,
+                CancellationToken cancellationToken) =>
+            {
+                SearchProductQuery query = new(searchQuery);
+
+                Result<IEnumerable<ProductSearchModel>> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.Products.Read)
+            .WithTags(Tags.Products);
+        }
+    }
+}
