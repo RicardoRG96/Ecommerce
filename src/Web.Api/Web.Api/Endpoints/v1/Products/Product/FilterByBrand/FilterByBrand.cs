e602c47cf1c1104a160d17845180f986d8fa86d0
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 13:39:14 -0300

    Add FilterByBrand endpoint

---
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Product/FilterByBrand/FilterByBrand.cs
@@ -0,0 +1,34 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Abstractions.Search;
+using Application.Products.Products.FilterByBrand;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.v1.Products.Product.FilterByBrand
+{
+    internal sealed class FilterByBrand : IEndpoint
+    {
+        public record BrandQueriesRequest(
+            [FromQuery] List<string> Brands);
+
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products/filter-by-brand", async (
+                string[] brands,
+                IQueryHandler<FilterByBrandQuery, IEnumerable<ProductSearchModel>> handler,
+                CancellationToken cancellationToken) =>
+            {
+                List<string> formatedQuery = [.. brands];
+
+                FilterByBrandQuery query = new(formatedQuery);
+
+                Result<IEnumerable<ProductSearchModel>> result = await handler.Handle(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .WithTags(Tags.Products);
+        }
+    }
+}
