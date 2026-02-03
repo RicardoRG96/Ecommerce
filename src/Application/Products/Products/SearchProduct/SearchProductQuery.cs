e0a65747596c33b65470aedf02eb9a0baaa21f17
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 12:02:59 -0300

    Add SearchProductQuery

---
+++ src/Application/Products/Products/SearchProduct/SearchProductQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Abstractions.Search;
+
+namespace Application.Products.Products.SearchProduct
+{
+    public sealed record SearchProductQuery(string Query) 
+        : IQuery<IEnumerable<ProductSearchModel>>; 
+}
