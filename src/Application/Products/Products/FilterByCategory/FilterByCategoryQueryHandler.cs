22a7e4a8d535cbd8dceed742fb8be368ea412137
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 13:49:02 -0300

    Add FilterByCategoryQueryHandler

---
+++ src/Application/Products/Products/FilterByCategory/FilterByCategoryQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Abstractions.Search;
+using Domain.Errors.Products;
+using SharedKernel;
+
+namespace Application.Products.Products.FilterByCategory
+{
+    internal sealed class FilterByCategoryQueryHandler : IQueryHandler<FilterByCategoryQuery, IEnumerable<ProductSearchModel>>
+    {
+        private readonly ISearchService _searchService;
+
+        public FilterByCategoryQueryHandler(ISearchService searchService)
+        {
+            _searchService = searchService;
+        }
+
+        public async Task<Result<IEnumerable<ProductSearchModel>>> Handle(FilterByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            IEnumerable<ProductSearchModel> productsResult = await _searchService.FilterByCategoryAsync(
+                query.Categories,
+                cancellationToken);
+
+            if (productsResult is null || !productsResult.Any())
+            {
+                return Result.Failure<IEnumerable<ProductSearchModel>>(ProductErrors.SearchNotFound);
+            }
+
+            return Result.Success(productsResult);
+        }
+    }
+}
