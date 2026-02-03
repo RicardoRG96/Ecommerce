426cc663d345336ddd06727ef47b8af9f754bcca
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 13:44:44 -0300

    Add FilterByBrandQueryValidator

---
+++ src/Application/Products/Products/FilterByBrand/FilterByBrandQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Products.FilterByBrand
+{
+    public sealed class FilterByBrandQueryValidator : AbstractValidator<FilterByBrandQuery>
+    {
+        public FilterByBrandQueryValidator()
+        {
+            RuleFor(q => q.Brands)
+                .NotEmpty().WithMessage("Brands list must not be empty.")
+                .Must(brands => brands.All(brand => !string.IsNullOrWhiteSpace(brand)))
+                .WithMessage("Brands list must not contain empty or whitespace-only strings.");
+        }
+    }
+}
