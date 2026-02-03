174739029d330e4030f9186c42664af0487900ec
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 16:54:48 -0300

    Add FilterByBrandTests

--- src/Web.Api/Web.Api/Endpoints/v1/Products/Product/FilterByBrand/FilterByBrand.cs
+++ src/Web.Api/Web.Api/Endpoints/v1/Products/Product/FilterByBrand/FilterByBrand.cs
@@ -1,7 +1,6 @@
 ﻿using Application.Abstractions.Messaging;
 using Application.Abstractions.Search;
 using Application.Products.Products.FilterByBrand;
-using Microsoft.AspNetCore.Mvc;
 using SharedKernel;
 using Web.Api.Extensions;
 using Web.Api.Infrastructure;
---
+++ tests/Api.FunctionalTests/Products/Product/Search/FilterByBrandTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Product.Search
+{
+    public class FilterByBrandTests : BaseFunctionalTest
+    {
+        public FilterByBrandTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
