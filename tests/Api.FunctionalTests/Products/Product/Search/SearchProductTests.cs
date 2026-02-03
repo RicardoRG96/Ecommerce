1ea3a939d7c8ddc3bb400e3bbfabb15f76bacc09
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-03 16:12:15 -0300

    Add SearchProductTests

---
+++ tests/Api.FunctionalTests/Products/Product/Search/SearchProductTests.cs
@@ -0,0 +1,14 @@
+﻿using Api.FunctionalTests.Abstractions;
+
+namespace Api.FunctionalTests.Products.Product.Search
+{
+    public class SearchProductTests : BaseFunctionalTest
+    {
+        public SearchProductTests(FunctionalTestWebAppFactory factory) 
+            : base(factory)
+        {
+        }
+
+
+    }
+}
