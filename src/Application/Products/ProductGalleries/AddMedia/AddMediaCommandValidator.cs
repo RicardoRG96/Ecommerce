274c185e5c6cbbd6eed0908a3c4567a11ab5e0da
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 11:24:01 -0300

    Add AddMediaCommandValidator

---
+++ src/Application/Products/ProductGalleries/AddMedia/AddMediaCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Products.ProductGalleries.AddMedia
+{
+    public sealed class AddMediaCommandValidator : AbstractValidator<AddMediaCommand>
+    {
+        public AddMediaCommandValidator()
+        {
+            RuleFor(c => c.Stream).NotNull().WithMessage("Stream cannot be null.");
+
+            RuleFor(c => c.FileName).NotEmpty().WithMessage("FileName cannot be empty.");
+
+            RuleFor(c => c.ContentType).NotEmpty().WithMessage("ContentType cannot be empty.");
+        }
+    }
+}
