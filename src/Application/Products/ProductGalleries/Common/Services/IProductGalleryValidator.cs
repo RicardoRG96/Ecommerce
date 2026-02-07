6b00e5c7bf24ba1c6cc4da46639e584e86097eaa
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 12:02:05 -0300

    Add IProductGalleryValidator

---
+++ src/Application/Products/ProductGalleries/Common/Services/IProductGalleryValidator.cs
@@ -0,0 +1,11 @@
+﻿using SharedKernel;
+
+namespace Application.Products.ProductGalleries.Common.Services
+{
+    public interface IProductGalleryValidator
+    {
+        Task<Result> ValidateProductIdExists(long productId, CancellationToken cancellationToken);
+        Task<Result> ValidateSkuIdExists(long? skuId, CancellationToken cancellationToken);
+        Task<Result> ValidateSkuBelongsToProduct(long productId, long? skuId, CancellationToken cancellationToken);
+    }
+}
