40272639567235ba06aa0a0752bcc2301cda5522
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 11:54:34 -0300

    Refactor to AddMediaCommandValidator

--- src/Application/Products/ProductGalleries/AddMedia/AddMediaCommandValidator.cs
+++ src/Application/Products/ProductGalleries/AddMedia/AddMediaCommandValidator.cs
@@ -6,11 +6,25 @@ namespace Application.Products.ProductGalleries.AddMedia
     {
         public AddMediaCommandValidator()
         {
+            RuleFor(c => c.ProductId)
+                .NotNull().WithMessage("ProductId cannot be empty.")
+                .GreaterThan(0).WithMessage("ProductId must be greater than 0.");
+
             RuleFor(c => c.Stream).NotNull().WithMessage("Stream cannot be null.");
 
             RuleFor(c => c.FileName).NotEmpty().WithMessage("FileName cannot be empty.");
 
             RuleFor(c => c.ContentType).NotEmpty().WithMessage("ContentType cannot be empty.");
+
+            RuleFor(c => c.IsPrimary)
+                .NotNull().WithMessage("IsPrimary cannot be null.");
+
+            RuleFor(c => c.DisplayOrder)
+                .NotNull().WithMessage("DisplayOrder cannot be empty.")
+                .GreaterThanOrEqualTo(0).WithMessage("DisplayOrder must be greater than or equal to 0.");
+
+            RuleFor(c => c.AltText)
+                .MaximumLength(200).WithMessage("AltText cannot exceed 200 characters.");
         }
     }
 }
