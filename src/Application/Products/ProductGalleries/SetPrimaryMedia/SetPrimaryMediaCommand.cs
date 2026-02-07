b7acd9d9dfd4a968168a988ab19a4c6c964508d8
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 12:52:57 -0300

    Add SetPrimaryMediaCommand

---
+++ src/Application/Products/ProductGalleries/SetPrimaryMedia/SetPrimaryMediaCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.ProductGalleries.SetPrimaryMedia
+{
+    public sealed record SetPrimaryMediaCommand(long Id) : ICommand;
+}
