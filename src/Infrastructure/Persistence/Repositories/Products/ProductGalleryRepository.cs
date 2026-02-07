a0db2b22e75d7eb5be40c59a4bffeed4ba771ad9
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 11:19:47 -0300

    Add ProductGalleryRepository

---
+++ src/Infrastructure/Persistence/Repositories/Products/ProductGalleryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Domain.Entities.Products;
+using Infrastructure.Persistence.Database;
+
+namespace Infrastructure.Persistence.Repositories.Products
+{
+    public class ProductGalleryRepository : Repository<ProductGallery>, IProductGalleryRepository
+    {
+        public ProductGalleryRepository(ApplicationDbContext context) 
+            : base(context)
+        {
+        }
+    }
+}
