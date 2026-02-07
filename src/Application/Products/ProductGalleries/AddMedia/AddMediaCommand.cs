083590132eb4b62249777a2b2927369a8a73426d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 11:22:35 -0300

    Add AddMediaCommand

---
+++ src/Application/Products/ProductGalleries/AddMedia/AddMediaCommand.cs
@@ -0,0 +1,9 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.ProductGalleries.AddMedia
+{
+    public sealed record AddMediaCommand(
+        Stream Stream,
+        string FileName,
+        string ContentType) : ICommand<long>;
+}
