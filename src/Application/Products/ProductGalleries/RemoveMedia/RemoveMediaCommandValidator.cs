fbf4c5edb8144f83fc1ddea9728c56857fee6550
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 12:44:50 -0300

    Add RemoveMediaCommandValidator

---
+++ src/Application/Products/ProductGalleries/RemoveMedia/RemoveMediaCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Products.ProductGalleries.RemoveMedia
+{
+    public sealed class RemoveMediaCommandValidator : AbstractValidator<RemoveMediaCommand>
+    {
+        public RemoveMediaCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotNull().WithMessage("ProductGallery ID is required.")
+                .GreaterThan(0).WithMessage("Invalid ProductGallery ID.");
+        }
+    }
+}
