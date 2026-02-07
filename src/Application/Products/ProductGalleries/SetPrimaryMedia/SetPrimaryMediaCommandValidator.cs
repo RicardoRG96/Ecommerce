cf4101f171519b5f8bbd00788d04171e8f9e25c7
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 12:53:38 -0300

    Add SetPrimaryMediaCommandValidator

---
+++ src/Application/Products/ProductGalleries/SetPrimaryMedia/SetPrimaryMediaCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Products.ProductGalleries.SetPrimaryMedia
+{
+    public sealed class SetPrimaryMediaCommandValidator : AbstractValidator<SetPrimaryMediaCommand>
+    {
+        public SetPrimaryMediaCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotNull().WithMessage("ProductGallery ID is required.")
+                .GreaterThan(0).WithMessage("Invalid ProductGallery ID.");
+        }
+    }
+}
