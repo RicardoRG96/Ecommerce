9d34725876abd38aa70a7d17f746a4e863308aca
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 12:55:46 -0300

    Add methods to IProductGalleryRepository

--- src/Application/Abstractions/Data/Repositories/Products/IProductGalleryRepository.cs
+++ src/Application/Abstractions/Data/Repositories/Products/IProductGalleryRepository.cs
@@ -4,5 +4,7 @@ namespace Application.Abstractions.Data.Repositories.Products
 {
     public interface IProductGalleryRepository : IRepository<ProductGallery>
     {
+        Task<ProductGallery?> GetPrimaryMedia(long productGalleryId, CancellationToken cancellationToken);
+         Task<List<ProductGallery>> GetByProductIdAsync(long productId, CancellationToken cancellationToken);
     }
 }
