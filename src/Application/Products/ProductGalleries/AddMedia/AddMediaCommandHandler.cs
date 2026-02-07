dd50eea788e05817c62f7fa4efdca0327cab960e
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 11:33:46 -0300

    Add AddMediaCommandHandler

---
+++ src/Application/Products/ProductGalleries/AddMedia/AddMediaCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Application.Abstractions.Data.UnitOfWork;
+using Application.Abstractions.Messaging;
+using Application.Abstractions.Storage;
+using SharedKernel;
+
+namespace Application.Products.ProductGalleries.AddMedia
+{
+    internal sealed class AddMediaCommandHandler : ICommandHandler<AddMediaCommand, long>
+    {
+        private readonly IProductGalleryRepository _productGalleryRepository;
+        private readonly IFileStorageService _fileStorageService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AddMediaCommandHandler(
+            IProductGalleryRepository productGalleryRepository,
+            IFileStorageService fileStorageService,
+            IUnitOfWork unitOfWork)
+        {
+            _productGalleryRepository = productGalleryRepository;
+            _fileStorageService = fileStorageService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<long>> Handle(AddMediaCommand command, CancellationToken cancellationToken)
+        {
+            Result<string> uploadResult = await _fileStorageService.UploadAsync(
+                command.Stream,
+                command.FileName,
+                command.ContentType,
+                cancellationToken);
+
+            if (uploadResult.IsFailure)
+            {
+                return Result.Failure<long>(uploadResult.Error);
+            }
+        }
+    }
+}
