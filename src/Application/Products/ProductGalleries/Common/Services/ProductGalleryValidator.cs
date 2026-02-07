d716e611f3e2cfa4c54821fd64ee3c511e105e76
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 12:11:22 -0300

    Add ProductGalleryValidator

---
+++ src/Application/Products/ProductGalleries/Common/Services/ProductGalleryValidator.cs
@@ -0,0 +1,72 @@
+﻿using Application.Abstractions.Data.Repositories.Products;
+using Domain.Entities.Products;
+using Domain.Errors.Products;
+using SharedKernel;
+
+namespace Application.Products.ProductGalleries.Common.Services
+{
+    public sealed class ProductGalleryValidator : IProductGalleryValidator
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IProductSkuRepository _productSkuRepository;
+
+        public ProductGalleryValidator(
+            IProductRepository productRepository, 
+            IProductSkuRepository productSkuRepository)
+        {
+            _productRepository = productRepository;
+            _productSkuRepository = productSkuRepository;
+        }
+
+        public async Task<Result> ValidateProductIdExists(long productId, CancellationToken cancellationToken)
+        {
+            Product? product = await _productRepository.GetByIdAsync(productId, cancellationToken);
+
+            if (product is null)
+            {
+                return Result.Failure(ProductGalleryErrors.ProductNotFound);
+            }
+
+            return Result.Success();
+        }
+
+        public async Task<Result> ValidateSkuIdExists(long? skuId, CancellationToken cancellationToken)
+        {
+            if (skuId is null)
+            {
+                return Result.Success();
+            }
+
+            ProductSku? sku = await _productSkuRepository.GetByIdAsync(skuId.Value, cancellationToken);
+
+            if (sku is null)
+            {
+                return Result.Failure(ProductGalleryErrors.SkuNotFound);
+            }
+
+            return Result.Success();
+        }
+
+        public async Task<Result> ValidateSkuBelongsToProduct(long productId, long? skuId, CancellationToken cancellationToken)
+        {
+            if (skuId is null)
+            {
+                return Result.Success();
+            }
+
+            Product? product = await _productRepository.GetByIdAsync(productId, cancellationToken);
+
+            if (product is null)
+            {
+                return Result.Failure(ProductGalleryErrors.ProductNotFound);
+            }
+
+            if (!product.ProductSkus.Any(s => s.Id == skuId.Value))
+            {
+                return Result.Failure(ProductGalleryErrors.SkuNotBelongsToProduct);
+            }
+
+            return Result.Success();
+        }
+    }
+}
---
+++ src/Domain/Errors/Products/ProductGalleryErrors.cs
@@ -0,0 +1,23 @@
+﻿using SharedKernel;
+
+namespace Domain.Errors.Products
+{
+    public static class ProductGalleryErrors
+    {
+        public static Error NotFound(long productGalleryId) => Error.NotFound(
+            "ProductGallery.NotFound",
+            $"The ProductGallery with the id = '{productGalleryId}' was not found");
+
+        public static readonly Error ProductNotFound = Error.NotFound(
+            "ProductGallery.ProductNotFound",
+            "The provided productId was not found");
+
+        public static readonly Error SkuNotFound = Error.NotFound(
+            "ProductGallery.SkuNotFound",
+            "The provided skuId was not found");
+
+        public static readonly Error SkuNotBelongsToProduct = Error.NotFound(
+            "ProductGallery.SkuNotBelongsToProduct",
+            "The provided skuId does not belong to the product");
+    }
+}
