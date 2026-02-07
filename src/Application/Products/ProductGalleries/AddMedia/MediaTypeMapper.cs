479162c891f4d625218f7c646f0236be19db112d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 11:58:16 -0300

    Add MediaTypeMapper

---
+++ src/Application/Products/ProductGalleries/AddMedia/MediaTypeMapper.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.Products.ProductGalleries.AddMedia
+{
+    public static class MediaTypeMapper
+    {
+        public static string Map(string contentType)
+        {
+            if (contentType.StartsWith("image/", StringComparison.OrdinalIgnoreCase))
+            {
+                return "Image";
+            }
+            else if (contentType.StartsWith("video/", StringComparison.OrdinalIgnoreCase))
+            {
+                return "Video";
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
+    }
+}
