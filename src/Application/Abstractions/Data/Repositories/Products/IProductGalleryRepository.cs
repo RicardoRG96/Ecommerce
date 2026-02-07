4070d9a0218cac9b9f705e60387435ccad12b484
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-07 11:17:00 -0300

    Add IProductGalleryRepository

---
+++ src/Application/Abstractions/Data/Repositories/Products/IProductGalleryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities.Products;
+
+namespace Application.Abstractions.Data.Repositories.Products
+{
+    public interface IProductGalleryRepository : IRepository<ProductGallery>
+    {
+    }
+}
