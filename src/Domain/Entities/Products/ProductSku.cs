b8f13b523c8bd100e81258be06e3de9927fb69b3
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-08 09:31:58 -0300

    Add ReorderGallery method to ProductSku Entity

--- src/Domain/Entities/Products/ProductSku.cs
+++ src/Domain/Entities/Products/ProductSku.cs
@@ -1,4 +1,5 @@
-﻿using SharedKernel;
+﻿using Domain.Errors.Products;
+using SharedKernel;
 
 namespace Domain.Entities.Products
 {
@@ -83,6 +84,33 @@ namespace Domain.Entities.Products
         public void Deactivate()
         {
             IsActive = false;
+        }
+
+        public Result ReorderGallery(IReadOnlyList<long> orderedIds)
+        {
+            if (orderedIds.Count != ProductGalleries.Count)
+            {
+                return Result.Failure(ProductSkuErrors.InvalidOrderGallery);
+            }
+
+            if (orderedIds.Distinct().Count() != orderedIds.Count)
+            {
+                return Result.Failure(ProductSkuErrors.DuplicateIdsInGallery);
+            }
+
+            Dictionary<long, ProductGallery> galleryMap = ProductGalleries.ToDictionary(g => g.Id);
+
+            if (orderedIds.Any(id => !galleryMap.ContainsKey(id)))
+            {
+                return Result.Failure(ProductSkuErrors.ForeignItemInGallery);
+            }
+
+            for (int i = 0; i < orderedIds.Count; i++)
+            {
+                galleryMap[orderedIds[i]].DisplayOrder = i + 1;
+            }
+
+            return Result.Success();
         }
     }
 }
