531ce5ba2b47b00f4617f58e58df4b86c169cb8d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-08 09:08:41 -0300

    Add ReorderGalleryCommandValidator

---
+++ src/Application/Products/ProductGalleries/ReorderGallery/ReorderGalleryCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Products.ProductGalleries.ReorderGallery
+{
+    public sealed class ReorderGalleryCommandValidator : AbstractValidator<ReorderGalleryCommand>
+    {
+        public ReorderGalleryCommandValidator()
+        {
+            RuleFor(c => c.ProductId)
+                .NotNull().WithMessage("ProductId is required.")
+                .GreaterThan(0).WithMessage("ProductId must be greater than zero.");
+
+            RuleFor(c => c.OrderedGalleryItemIds)
+                .NotNull().WithMessage("OrderedGalleryItemIds is required.")
+                .Must(ids => ids.Length > 0).WithMessage("OrderedGalleryItemIds must contain at least one ID.");
+        }
+    }
+}
