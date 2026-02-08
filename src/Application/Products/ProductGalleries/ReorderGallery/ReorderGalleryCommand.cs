090a9de89333d370acfdbd4d6cc280db3289df0a
RicardoRG96 <ricardoretamal10@gmail.com>
2026-02-08 09:06:57 -0300

    Add ReorderGalleryCommand

---
+++ src/Application/Products/ProductGalleries/ReorderGallery/ReorderGalleryCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Products.ProductGalleries.ReorderGallery
+{
+    public sealed record ReorderGalleryCommand(
+        long ProductId,
+        long[] OrderedGalleryItemIds) : ICommand;
+}
